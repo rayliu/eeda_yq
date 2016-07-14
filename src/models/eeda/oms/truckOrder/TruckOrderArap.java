feb3b6246f048e607080139c4f8efd56e742ad0b
qiujiazhen <qiu_jiazhen@163.com>
2016-07-14 15:52:48 +0800

    truckOrder文件补交

---
+++ src/models/eeda/oms/truckOrder/TruckOrderArap.java
@@ -0,0 +1,10 @@
+package models.eeda.oms.truckOrder;
+
+import com.jfinal.plugin.activerecord.Model;
+
+@SuppressWarnings("serial")
+public class TruckOrderArap extends Model<TruckOrderArap> {
+	public static final TruckOrderArap dao = new TruckOrderArap();
+}
+
+
