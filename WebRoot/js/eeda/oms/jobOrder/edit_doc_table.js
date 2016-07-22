8e71e596e41f52c7fac596e78d8895e19f3c0f29
qiujiazhen <qiu_jiazhen@163.com>
2016-07-22 19:58:47 +0800

    添加航班和文档的前端

---
+++ WebRoot/js/eeda/oms/jobOrder/edit_doc_table.js
@@ -0,0 +1,112 @@
+define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco'], function ($, metisMenu) {
+$(document).ready(function() {
+
+	var deletedTableIds=[];
+	
+    //删除一行
+    $("#doc_table").on('click', '.delete', function(e){
+        e.preventDefault();
+        var tr = $(this).parent().parent();
+        deletedTableIds.push(tr.attr('id'))
+        
+        docTable.row(tr).remove().draw();
+    }); 
+    //添加一行
+    $('#add_doc').on('click', function(){
+        var item={};
+        docTable.row.add(item).draw(true);
+    });
+
+    itemOrder.buildDocDetail=function(){
+        var cargo_table_rows = $("#doc_table tr");
+        var cargo_items_array=[];
+        for(var index=0; index<cargo_table_rows.length; index++){
+            if(index==0)
+                continue;
+
+            var row = cargo_table_rows[index];
+            var empty = $(row).find('.dataTables_empty').text();
+            if(empty)
+            	continue;
+            
+            var id = $(row).attr('id');
+            if(!id){
+                id='';
+            }
+            
+            var item={}
+            item.id = id;
+            for(var i = 1; i < row.childNodes.length; i++){
+            	var el = $(row.childNodes[i]).find('input');
+            	var name = el.attr('name'); 
+            	
+            	if(el && name){
+                	var value = el.val();//元素的值
+                	item[name] = value;
+            	}
+            }
+            item.action = id.length > 0?'UPDATE':'CREATE';
+            cargo_items_array.push(item);
+        }
+
+        //add deleted items
+        for(var index=0; index<deletedTableIds.length; index++){
+            var id = deletedTableIds[index];
+            var item={
+                id: id,
+                action: 'DELETE'
+            };
+            cargo_items_array.push(item);
+        }
+        deletedTableIds = [];
+        return cargo_items_array;
+    };
+    
+    
+    //------------事件处理
+    var docTable = eeda.dt({
+        id: 'doc_table',
+        columns:[
+            { "width": "30px",
+                "render": function ( data, type, full, meta ) {
+                	return '<button type="button" class="delete btn btn-default btn-xs">删除</button> ';
+                }
+            },
+            { "data": "DOC_NAME",
+                "render": function ( data, type, full, meta ) {
+                    if(!data)
+                        data='';
+                    return '<input type="text" name="doc_name" style="width:80px" value="'+data+'" class="form-control" />';
+                }
+            },
+            { "data": "UPLOADER",
+                "render": function ( data, type, full, meta ) {
+                    if(!data)
+                        data='';
+                    return '<input type="text" name="uploader" style="width:80px" value="'+data+'" class="form-control" />';
+                }
+            },
+            { "data": "DOC_NAME", 
+                "render": function ( data, type, full, meta ) {
+                    if(!data)
+                        data='';
+                    return '<input type="text" name="doc_name" style="width:80px" value="'+data+'" class="form-control" />';
+                }
+            },
+            { "data": "UPLOAD_TIME",
+                "render": function ( data, type, full, meta ) {
+                    if(!data)
+                        data='';
+                    return '<input type="number" name="upload_time" style="width:80px" value="'+data+'" class="form-control "/>';
+                }
+            }
+        ]
+    });
+
+    //刷新明细表
+    itemOrder.refleshDocTable = function(order_id){
+    	var url = "/jobOrder/tableList?order_id="+order_id+"&type=charge";
+    	docTable.ajax.url(url).load();
+    }
+} );
+});
