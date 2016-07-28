9b12f95c87725fd819eff8f2598cdb01b44ee00b
qiujiazhen <qiu_jiazhen@163.com>
2016-07-28 15:40:37 +0800

    planOrder/edit.js

---
+++ WebRoot/js/eeda/oms/planOrder/edit.js
@@ -0,0 +1,72 @@
+define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco'], function ($, metisMenu) { 
+    $(document).ready(function() {
+    	
+    	document.title = order_no + ' | ' + document.title;
+
+        //------------save
+        $('#saveBtn').click(function(e){
+            //阻止a 的默认响应行为，不需要跳转
+            e.preventDefault();
+            //提交前，校验数据
+            if(!$("#orderForm").valid()){
+                return;
+            }
+            
+            $(this).attr('disabled', true);
+
+            var items_array = salesOrder.buildCargoDetail();
+            var order = {
+                id: $('#order_id').val(),
+                customer_id: $('#customer_id').val(),
+                type: $('#type').val(),
+                remark: $('#note').val(),
+                status: $('#status').val()==''?'新建':$('#status').val(),
+                item_list:items_array
+                
+            };
+
+            //异步向后台提交数据
+            $.post('/planOrder/save', {params:JSON.stringify(order)}, function(data){
+                var order = data;
+                if(order.ID>0){
+                	$("#creator_name").val(order.CREATOR_NAME);
+                    $("#create_stamp").val(order.CREATE_STAMP);
+                    $("#order_id").val(order.ID);
+                    $("#order_no").val(order.ORDER_NO);
+                    $("#status").val(order.STATUS);
+                    $("#note").val(order.REMARK);
+                    eeda.contactUrl("edit?id",order.ID);
+                    $.scojs_message('保存成功', $.scojs_message.TYPE_OK);
+                    $('#saveBtn').attr('disabled', false);
+                    
+                    //异步刷新明细表
+                    salesOrder.refleshTable(order.ID);
+                }else{
+                    $.scojs_message('保存失败', $.scojs_message.TYPE_ERROR);
+                    $('#saveBtn').attr('disabled', false);
+                }
+            },'json').fail(function() {
+                $.scojs_message('保存失败', $.scojs_message.TYPE_ERROR);
+                $('#saveBtn').attr('disabled', false);
+              });
+        });  
+        
+        
+        //计划单跳转到工作单
+        $('#create_jobOrder').click(function(){
+        	$(this).attr('disabled', true);
+        	var order_id = $('#order_id').val();
+        	var itemIds=[];
+        	$('#cargo_table input[type="checkbox"]').each(function(){
+        		var checkbox = $(this).prop('checked');
+        		if(checkbox){
+        			var itemId = $(this).parent().parent().attr('id');
+        			itemIds.push(itemId);
+        		}
+        	});
+        	location.href ="/jobOrder/create?order_id="+order_id+"&itemIds="+itemIds;
+        })
+        
+
+     });
+});
---
+++ WebRoot/js/eeda/oms/planOrder/edit_item_table.js
@@ -0,0 +1,281 @@
+define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco'], function ($, metisMenu) { 
+	$(document).ready(function() {
+
+	    var deletedTableIds=[];
+
+	    //删除一行
+	    $("#cargo_table").on('click', '.delete', function(e){
+	        e.preventDefault();
+	        var tr = $(this).parent().parent();
+	        deletedTableIds.push(tr.attr('id'));
+	        
+	        cargoTable.row(tr).remove().draw();
+	    }); 
+	    
+	    salesOrder.buildCargoDetail=function(){
+	    	var cargo_table_rows = $("#cargo_table tr");
+	        var cargo_items_array=[];
+	        for(var index=0; index<cargo_table_rows.length; index++){
+	            if(index==0)
+	                continue;
+
+	            var row = cargo_table_rows[index];
+	            var empty = $(row).find('.dataTables_empty').text();
+	            if(empty)
+	            	continue;
+	            
+	            var id = $(row).attr('id');
+	            if(!id){
+	                id='';
+	            }
+	            
+	            var item={}
+	            item.id = id;
+	            for(var i = 1; i < row.childNodes.length; i++){
+	            	var el = $(row.childNodes[i]).find('input, select');
+	            	var name = el.attr('name'); //name='abc'
+	            	
+	            	if(el && name){
+	                	var value = el.val();//元素的值
+	                	item[name] = value;
+	            	}
+	            }
+	            item.action = id.length > 0?'UPDATE':'CREATE';
+	            cargo_items_array.push(item);
+	        }
+
+	        //add deleted items
+	        for(var index=0; index<deletedTableIds.length; index++){
+	            var id = deletedTableIds[index];
+	            var item={
+	                id: id,
+	                action: 'DELETE'
+	            };
+	            cargo_items_array.push(item);
+	        }
+	        deletedTableIds = [];
+	        return cargo_items_array;
+	    };
+	    
+	    //------------事件处理
+	    var cargoTable = eeda.dt({
+            id: 'cargo_table',
+            columns:[
+				{ "data":"ID","width": "10px",
+				    "render": function ( data, type, full, meta ) {
+				    	if(data)
+				    		return '<input type="checkbox" class="checkBox">';
+				    	else 
+				    		return '<input type="checkbox" class="checkBox" disabled>';
+				    }
+				},
+                         
+	            { "width": "30px",
+	                "render": function ( data, type, full, meta ) {
+	                	return '<button type="button" class="delete btn btn-default btn-xs">删除</button> ';
+	                }
+	            },
+	            { "data": "TRANSPORT_TYPE",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                    var str = '<select name="transport_type" class="form-control search-control">'
+                			+'<option></option>'
+		                   +'<option value="ocean" '+(data=='ocean' ? 'selected':'')+'>海运</option>'
+		                   +'<option value="land" '+(data=='land' ? 'selected':'')+'>陆运</option>'
+		                   +'<option value="air" '+(data=='air' ? 'selected':'')+'>空运</option>'
+		                   +'</select>';
+	                    return str;
+	                }
+	            },
+	            { "data": "LOAD_TYPE", 
+	                "render": function ( data, type, full, meta ) {
+	                   if(!data)
+	                	   data='';
+	                   var str= '<select name="load_type" class="form-control search-control">'
+	                	   	 	+'<option></option>'
+			                   +'<option value="FCL" '+ (data=='FCL'?'selected':'') +'>FCL</option>'
+			                   +'<option value="LCL" '+ (data=='LCL'?'selected':'') +'>LCL</option>'
+			                   +'<option value="FTL" '+ (data=='FTL'?'selected':'') +'>FTL</option>'
+			                   +'<option value="LTL" '+ (data=='LTL'?'selected':'') +'>LTL</option>'
+			                   +'</select>';
+			           return str;
+	                }
+	            },
+	            { "data": "CONTAINER_TYPE", 
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                    var str = '<select name="container_type" class="form-control search-control">'
+	                    			+'<option></option>'
+				                   +'<option value="20GP" '+(data=='20GP' ? 'selected':'')+'>20GP</option>'
+				                   +'<option value="40GP" '+(data=='40GP' ? 'selected':'')+'>40GP</option>'
+				                   +'<option value="45GP" '+(data=='45GP' ? 'selected':'')+'>45GP</option>'
+				                   +'</select>';
+	                    return str;
+	                }
+	            },
+	            { "data": "CONTAINER_AMOUNT",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="CONTAINER_AMOUNT" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "CARGO_NAME",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="CARGO_NAME" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "PIECES" ,
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="PIECES" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "VOLUME",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="VOLUME" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "NET_WEIGHT",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="NET_WEIGHT" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "GROSS_WEIGHT",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="GROSS_WEIGHT" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "FACTORY_LOADING_TIME" ,
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="FACTORY_LOADING_TIME" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "TRUCK_TYPE",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="TRUCK_TYPE" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "PICKUP_ADDR",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="PICKUP_ADDR" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "DILVERY_ADDR",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="DILVERY_ADDR" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "POR" ,
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="POR" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "POL" ,
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="POL" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "POD",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="POD" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "CARRIER",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="CARRIER" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "VESSEL",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="VESSEL" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "VOYAGE" ,
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="VOYAGE" value="'+data+'" class="form-control" style="width:80px"/>';
+	                }
+	            },
+	            { "data": "CLS",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                   return '<input type="text" name="CLS" value="'+data+'" class="form-control" style="width:180px"/>';
+	                }
+	            },
+	            { "data": "ETD",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                    return '<input type="text" name="ETD" value="'+data+'" class="form-control" style="width:180px"/>';
+	                }
+	            },
+	            { "data": "ETA",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                    return '<input type="text" name="ETA" value="'+data+'" class="form-control" style="width:180px"/>';
+	                }
+	            }
+	        ]
+	    });
+
+	    $('#add_cargo').on('click', function(){
+	        var item={};
+	        cargoTable.row.add(item).draw(true);
+	    });
+	    
+	    //刷新明细表
+	    salesOrder.refleshTable = function(order_id){
+	    	var url = "/planOrder/tableList?order_id="+order_id;
+	    	cargoTable.ajax.url(url).load();
+	    }
+	    
+	    //checkbox选中则button可点击
+		$('#cargo_table').on('click','.checkBox',function(){
+			var hava_check = 0;
+			$('#cargo_table input[type="checkbox"]').each(function(){	
+				var checkbox = $(this).prop('checked');
+	    		if(checkbox){
+	    			hava_check=1;
+	    		}	
+			})
+			if(hava_check>0){
+				$('#create_jobOrder').attr('disabled',false);
+			}else{
+				$('#create_jobOrder').attr('disabled',true);
+			}
+		});
+	});
+});
