2b0c13c058ff50366559cb4d137d06801f98757b
qiujiazhen <qiu_jiazhen@163.com>
2016-08-11 11:26:27 +0800

    去掉海运一些必填项；生成PDF数据不足判断

--- WebRoot/js/eeda/oms/jobOrder/job_order_report.js
+++ WebRoot/js/eeda/oms/jobOrder/job_order_report.js
@@ -1,6 +1,9 @@
 define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco'], function ($, metisMenu) {
 $(document).ready(function() {
 	
+    //隐藏pdf alert
+    $('#pdfAlert').hide();
+	
 	//未保存或数据不足不生成PDF
 	if($('#order_id').val()==''){
 		$("#oceanPDF").hide();
@@ -9,17 +12,75 @@ $(document).ready(function() {
 	//生成海运HBL PDF
     $('#printOceanHBL').click(function(){
     	//数据不足提示
-    	if( $('#gross_weight').val()==''||$('#volume').val()==''||$('#pieces').val()==''
-    		||$('#ocean_shipper_input').val()==''||$('#ocean_shipper_info').val()==''||$('#ocean_consignee_input').val()==''
-    		||$('#ocean_consignee_info').val()==''||$('#ocean_consignee_info').val()==''||$('#ocean_notify_party_input').val()==''
-    		||$('#ocean_notify_party_info').val()==''||$('#hbl_no').val()==''||$('#vessel').val()==''
-    		||$('#route').val()==''||$('#voyage').val()==''||$('#por').val()==''||$('#pol').val()==''
-    		||$('#pod').val()==''||$('#fnd').val()==''||$('#hub').val()==''
-    		||$('#etd').val()==''||$('#shipping_mark').val()==''||$('#cargo_desc').val()==''
-    	){
-    		$.scojs_message('数据不足,不能打印PDF', $.scojs_message.TYPE_ERROR);
-    		
-    	}else{
+    	var alert = '';
+    	if( $('#gross_weight').val()==''){
+    		alert+='毛重,';
+    	}
+    	if($('#volume').val()==''){
+    		alert+='体积 ,';
+    	}
+    	if($('#pieces').val()==''){
+    		alert+='件数 ,';
+    	}
+    	if($('#ocean_shipper_input').val()==''){
+    		alert+='发货人Shipper,';
+    	}
+    	if($('#ocean_shipper_info').val()==''){
+    		alert+='发货人备注,';
+    	}
+    	if($('#ocean_consignee_input').val()==''){
+    		alert+='收货人Consignee,';
+    	}
+    	if($('#ocean_consignee_info').val()==''){
+    		alert+='收货人备注,';
+    	}
+    	if($('#ocean_notify_party_input').val()==''){
+    		alert+='通知人NotifyParty,';
+    	}
+    	if($('#ocean_notify_party_info').val()==''){
+    		alert+='通知人备注,';
+    	}
+    	if($('#hbl_no').val()==''){
+    		alert+='HBL号码,';
+    	}
+    	if($('#vessel').val()==''){
+    		alert+='船名,';
+    	}
+    	if($('#route').val()==''){
+    		alert+='航线,';
+    	}
+    	if($('#voyage').val()==''){
+    		alert+='航次,';
+    	}
+    	if($('#por').val()==''){
+    		alert+='收货港 POR,';
+    	}
+    	if($('#pol').val()==''){
+    		alert+='装货港 POL,';
+    	}
+    	if($('#pod').val()==''){
+    		alert+='卸货港 POD,';
+    	}
+    	if($('#fnd').val()==''){
+    		alert+='目的地 FND,';
+    	}
+    	if($('#hub').val()==''){
+    		alert+='转运港 HUB,';
+    	}
+    	if($('#etd').val()==''){
+    		alert+='ETD,';
+    	}
+    	if($('#shipping_mark').val()==''){
+    		alert+='唛头 ,';
+    	}
+    	if($('#cargo_desc').val()==''){
+    		alert+='货物描述 ';
+    	}
+    	
+		if(alert!=''){
+			$('#pdfAlert').show();
+			$('#pdfAlertContent').html("以下字段未填，请先填好才能生成PDF<br><br>"+alert);
+		}else{
 	    	var order_no = $("#order_no").val();
 	    	$.post('/jobOrderReport/printOceanHBL', {order_no:order_no}, function(data){
 	    		if(data){
@@ -29,21 +90,81 @@ $(document).ready(function() {
 	               }
 	    	}); 
     	}
+    		
     });
     
     //生成海运booking PDF
     $('#printOceanBooking').click(function(){
     	//数据不足提示
-    	if( $('#gross_weight').val()==''||$('#volume').val()==''||$('#pieces').val()==''
-    		||$('#ocean_shipper_input').val()==''||$('#ocean_shipper_info').val()==''||$('#ocean_consignee_input').val()==''
-    		||$('#ocean_consignee_info').val()==''||$('#ocean_consignee_info').val()==''||$('#ocean_notify_party_input').val()==''
-    		||$('#ocean_notify_party_info').val()==''||$('#hbl_no').val()==''||$('#vessel').val()==''
-    		||$('#route').val()==''||$('#voyage').val()==''||$('#por').val()==''||$('#pol').val()==''
-    		||$('#pod').val()==''||$('#fnd').val()==''||$('#hub').val()==''
-    		||$('#etd').val()==''||$('#shipping_mark').val()==''||$('#cargo_desc').val()==''
-    	){
-    		$.scojs_message('数据不足,不能打印PDF', $.scojs_message.TYPE_ERROR);
-    	}else{
+    	var alert = '';
+    	if( $('#gross_weight').val()==''){
+    		alert+='毛重,';
+    	}
+    	if($('#volume').val()==''){
+    		alert+='体积 ,';
+    	}
+    	if($('#pieces').val()==''){
+    		alert+='件数 ,';
+    	}
+    	if($('#ocean_shipper_input').val()==''){
+    		alert+='发货人Shipper,';
+    	}
+    	if($('#ocean_shipper_info').val()==''){
+    		alert+='发货人备注,';
+    	}
+    	if($('#ocean_consignee_input').val()==''){
+    		alert+='收货人Consignee,';
+    	}
+    	if($('#ocean_consignee_info').val()==''){
+    		alert+='收货人备注,';
+    	}
+    	if($('#ocean_notify_party_input').val()==''){
+    		alert+='通知人NotifyParty,';
+    	}
+    	if($('#ocean_notify_party_info').val()==''){
+    		alert+='通知人备注,';
+    	}
+    	if($('#hbl_no').val()==''){
+    		alert+='HBL号码,';
+    	}
+    	if($('#vessel').val()==''){
+    		alert+='船名,';
+    	}
+    	if($('#route').val()==''){
+    		alert+='航线,';
+    	}
+    	if($('#voyage').val()==''){
+    		alert+='航次,';
+    	}
+    	if($('#por').val()==''){
+    		alert+='收货港 POR,';
+    	}
+    	if($('#pol').val()==''){
+    		alert+='装货港 POL,';
+    	}
+    	if($('#pod').val()==''){
+    		alert+='卸货港 POD,';
+    	}
+    	if($('#fnd').val()==''){
+    		alert+='目的地 FND,';
+    	}
+    	if($('#hub').val()==''){
+    		alert+='转运港 HUB,';
+    	}
+    	if($('#etd').val()==''){
+    		alert+='ETD,';
+    	}
+    	if($('#shipping_mark').val()==''){
+    		alert+='唛头 ,';
+    	}
+    	if($('#cargo_desc').val()==''){
+    		alert+='货物描述 ';
+    	}
+    	
+    	if(alert!=''){
+			$('#pdfAlert').show();
+			$('#pdfAlertContent').html("以下字段未填，请先填好才能生成PDF<br><br>"+alert);
+		}else{
 	    	var order_no = $("#order_no").val();
 	    	$.post('/jobOrderReport/printOceanBooking', {order_no:order_no}, function(data){
 	    		if(data){
@@ -57,24 +178,54 @@ $(document).ready(function() {
     
     //生成空运booking PDF
     $('#printAirBooking').click(function(){
-    	//数据不足提示
-    	if( $('#gross_weight').val()==''||$('#volume').val()==''||$('#pieces').val()==''||$('#shipper_input').val()==''
-    		||$('#shipper_info').val()==''||$('#consignee_input').val()==''||$('#consignee_info').val()==''||$('#consignee_info').val()==''
-    		||$('#notify_party_input').val()==''||$('#air_gross_weight').val()==''||$('#air_net_weight').val()==''||$('#air_volume').val()==''
-    		||$('#shipping_mark').val()==''
-    	){
-    		$.scojs_message('数据不足,不能打印PDF', $.scojs_message.TYPE_ERROR);
-    	}else{
-    		var order_no = $("#order_no").val();
-    		$.post('/jobOrderReport/printAirBooking', {order_no:order_no}, function(data){
-    			if(data){
-    				window.open(data);
+    	$('#shipper_input').val()==''
+    			//数据不足提示
+    	    	var alert = '';
+    	    	if($('#shipper_input').val()==''){
+    	    		alert+='发货人Shipper,';
+    	    	}
+    	    	if($('#shipper_info').val()==''){
+    	    		alert+='发货人备注,';
+    	    	}
+    	    	if($('#consignee_input').val()==''){
+    	    		alert+='收货人Consignee,';
+    	    	}
+    	    	if($('#consignee_info').val()==''){
+    	    		alert+='收货人备注,';
+    	    	}
+    	    	if($('#notify_party_input').val()==''){
+    	    		alert+='通知人NotifyParty,';
+    	    	}
+    	    	if($('#notify_party_info').val()==''){
+    	    		alert+='通知人备注,';
+    	    	}
+    	    	if( $('#air_gross_weight').val()==''){
+    	    		alert+='毛重,';
+    	    	}
+    	    	if($('#air_volume').val()==''){
+    	    		alert+='体积 ,';
+    	    	}
+    	    	if($('#shipping_mark').val()==''){
+    	    		alert+='唛头 ,';
+    	    	}
+    	    	if($('#air_net_weight').val()==''){
+    	    		alert+='净重 ,';
+    	    	}
+    	    	
+    	    	if(alert!=''){
+    				$('#pdfAlert').show();
+    				$('#pdfAlertContent').html("以下字段未填，请先填好才能生成PDF<br><br>"+alert);
     			}else{
-    				$.scojs_message('生成空运booking PDF失败', $.scojs_message.TYPE_ERROR);
+		    		var order_no = $("#order_no").val();
+		    		$.post('/jobOrderReport/printAirBooking', {order_no:order_no}, function(data){
+		    			if(data){
+		    				window.open(data);
+		    			}else{
+		    				$.scojs_message('生成空运booking PDF失败', $.scojs_message.TYPE_ERROR);
+		    			}
+		    		});    	
     			}
-    		});    	
-    	}
-    });
+    	});
 
     
 });
