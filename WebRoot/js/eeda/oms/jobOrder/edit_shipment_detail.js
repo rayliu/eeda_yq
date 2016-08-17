132f9712e7b1e5b669f145348c266dfae91f8208
Victor.Tan <victor.tan@eeda123.com>
2016-08-17 09:25:07 +0800

    PDF字段的补充

--- WebRoot/js/eeda/oms/jobOrder/edit_shipment_detail.js
+++ WebRoot/js/eeda/oms/jobOrder/edit_shipment_detail.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
     	var item = {};
     	item['id'] = $('#shipment_id').val();
     	item['release_type'] = $('#shipmentForm input[type="radio"]:checked').val();
+    	item['prepaid'] = $('#prepaid').val($('#prepaid').prop('checked')==true?'Y':'N');
+    	item['wait_overseaCustom'] = $('#wait_overseaCustom').val($('#wait_overseaCustom').prop('checked')==true?'Y':'N');
     	var shipmentForm = $('#shipmentForm input,#shipmentForm select,#shipmentForm textarea');
     	for(var i = 0; i < shipmentForm.length; i++){
     		var name = shipmentForm[i].id;
@@ -32,6 +34,22 @@ $(document).ready(function() {
     		$(this).attr("checked",true);
     	}
     });
+    //预付回显
+    var checkBoxVal = $('#hidden_prepaid').val();
+    if(checkBoxVal=='Y'){
+        $('#prepaid').attr("checked",true);    	
+    }
+    else{
+    	$('#prepaid').attr("checked",false);
+    }
+    //待海外报关回显
+    var checkBoxVal = $('#hidden_wait_overseaCustom').val();
+    if(checkBoxVal=='Y'){
+        $('#wait_overseaCustom').attr("checked",true);    	
+    }
+    else{
+    	$('#wait_overseaCustom').attr("checked",false);
+    }
     
 });
 });
