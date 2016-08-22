3102cab9a948920944b0fa2cf381eb7032e9858a
Ray Liu <liuyubie@gmail.com>
2016-08-22 11:58:53 +0800

    jobOrder edit: 空运 航空公司列表组件  js文件

---
+++ WebRoot/WEB-INF/template/eeda/component/jsTemplate/tableInputAirCarrierField.js
@@ -0,0 +1,11 @@
+<script id="table_air_carrier_field_template" type="text/html">
+        <div class="form-group">
+            <input type="text" name="{{id}}" value="{{value}}" field_type='air_carrier_id' style="display:none;"/>
+            <input type="text" class="form-control search-control" 
+                   name="air_carrier_input" placeholder="请选择" value="{{display_value}}">
+        </div> 
+</script>
+<ul id='table_air_carrier_field_list' tabindex="-1" 
+    class="pull-right dropdown-menu default dropdown-scroll" 
+    style="top: 22%; left: 33%; width: 20%;">
+</ul>
