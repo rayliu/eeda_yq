4b7a9bf4313a25a2b4e54e19ea8b46ad4596d3c2
Victor.Tan <victor.tan@eeda123.com>
2016-09-19 21:11:41 +0800

    应收应付对账明细打印

--- WebRoot/js/eeda/arap/ChargeCheckOrder/edit.js
+++ WebRoot/js/eeda/arap/ChargeCheckOrder/edit.js
@@ -98,7 +98,7 @@ $(document).ready(function() {
         })
     //打印应收对账明细
     $('#printBtn').click(function(){
-    	var order_id = $('#order_id1').val();
+    	var order_id = $('#order_id').val();
     	var company_name = $('结算公司').val();
     	$.post('/jobOrderReport/printReceiveDetailPDF',{order_id:order_id,company_name:company_name},function(data){
     		if(data){
