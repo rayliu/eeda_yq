92523012ce11ee2c02511221dc4d9750d082d6ae
Ray Liu <liuyubie@gmail.com>
2016-09-20 15:54:28 +0800

    单号生成修改: 每个前缀都按顺序生成

---
+++ src/models/eeda/profile/OrderNoSeq.java
@@ -0,0 +1,7 @@
+package models.eeda.profile;
+
+import com.jfinal.plugin.activerecord.Model;
+@SuppressWarnings("serial")
+public class OrderNoSeq extends Model<OrderNoSeq> {
+	public static final OrderNoSeq dao = new OrderNoSeq();
+}
