e85fcd0a3d019049cf2b525d1f25b91a56ae94a5
Kevin <kevin.dou@eeda123.com>
2016-09-27 16:16:06 +0800

    出纳日记账

--- WebRoot/js/eeda/arap/AccountAuditLog/list.js
+++ WebRoot/js/eeda/arap/AccountAuditLog/list.js
@@ -14,31 +14,15 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
             serverSide: true, 
             ajax: "/accountAuditLog/list?beginTime="+$("#beginTime_filter").val(),
             columns:[
-                {   "width":"70px",
+                {   "width":"30px",
                     "render": function(data, type, full, meta) {
-                        var str = "";
-                        if(checkedIds.length>0){
-                            for(id in checkedIds){
-                                if(checkedIds[id] == full.ID){
-                                    return str =  '<input type="checkbox" id="'+full.ID+'" class="invoice" order_no="'+full.ORDER_NO+'" checked = "checked">';
-                               }else{
-                                    str =  '<input type="checkbox" id="'+full.ID+'" class="invoice" order_no="'+full.ORDER_NO+'">';
-                                }
-                            }
-                        }else{
-                           str = '<input type="checkbox" id="'+full.ID+'" class="invoice" order_no="'+full.ORDER_NO+'">';
-                        }
-                        return str;
+                    	return '<input type="checkbox" name="" value="'+full.ID+'">';
                     }
                 },
                 {
                     "data":"ORDER_NO","width":"70px",
                     "render": function(data, type, full, meta) {
-                        if(full.SOURCE_ORDER=='应付开票申请单' || full.SOURCE_ORDER=='应收开票申请单' || full.SOURCE_ORDER=='转账单'){
-                            return eeda.getUrlByNo(full.INVOICE_ORDER_ID, full.ORDER_NO);
-                        }else{
-                            return full.ORDER_NO;
-                        }
+                    	return full.ORDER_NO;
                     }
                 },
                 {"data":"SOURCE_ORDER"},
@@ -50,7 +34,6 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
                             return data;
                     }
                 },
-                {"data":"PAYEE_NAME_IN"},
                 {"data":"COST_AMOUNT",
                     "render": function(data, type, full, meta) {
                         if(data!=null)
@@ -59,13 +42,14 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
                             return data;
                     }
                 },
-                {"data":"PAYEE_NAME_OUT"},
                 {"data":"BANK_NAME"},
                 {"data":"CREATE_DATE"},
                 {"data":"USER_NAME"}
             ]
         });
         
+        
+        //上
         var accountTable = eeda.dt({
             id: 'account-table',
             paging: true,
@@ -101,26 +85,6 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
                 }
             ]
         });
-      //   var accountTable = $('#account-table').dataTable({
-      //   	"bFilter": false, //不需要默认的搜索框
-      //   	"bSort": false, 
-      //       // "bPaginate": false, //翻页功能
-      //       "bInfo": false,//页脚信息
-      //   	"sDom": "<'row-fluid'<'span6'l><'span6'f>r><'datatable-scroll't><'row-fluid'<'span12'i><'span12 center'p>>",
-      //   	"bServerSide": true,
-      //   	"oLanguage": {
-      //   		"sUrl": "/eeda/dataTables.ch.txt"
-      //   	},
-      //   	"fnRowCallback": function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
-    		// 	$(nRow).attr('id', aData.ID);
-    		// 	$(nRow).attr('account_id', aData.ID);
-    		// 	return nRow;
-    		// },
-      //   	"sAjaxSource": "/accountAuditLog/accountList?beginTime="+ $("#beginTime_filter").val(),
-      //   	"aoColumns": [   
-    	        
-    	 //     ]      
-      //   });
         
         $('#datetimepicker').datetimepicker({  
             format: 'yyyy-MM',  
@@ -133,7 +97,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
            	var idArr=[];
        	    $("input[name='order_check_box']").each(function(){
        	    	if($(this).prop('checked') == true){
-       	    		idArr.push($(this).parent().parent().attr("account_id"));
+       	    		idArr.push($(this).parent().parent().attr("id"));
        	    	}
        	    });
        	    var ids = idArr.toString();
@@ -148,9 +112,9 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
         
         $("#account-table").on('click', function(e){
         	var idArr=[];
-       	    $("input[name='order_check_box']").each(function(){
+       	    $("#account-table input[name='order_check_box']").each(function(){
        	    	if($(this).prop('checked') == true){
-       	    		idArr.push($(this).parent().parent().attr("account_id"));
+       	    		idArr.push($(this).parent().parent().attr("id"));
        	    	}
        	    });
        	    var ids = idArr.toString();
