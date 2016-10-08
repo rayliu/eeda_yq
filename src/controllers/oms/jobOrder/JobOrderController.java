3a2c515f17195a557bc0c3f4274db69171c2f0e3
Victor.Tan <victor.tan@eeda123.com>
2016-10-08 11:06:23 +0800

    工作单list增加显示空运出货时间

--- src/controllers/oms/jobOrder/JobOrderController.java
+++ src/controllers/oms/jobOrder/JobOrderController.java
@@ -759,18 +759,20 @@ public class JobOrderController extends Controller {
         }
         String sql = "";
         if("sowait".equals(type)){
-        	sql=" SELECT jor.*,ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name, jos.export_date sent_out_time"
+        	sql=" SELECT jor.*,ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name, ifnull(jos.export_date,joa.export_date) sent_out_time"
         			+ " FROM job_order jor "
         			+ " LEFT JOIN job_order_shipment jos on jor.id = jos.order_id "
+        			+ " LEFT JOIN job_order_air joa on joa.order_id = jo.id"
         			+ " left join party p on p.id = jor.customer_id"
         			+ " left join user_login u on u.id = jor.creator "
         			+ " WHERE jor.office_id="+office_id
         			+ " and jor.type = '出口柜货' AND jos.SONO IS NULL AND jor.transport_type LIKE '%ocean%'";        	
         }else if("truckorderwait".equals(type)){
-        	 sql = "SELECT jor.*, ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name, jos.export_date sent_out_time"
+        	 sql = "SELECT jor.*, ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name, ifnull(jos.export_date,joa.export_date) sent_out_time"
         			+ " FROM job_order_land_item joli"
         			+ " left join job_order jor on jor.id = joli.order_id"
         			+ " left join job_order_shipment jos on jos.order_id = jor.id"
+        			+ " LEFT JOIN job_order_air joa on joa.order_id = jo.id"
         			+ " left join party p on p.id = jor.customer_id"
         			+ " left join user_login u on u.id = jor.creator"
         			+ " WHERE jor.office_id="+office_id
@@ -779,16 +781,17 @@ public class JobOrderController extends Controller {
         	
         	
         } else if("siwait".equals(type)){
-        	 sql = " SELECT jor.*, ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name, jos.export_date sent_out_time"
+        	 sql = " SELECT jor.*, ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name, ifnull(jos.export_date,joa.export_date) sent_out_time"
         	 		+ " FROM job_order_shipment jos"
         	 		+ " left join job_order jor on jos.order_id = jor.id"
+        	 		+ " LEFT JOIN job_order_air joa on joa.order_id = jo.id"
         	 		+ " left join party p on p.id = jor.customer_id"
         	 		+ " left join user_login u on u.id = jor.creator "
         	 		+ " WHERE jor.office_id="+office_id
                     + " and TO_DAYS(export_date)=TO_DAYS(now())";
         	
         } else if("mblwait".equals(type)){
-        	sql = "SELECT jor.*, ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name, jos.export_date sent_out_time"
+        	sql = "SELECT jor.*, ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name, ifnull(jos.export_date,joa.export_date) sent_out_time"
         			+ " FROM job_order_shipment jos "
         			+ " left join job_order jor on jos.order_id = jor.id"
         			+ " left join party p on p.id = jor.customer_id"
@@ -797,46 +800,51 @@ public class JobOrderController extends Controller {
                     + " and  jos.si_flag = 'Y' and (jos.mbl_flag != 'Y' or jos.mbl_flag is null)";
         	
         } else if("customwait".equals(type)){
-        	sql = " SELECT jor.*, ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name,jos.export_date sent_out_time "
+        	sql = " SELECT jor.*, ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name,ifnull(jos.export_date,joa.export_date) sent_out_time "
         			+ " from job_order jor "
         			+ " LEFT JOIN job_order_custom joc on joc.order_id = jor.id"
         			+ " left join job_order_shipment jos on jos.order_id = jor.id"
+        			+ " LEFT JOIN job_order_air joa on joa.order_id = jo.id"
         			+ " left join party p on p.id = jor.customer_id"
         			+ " left join user_login u on u.id = jor.creator"
         			+ " where jor.office_id="+office_id
-                                + "  and  jor.transport_type LIKE '%custom%'"
+                    + "  and  jor.transport_type LIKE '%custom%'"
         			+ "  and ifnull(joc.customs_broker,'') = ''";
         	
         } else if("insurancewait".equals(type)){
-        	sql = " SELECT jor.*, ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name, jos.export_date sent_out_time"
+        	sql = " SELECT jor.*, ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name, ifnull(jos.export_date,joa.export_date) sent_out_time"
         			+ " FROM job_order jor LEFT JOIN job_order_insurance joi ON jor.id = joi.order_id"
         			+ " left join job_order_shipment jos on jos.order_id = jor.id"
+        			+ " LEFT JOIN job_order_air joa on joa.order_id = jo.id"
         			+ " left join party p on p.id = jor.customer_id"
         			+ " left join user_login u on u.id = jor.creator"
         			+ " WHERE jor.office_id="+office_id
                     + " and  jor.transport_type LIKE '%insurance%' and joi.insure_no is NULL";
         } else if("overseacustomwait".equals(type)){
-        	sql = "SELECT jor.*, ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name, jos.export_date sent_out_time"
+        	sql = "SELECT jor.*, ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name, ifnull(jos.export_date,joa.export_date) sent_out_time"
         			+ " FROM job_order_shipment jos "
         			+ " LEFT JOIN job_order jor on jos.order_id = jor.id"
+        			+ " LEFT JOIN job_order_air joa on joa.order_id = jo.id"
         			+ " left join party p on p.id = jor.customer_id"
         			+ " left join user_login u on u.id = jor.creator"
         			+ " WHERE jor.office_id="+office_id
                     + " and (jos.afr_ams_flag !='Y' OR jos.afr_ams_flag is  NULL) and jos.wait_overseaCustom = 'Y' "
         			+ " and timediff(now(),jos.etd)<TIME('48:00:00') ";
         } else if("tlxOrderwait".equals(type)){
-        	sql = " SELECT jor.*, ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name, jos.export_date sent_out_time"
+        	sql = " SELECT jor.*, ifnull(u.c_name, u.user_name) creator_name,p.abbr customer_name, ifnull(jos.export_date,joa.export_date) sent_out_time"
         			+ " FROM job_order_shipment jos"
         			+ " LEFT JOIN job_order jor on jos.order_id = jor.id"
+        			+ " LEFT JOIN job_order_air joa on joa.order_id = jo.id"
         			+ " left join party p on p.id = jor.customer_id"
         			+ " left join user_login u on u.id = jor.creator"
         			+ " WHERE jor.office_id="+office_id
                     + " and TO_DAYS(jos.etd)= TO_DAYS(now())";
         }
         else{
-		         sql = "SELECT * from (select jo.*, jos.export_date sent_out_time, ifnull(u.c_name, u.user_name) creator_name, p.abbr customer_name,p.company_name,p.code "
+		         sql = "SELECT * from (select jo.*, ifnull(jos.export_date,joa.export_date) sent_out_time, ifnull(u.c_name, u.user_name) creator_name, p.abbr customer_name,p.company_name,p.code "
 		    			+ " from job_order jo "
 		    			+ " left join job_order_shipment jos on jos.order_id = jo.id"
+		    			+ " LEFT JOIN job_order_air joa on joa.order_id = jo.id"
 		    			+ " left join party p on p.id = jo.customer_id"
 		    			+ " left join user_login u on u.id = jo.creator"
 		    			+ " where jo.office_id="+office_id
