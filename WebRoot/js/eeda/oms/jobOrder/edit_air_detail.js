1d23baf4d15b49325fa4c4f4a792aa39f6421eb2
qiujiazhen <qiu_jiazhen@163.com>
2016-10-09 11:54:10 +0800

    空运常用信息

--- WebRoot/js/eeda/oms/jobOrder/edit_air_detail.js
+++ WebRoot/js/eeda/oms/jobOrder/edit_air_detail.js
@@ -20,5 +20,29 @@ $(document).ready(function() {
     	arrays.push(item);
         return arrays;
     };
+    
+    
+    //常用海运信息模版
+    $('#usedAirInfo').on('click', 'li', function(){
+        var li = $(this);
+        $('#shipper_input').val(li.attr('shipper_abbr'));
+        $('#shipper_info').val(li.attr('shipper_info'));
+        $('#shipper').val(li.attr('shipper_id'));
+        $('#consignee_input').val(li.attr('consignee_abbr'));
+        $('#consignee_info').val(li.attr('consignee_info'));
+        $('#consignee').val(li.attr('consignee_id'));
+        $('#notify_party_input').val(li.attr('notify_abbr'));
+        $('#notify_party_info').val(li.attr('notify_info'));
+        $('#notify_party').val(li.attr('notify_id'));
+        $('#booking_agent_input').val(li.attr('booking_agent_name'));
+        $('#booking_agent').val(li.attr('booking_agent'));
+    });
+    $('#collapseAirInfo').on('show.bs.collapse', function () {
+      $('#collapseAirIcon').removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
+    });
+    $('#collapseAirInfo').on('hide.bs.collapse', function () {
+      $('#collapseAirIcon').removeClass('fa-angle-double-up').addClass('fa-angle-double-down');
+    });
+    
 } );
 });
