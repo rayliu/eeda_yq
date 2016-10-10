4a33550be74ad9a59f1e5c828e2cbf1b6db0c24c
Victor.Tan <victor.tan@eeda123.com>
2016-10-10 09:26:48 +0800

    CustomPlanOrder的model

---
+++ src/models/eeda/cms/CustomPlanOrder.java
@@ -0,0 +1,10 @@
+package models.eeda.cms;
+
+import com.jfinal.plugin.activerecord.Model;
+
+@SuppressWarnings("serial")
+public class CustomPlanOrder extends Model<CustomPlanOrder> {
+	public static final CustomPlanOrder dao = new CustomPlanOrder();
+}
+
+
---
+++ src/models/eeda/cms/CustomPlanOrderItem.java
@@ -0,0 +1,7 @@
+package models.eeda.cms;
+import com.jfinal.plugin.activerecord.Model;
+
+@SuppressWarnings("serial")
+public class CustomPlanOrderItem extends Model<CustomPlanOrderItem> {
+	public static final CustomPlanOrderItem dao = new CustomPlanOrderItem();
+}
