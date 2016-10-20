3e98dbbb25b556f4fef733f71768dbabfc00d3e2
Kevin <kevin.dou@eeda123.com>
2016-10-20 14:45:04 +0800

    rate_contrast model对象

---
+++ src/models/RateContrast.java
@@ -0,0 +1,1 @@
+package models;import com.jfinal.plugin.activerecord.Model;@SuppressWarnings("serial")public class RateContrast extends Model<RateContrast> {	public static final RateContrast dao = new RateContrast();}
