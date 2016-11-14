8cda7a69a3aa23a2626e10421cc4a0a3a7065ad9
Ray Liu <liuyubie@gmail.com>
2016-11-14 17:03:08 +0800

    管理看板: 优化todo的查询

--- WebRoot/js/eeda/index/todo.js
+++ WebRoot/js/eeda/index/todo.js
@@ -3,78 +3,33 @@ define(['jquery'], function ($) {
     var updateTodo = function(bUpdateIndex){
     	var bIndex = bUpdateIndex || false;
         //查询系统单据数量合计
-        $.get('/todo/getPlanOrderTodoCount', function(data){
+        $.get('/todo/getYqTodoList', function(data){
         	if(bIndex){
-            	$("#planOrderTodoCount").empty().text(data);
+            	$("#planOrderTodoCount").empty().text(data.planOrderTodoCount);
+                $("#SOTodoCount").empty().text(data.SOTodoCount);
+                $("#truckOrderTodoCount").empty().text(data.truckOrderTodoCount);
+                $("#SITodoCount").empty().text(data.SITodoCount);
+                $("#MBLTodoCount").empty().text(data.MBLTodoCount);
+                $("#waitCustomTodoCountPlan").empty().text(data.waitCustomTodoCountPlan);
+                $("#waitCustomTodoCount").empty().text(data.waitCustomTodoCount);
+                $("#waitOverseaCustomTodoCount").empty().text(data.waitOverseaCustomTodoCount);
+                $("#TlxOrderTodoCount").empty().text(data.TlxOrderTodoCount);
+                $("#waitBuyInsuranceTodoCount").empty().text(data.waitBuyInsuranceTodoCount);
         	}
-            $(".planOrderWait").empty().text(data);
+            // $(".planOrderWait").empty().text(data);
+            // $(".soWait").empty().text(data);
+            // $(".truckOrderWait").empty().text(data);
+            // $(".siWait").empty().text(data);
+            // $(".mblWait").empty().text(data);
+            // $(".customwaitPlan").empty().text(data);
+            // $(".customWait").empty().text(data);
+            // $(".insuranceWait").empty().text(data);
+            // $(".overseaCustomWait").empty().text(data);
+            // $(".tlxOrderWait").empty().text(data);
         });
-        
-        $.get('/todo/getSOTodoCount', function(data){
-            if(bIndex){
-            	$("#SOTodoCount").empty().text(data);
-            }
-            $(".soWait").empty().text(data);
-        });
-        
-        $.get('/todo/getTruckOrderTodoCount', function(data){
-        	if(bIndex){
-            	$("#truckOrderTodoCount").empty().text(data);
-            }
-        	$(".truckOrderWait").empty().text(data);
 
-        });
-        
-        $.get('/todo/getSITodoCount', function(data){
-            if(bIndex){
-	            $("#SITodoCount").empty().text(data);
-	        }
-            $(".siWait").empty().text(data);
-        });
-        
-        $.get('/todo/getMBLTodoCount', function(data){
-            if(bIndex){
-            	$("#MBLTodoCount").empty().text(data);
-            }
-            $(".mblWait").empty().text(data);
-        });
-        
-        $.get('/todo/getWaitCustomTodoCountPlan', function(data){
-            if(bIndex){
-            	$("#waitCustomTodoCountPlan").empty().text(data);
-            }
-            $(".customwaitPlan").empty().text(data);
-        });
-        
-        $.get('/todo/getWaitCustomTodoCount', function(data){
-            if(bIndex){
-            	$("#waitCustomTodoCount").empty().text(data);
-            }
-            $(".customWait").empty().text(data);
-        });
-        
-        $.get('/todo/getWaitBuyInsuranceTodoCount', function(data){
-            if(bIndex){
-            	$("#waitBuyInsuranceTodoCount").empty().text(data);
-            }
-            $(".insuranceWait").empty().text(data);
-        });
-        
-        $.get('/todo/getWaitOverseaCustomTodoCount', function(data){
-            if(bIndex){
-            	$("#waitOverseaCustomTodoCount").empty().text(data);
-            }
-            $(".overseaCustomWait").empty().text(data);
-        });
-        
-        $.get('/todo/getTlxOrderTodoCount', function(data){
-            if(bIndex){
-            	$("#TlxOrderTodoCount").empty().text(data);
-            }
-            $(".tlxOrderWait").empty().text(data);
-        });
     }
-        
+
     return {
     	updateTodo: updateTodo
     };
--- src/controllers/oms/todo/TodoController.java
+++ src/controllers/oms/todo/TodoController.java
@@ -26,122 +26,76 @@ public class TodoController extends Controller {
 	UserLogin user = LoginUserController.getLoginUser(this);
     long office_id=user.getLong("office_id");
 
-	public void getPlanOrderTodoCount() {
-		String sql = "SELECT count(1) total"
-				    +" FROM plan_order_item poi"
-				    +" left join plan_order po on po.id = poi.order_id"
-				    +" WHERE is_gen_job='N' AND factory_loading_time is not NULL"
-				    +" AND datediff(factory_loading_time, now())<=5 and po.office_id="+office_id;
-		
-		Record planOrder = Db.findFirst(sql);
-		String total = planOrder.getLong("TOTAL").toString();
-		
-		renderText(total);
-	}
-	
-	public void getSOTodoCount() {
-		String sql= " SELECT COUNT(1) total"
-				+ " FROM job_order jo LEFT JOIN job_order_shipment jos ON jo.id=jos.order_id"
-				+ " WHERE jo.type='出口柜货' and jos.SONO is null and jo.transport_type LIKE '%ocean%'"
-				+ " and jo.office_id ="+office_id;
-		Record jobOrder=Db.findFirst(sql);
-		String total = jobOrder.getLong("TOTAL").toString();
-				renderText(total);
-	}
-	
-	public void getTruckOrderTodoCount() {
-		String sql = "SELECT count(1) total"
-				+ " FROM job_order_land_item joli LEFT JOIN job_order jo on jo.id = joli.order_id"
-				+ " WHERE datediff(eta,now() )<=3 and (truckorder_flag != 'Y' OR truckorder_flag is null) "
-				+ "and transport_type LIKE '%land%' and jo.office_id ="+office_id;
-		Record planOrder = Db.findFirst(sql);
-		String total = planOrder.getLong("TOTAL").toString();
-		
-		renderText(total);
-	}
-	
-	public void getSITodoCount() {
-		String sql = "SELECT count(1) total"
-				    +" FROM job_order_shipment jos "
-				    +" LEFT JOIN job_order jo on jo.id = jos.order_id"
-				    +" WHERE TO_DAYS(jos.export_date)=TO_DAYS(now()) and (jos.si_flag != 'Y' or jos.si_flag is null)"
-				    +" and jo.office_id ="+office_id;
-		
-		Record planOrder = Db.findFirst(sql);
-		String total = planOrder.getLong("TOTAL").toString();
-		renderText(total);
-	}
-
-	public void getMBLTodoCount() {
-		String sql = " SELECT COUNT(1) total,jos.si_flag,jos.mbl_flag"
-				+ " FROM job_order_shipment jos"
-				+ " LEFT JOIN job_order jo on jo.id = jos.order_id"
-				+ " WHERE jos.si_flag = 'Y' and (jos.mbl_flag != 'Y' or jos.mbl_flag is null) and jo.office_id ="+office_id;
-		Record jobOrder = Db.findFirst(sql);
-		String total = jobOrder.getLong("TOTAL").toString();
-		renderText(total);
-	}
-
-	public void getWaitCustomTodoCountPlan() {
-		String sql = " select count(1) total from plan_order por"
-				+ " LEFT JOIN plan_order_item poi on poi.order_id = por.id"
-				+ " where "
-				+ " poi.customs_type = '自理报关' and poi.is_gen_job = 'N' and por.office_id="+office_id;
-
-		Record planOrder = Db.findFirst(sql);
-		String total = planOrder.getLong("TOTAL").toString();
-
-		renderText(total);
-	}
-	
-	public void getWaitCustomTodoCount() {
-		String sql = " select count(total_count) total from ( "
-				+ " select count(1) total_count from job_order jor "
-				+ " LEFT JOIN job_order_custom joc on joc.order_id = jor.id"
-				+ " left join job_order_custom_china_self_item jocc on jocc.order_id = jor.id"
-				+ " where jor.transport_type LIKE '%custom%'"
-				+ " and isnull(joc.customs_broker) and isnull(jocc.custom_bank) and jor.office_id="+office_id
-				+ " group by jor.id) A";
-
-		Record planOrder = Db.findFirst(sql);
-		String total = planOrder.getLong("TOTAL").toString();
-		renderText(total);
-	}
-
-	public void getWaitBuyInsuranceTodoCount() {
-		String sql = "SELECT count(1) total "
-				+ " FROM job_order jo LEFT JOIN job_order_insurance joi ON jo.id = joi.order_id "
-				+ " WHERE transport_type LIKE '%insurance%' and joi.insure_no is NULL"
-				+ " and jo.office_id="+office_id;
-
-		Record planOrder = Db.findFirst(sql);
-		String total = planOrder.getLong("TOTAL").toString();
-
-		renderText(total);
-	}
-
-	public void getWaitOverseaCustomTodoCount() {
-		String sql = " SELECT COUNT(1) total"
-				+ " FROM job_order_shipment jos "
-				+ " LEFT JOIN job_order jo on jo.id = jos.order_id"
-				+ " WHERE  (jos.afr_ams_flag !='Y' OR jos.afr_ams_flag is  NULL) and jos.wait_overseaCustom = 'Y' and timediff(now(),jos.etd)<TIME('48:00:00') "
-				+ " and jo.office_id="+office_id;
-		Record jobOrderShipment = Db.findFirst(sql);
-        String total = jobOrderShipment.getLong("TOTAL").toString();
-		renderText(total);
-	}
-
-	public void getTlxOrderTodoCount() {
-		String sql = "SELECT count(1) total"
-	               + " FROM job_order_shipment jos"
-	               + " LEFT JOIN job_order jo on jo.id = jos.order_id"
-				   + " WHERE TO_DAYS(jos.etd)= TO_DAYS(now()) and (jos.in_line_flag != 'Y' or jos.in_line_flag is null)"
-				   + " and jo.office_id="+office_id;
-
-		Record planOrder = Db.findFirst(sql);
-		String total = planOrder.getLong("TOTAL").toString();
-		renderText(total);
-
-	}
+    public void getYqTodoList(){
+         String sql= "select ( "
+            +"SELECT count(1) total FROM plan_order_item poi "
+            +"    left join plan_order po on po.id = poi.order_id "
+            +"    WHERE is_gen_job='N' AND factory_loading_time is not NULL "
+            +"    AND datediff(factory_loading_time, now())<=5 and po.office_id="+office_id 
+            +") planOrderTodoCount, "
+            +"(SELECT COUNT(1) total "
+            +"    FROM job_order jo LEFT JOIN job_order_shipment jos ON jo.id=jos.order_id "
+            +"    WHERE jo.type='出口柜货' and jos.SONO is null and jo.transport_type LIKE '%ocean%' "
+            +"    and jo.office_id ="+office_id 
+            +") SOTodoCount, "
+            +"( "
+            +"    SELECT count(1) total "
+            +"        FROM job_order_land_item joli LEFT JOIN job_order jo on jo.id = joli.order_id "
+            +"        WHERE datediff(eta,now() )<=3 and (truckorder_flag != 'Y' OR truckorder_flag is null) "
+            +"        and transport_type LIKE '%land%' and jo.office_id ="+office_id 
+            +") TruckOrderTodoCount, "
+            +"( "
+            +"    SELECT count(1) total "
+            +"        FROM job_order_shipment jos  "
+            +"        LEFT JOIN job_order jo on jo.id = jos.order_id "
+            +"        WHERE TO_DAYS(jos.export_date)=TO_DAYS(now()) and (jos.si_flag != 'Y' or jos.si_flag is null) "
+            +"        and jo.office_id ="+office_id 
+            +") SITodoCount, "
+            +"( "
+            +"    SELECT COUNT(1) total "
+            +"        FROM job_order_shipment jos "
+            +"        LEFT JOIN job_order jo on jo.id = jos.order_id "
+            +"        WHERE jos.si_flag = 'Y' and (jos.mbl_flag != 'Y' or jos.mbl_flag is null) "
+            + "and jo.office_id ="+office_id 
+            +") MBLTodoCount, "
+            +"( "
+            +"    select count(1) total from plan_order por "
+            +"        LEFT JOIN plan_order_item poi on poi.order_id = por.id "
+            +"        where poi.customs_type = '自理报关' and poi.is_gen_job = 'N' and por.office_id="+office_id 
+            +") WaitCustomTodoCountPlan, "
+            +"( "
+            +"    select count(total_count) total from (  "
+            +"        select count(1) total_count from job_order jor  "
+            +"        LEFT JOIN job_order_custom joc on joc.order_id = jor.id "
+            +"        left join job_order_custom_china_self_item jocc on jocc.order_id = jor.id "
+            +"        where jor.transport_type LIKE '%custom%' "
+            +"        and isnull(joc.customs_broker) and isnull(jocc.custom_bank) and jor.office_id="+office_id 
+            +"        group by jor.id) A "
+            +") WaitCustomTodoCount, "
+            +"( "
+            +"    SELECT count(1) total  "
+            +"        FROM job_order jo LEFT JOIN job_order_insurance joi ON jo.id = joi.order_id  "
+            +"        WHERE transport_type LIKE '%insurance%' and joi.insure_no is NULL "
+            +"        and jo.office_id="+office_id 
+            +") WaitBuyInsuranceTodoCount, "
+            +"( "
+            +"    SELECT COUNT(1) total "
+            +"        FROM job_order_shipment jos  "
+            +"        LEFT JOIN job_order jo on jo.id = jos.order_id "
+            +"        WHERE  (jos.afr_ams_flag !='Y' OR jos.afr_ams_flag is  NULL) and jos.wait_overseaCustom = 'Y'  "
+            +"        and timediff(now(),jos.etd)<TIME('48:00:00')  "
+            +"        and jo.office_id="+office_id 
+            +") WaitOverseaCustomTodoCount, "
+            +"( "
+            +"    SELECT count(1) total "
+            +"       FROM job_order_shipment jos "
+            +"       LEFT JOIN job_order jo on jo.id = jos.order_id "
+            +"       WHERE TO_DAYS(jos.etd)= TO_DAYS(now()) and (jos.in_line_flag != 'Y' or jos.in_line_flag is null) "
+            +"       and jo.office_id="+office_id
+            +") TlxOrderTodoCount; ";
+         Record rec = Db.findFirst(sql);
+         renderJson(rec);
+    }
+    
 	
 }
