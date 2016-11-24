1532bc7cb81c0a66916b315e3fb90c7e7b972ed6
Ray Liu <liuyubie@gmail.com>
2016-11-24 16:40:52 +0800

    common: 构建menu使用cache

--- src/interceptor/EedaMenuInterceptor.java
+++ src/interceptor/EedaMenuInterceptor.java
@@ -1,7 +1,9 @@
 package interceptor;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import models.UserLogin;
 
@@ -17,7 +19,10 @@ import com.jfinal.plugin.activerecord.Record;
 
 public class EedaMenuInterceptor implements Interceptor {
     private Log logger = Log.getLog(EedaMenuInterceptor.class);
-
+    
+    //这个map 作为登录用户的cahce, 免得每次都查很多次DB，消耗connection
+    public static Map<Long, List<Record>> menuCache = null;
+    
     @Override
     public void intercept(Invocation ai) {
         Subject currentUser = SecurityUtils.getSubject();
@@ -33,7 +38,25 @@ public class EedaMenuInterceptor implements Interceptor {
     private void loadMenu(Controller controller) {
         logger.debug("EedaInterceptor loadMenu...");
         UserLogin user = UserLogin.getCurrentUser();
+        Long user_id = user.getLong("id");
         Long office_id = user.getLong("office_id");
+        
+        List<Record> modules = Collections.EMPTY_LIST;
+        if(menuCache == null){
+            menuCache = new HashMap<Long, List<Record>>();
+            modules = buildMenu(user, user_id, office_id);
+        }else{
+            if(menuCache.get(user_id)==null){
+                modules = buildMenu(user, user_id, office_id);
+            }else{
+                modules = menuCache.get(user_id);
+                logger.debug("EedaInterceptor loadMenu from cache...");
+            }
+        }
+        controller.setAttr("modules", modules);
+    }
+
+    private List<Record> buildMenu(UserLogin user, Long user_id, Long office_id) {
         String username = user.getStr("user_name");
         // 查询当前用户菜单
         String sql = "select distinct module.* from eeda_modules module"
@@ -64,8 +87,11 @@ public class EedaMenuInterceptor implements Interceptor {
             module.set("orders", orders);
         }
 
-        if (modules == null)
+        if (modules == null){
             modules = Collections.EMPTY_LIST;
-        controller.setAttr("modules", modules);
+        }else{
+            menuCache.put(user_id, modules);
+        }
+        return modules;
     }
 }
