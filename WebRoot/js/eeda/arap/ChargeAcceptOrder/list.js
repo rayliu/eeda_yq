477f27b8c20f88513345eb045f29876b77d3a016
victor.tan@eeda123.com <kevin.dou@eeda123.com>
2016-11-30 18:01:38 +0800

    复核收款页面调整

--- WebRoot/js/eeda/arap/ChargeAcceptOrder/list.js
+++ WebRoot/js/eeda/arap/ChargeAcceptOrder/list.js
@@ -23,36 +23,23 @@ $(document).ready(function() {
 				{"data":"ORDER_TYPE","class":"order_type"},   
 				{"data":"STATUS"},
 				{"data":"SP_NAME","sClass":"SP_NAME"},
-				{"data":"APP_MSG"},  
-				{"data":"USD"},  
-				{"data":"HKD"},  
-				{"data":"JPY"},  
-				{"data":"CNY"},  
-				{"data":"PAID_USD",
-					"render": function(data, type, full, meta) {
-						if(data)
-							return parseFloat(data).toFixed(2);	
-						else 
-							return '';
-					}
-				},
-				{"data":"PAID_HKD",
-					"render": function(data, type, full, meta) {
-						if(data)
-							return parseFloat(data).toFixed(2);	
-						else 
-							return '';
-					}
-				},
-				{"data":"PAID_JPY",
-					"render": function(data, type, full, meta) {
-						if(data)
-							return parseFloat(data).toFixed(2);	
-						else 
-							return '';
-					}
-				},
+				{"data":"APP_MSG"},
+				{"data":"CNY"},
 				{"data":"PAID_CNY",
+					"render": function(data, type, full, meta) {
+						if(data)
+							return parseFloat(data).toFixed(2);	
+						else 
+							return '';
+					}
+				},
+				{
+					"render": function(data, type, full, meta) {
+						return full.CNY - full.PAID_CNY;	
+					}
+				},
+				{"data":"USD"},
+				{"data":"PAID_USD",
 					"render": function(data, type, full, meta) {
 						if(data)
 							return parseFloat(data).toFixed(2);	
@@ -65,9 +52,13 @@ $(document).ready(function() {
 						return full.USD - full.PAID_USD;	
 					}
 				},
-				{
+				{"data":"JPY"},
+				{"data":"PAID_JPY",
 					"render": function(data, type, full, meta) {
-						return full.HKD - full.PAID_HKD;	
+						if(data)
+							return parseFloat(data).toFixed(2);	
+						else 
+							return '';
 					}
 				},
 				{
@@ -75,11 +66,20 @@ $(document).ready(function() {
 						return full.JPY - full.PAID_JPY;	
 					}
 				},
+				{"data":"HKD"},  
+				{"data":"PAID_HKD",
+					"render": function(data, type, full, meta) {
+						if(data)
+							return parseFloat(data).toFixed(2);	
+						else 
+							return '';
+					}
+				}, 
 				{
 					"render": function(data, type, full, meta) {
-						return full.CNY - full.PAID_CNY;	
+						return full.HKD - full.PAID_HKD;	
 					}
-				},
+				} 	
         ]      
     });
                       
