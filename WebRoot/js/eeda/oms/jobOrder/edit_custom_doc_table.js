37234a360572c71bc4c981fb736560522482f956
Ray Liu <liuyubie@gmail.com>
2016-12-14 13:52:16 +0800

    jobOrder: 文件上传页面提示bug

--- WebRoot/js/eeda/oms/jobOrder/edit_custom_doc_table.js
+++ WebRoot/js/eeda/oms/jobOrder/edit_custom_doc_table.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 			    url: '/jobOrder/uploadCustomDoc?order_id='+order_id,
 			    dataType: 'json',
 		        done: function (e, data) {
-	        	    if(data.result == true){
+	        	    if(data.result.result){
 			    		$.scojs_message('上传成功', $.scojs_message.TYPE_OK);
 			    		//异步刷新显示上传的文档信息
 			    		itemOrder.refleshCustomDocTable(order_id);
