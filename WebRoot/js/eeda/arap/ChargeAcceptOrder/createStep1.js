2795d4582266fac0ce1a03a2646f29685c4b5269
victor.tan@eeda123.com <kevin.dou@eeda123.com>
2016-12-20 10:23:28 +0800

    应收申请单js

---
+++ WebRoot/js/eeda/arap/ChargeAcceptOrder/createStep1.js
@@ -0,0 +1,264 @@
+﻿define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco'], function ($, metisMenu) {
+$(document).ready(function() {
+	document.title = '复核收款| '+document.title;
+    $('#menu_finance').addClass('active').find('ul').addClass('in');
+    
+	var costAccept_table = eeda.dt({
+	    id: 'chargeAccept_table',
+	    autoWidth: true,
+	    paging: true,
+	    serverSide: true, //不打开会出现排序不对 
+	    ajax: "/chargeRequest/OrderList",
+	    columns: [
+				{ 
+				    "render": function(data, type, full, meta) {
+				        return '<input type="checkbox" class="checkBox" >';
+				    }
+				},
+				{"data":"ORDER_NO",
+					"render": function(data, type, full, meta) {
+						return "<a href='/chargeCheckOrder/edit?id="+full.ID+"'  target='_blank'>"+data+"</a>";
+					}
+				},
+				{"data":"ORDER_TYPE","class":"order_type"},   
+				{"data":"STATUS"},
+				{"data":"SP_NAME","sClass":"SP_NAME"},
+				{"data":"APP_MSG"},
+				{"data":"CNY"},
+				{"data":"PAID_CNY",
+					"render": function(data, type, full, meta) {
+						if(data)
+							return parseFloat(data).toFixed(2);	
+						else 
+							return '';
+					}
+				},
+				{
+					"render": function(data, type, full, meta) {
+						return full.CNY - full.PAID_CNY;	
+					}
+				},
+				{"data":"USD"},
+				{"data":"PAID_USD",
+					"render": function(data, type, full, meta) {
+						if(data)
+							return parseFloat(data).toFixed(2);	
+						else 
+							return '';
+					}
+				},
+				{
+					"render": function(data, type, full, meta) {
+						return full.USD - full.PAID_USD;	
+					}
+				},
+				{"data":"JPY"},
+				{"data":"PAID_JPY",
+					"render": function(data, type, full, meta) {
+						if(data)
+							return parseFloat(data).toFixed(2);	
+						else 
+							return '';
+					}
+				},
+				{
+					"render": function(data, type, full, meta) {
+						return full.JPY - full.PAID_JPY;	
+					}
+				},
+				{"data":"HKD"},  
+				{"data":"PAID_HKD",
+					"render": function(data, type, full, meta) {
+						if(data)
+							return parseFloat(data).toFixed(2);	
+						else 
+							return '';
+					}
+				}, 
+				{
+					"render": function(data, type, full, meta) {
+						return full.HKD - full.PAID_HKD;	
+					}
+				} 	
+        ]      
+    });
+                      
+    var application_table = eeda.dt({
+    	id: 'application_table',
+    	autoWidth: true,
+        paging: true,
+        serverSide: true, 
+    	ajax: "/chargeAcceptOrder/applicationList",
+		  columns: [
+		    {"data":"APPLICATION_ORDER_NO",
+            	 "render": function(data, type, full, meta) {
+            			return "<a href='/chargeAcceptOrder/edit?id="+full.ID+"'target='_blank'>"+data+"</a>";
+            	 }
+            },
+            {"data":"ORDER_TYPE"},
+            {"data":"STATUS"},    
+            {"data":"COST_ORDER_NO"},
+            {"data":"APP_USD",
+            	"render": function(data, type, full, meta) {
+            		if(data)
+            			return parseFloat(data).toFixed(2);
+            		else
+            			return '';
+            	}
+            },
+            {"data":"APP_HKD",
+            	"render": function(data, type, full, meta) {
+            		if(data)
+            			return parseFloat(data).toFixed(2);
+            		else
+            			return '';
+            	}
+            },
+            {"data":"APP_CNY",
+            	"render": function(data, type, full, meta) {
+            		if(data)
+            			return parseFloat(data).toFixed(2);
+            		else
+            			return '';
+            	}
+            },
+            {"data":"APP_JPY",
+            	"render": function(data, type, full, meta) {
+            		if(data)
+            			return parseFloat(data).toFixed(2);
+            		else
+            			return '';
+            	}
+            },
+            {"data":"PAYEE_UNIT"},  
+            {"data":"PAYEE_NAME"},
+            {"data":"PAYMENT_METHOD",
+                "render": function(data, type, full, meta) {
+                    if(data == 'cash')
+                        return '现金';
+                    else if(data == 'transfers')
+                        return '转账';
+                    else
+                    	return data;
+                }
+            },
+            {"data":"C_NAME"},
+            {"data":"CREATE_STAMP",
+        		"render":function(data, type, full, meta){
+        			if(data)
+        				return data.substr(0,10);
+        			else 
+        				return '';
+    			}
+    		},
+        	{"data":"CHECK_STAMP",
+        		"render":function(data, type, full, meta){
+        			if(data)
+        				return data.substr(0,10);
+        			else 
+        				return '';
+    			}
+        	},
+        	{"data":"PAY_TIME",
+        		"render":function(data, type, full, meta){
+        			if(data)
+        				return data.substr(0,10);
+        			else 
+        				return '';
+    			}
+        	},
+            {"data":"REMARK"}
+		]      
+    });
+      
+      //查询待申请单
+	$('#searchBtn').click(function(){
+  	    searchData(); 
+	})
+	$('#resetBtn').click(function(e){
+	  	$("#costAcceptForm")[0].reset();
+  	});
+	
+	
+	var searchData=function(){
+		var sp = $("#sp").val(); 
+	    var order_no = $("#orderNo_filter1").val().trim(); 
+	    var status = $('#status_filter1').val();
+	    var orderType = $('#orderType').val();
+	    var start_date = $("#create_stamp_begin_time").val();
+	    var end_date = $("#create_stamp_end_time").val();
+   
+        var url = "/chargeAcceptOrder/list?sp_id="+sp
+      	   +"&order_no="+order_no
+           +"&status="+status
+           +"&order_type="+orderType
+           +"&create_stamp_begin_time="+start_date
+           +"&create_stamp_end_time="+end_date;
+
+        costAccept_table.ajax.url(url).load();
+    };
+      
+      //查询已申请单
+    $('#searchBtn1').click(function(){
+    		searchData1(); 
+    })
+       
+	$('#resetBtn1').click(function(e){
+    	 $("#applicationForm")[0].reset();
+	});
+	var searchData1=function(){
+    	  var sp_id = $('#sp_id').val();
+          var order_no = $("#orderNo").val().trim(); 
+          var applicationOrderNo = $('#applicationOrderNo').val();
+          var status2 = $("#status2").val();
+          var begin_date_begin_time = $("#begin_date_begin_time").val();
+          var begin_date_end_time = $("#begin_date_end_time").val();
+          var check_begin_date_begin_time = $("#check_begin_date_begin_time").val();
+          var check_begin_date_end_time = $("#check_begin_end_begin_time").val();
+          var confirmBegin_date_begin_time = $("#confirmBegin_date_begin_time").val();
+          var confirmBegin_date_end_time = $("#confirmBegin_date_end_time").val();
+   
+          var url = "/chargeAcceptOrder/applicationList?sp_id="+sp_id
+               +"&order_no="+order_no
+               +"&application_order_no="+applicationOrderNo
+               +"&STATUS="+status2
+               +"&create_stamp_begin_time="+begin_date_begin_time
+               +"&create_stamp_end_time="+begin_date_end_time
+               +"&check_stamp_begin_time="+check_begin_date_begin_time
+               +"&check_stamp_end_time="+check_begin_date_end_time
+               +"&pay_time_begin_time="+confirmBegin_date_begin_time
+               +"&pay_time_end_time="+confirmBegin_date_end_time;
+
+          application_table.ajax.url(url).load();
+	};
+    	
+  	//checkbox选中则button可点击
+	
+	$('#chargeAccept_table').on('click','.checkBox',function(){
+		var hava_check = 0;
+		var payee_names = '';
+		var self = this;
+		$('#chargeAccept_table input[type="checkbox"]').each(function(){	
+			var checkbox = $(this).prop('checked');
+			var payee_name = $(this).parent().parent().find('.payee_name').text();
+    		if(checkbox){
+    			if(payee_name != payee_names && payee_names != ''){
+    				$.scojs_message('请选择同一个收款对象', $.scojs_message.TYPE_ERROR);
+    				$(self).attr('checked',false);
+    				return false;
+    			}else{
+    				payee_names = payee_name;
+    				hava_check++;
+    			}
+    		}	
+		})
+		if(hava_check>0){
+			$('#createBtn').attr('disabled',false);
+		}else{
+			$('#createBtn').attr('disabled',true);
+			var payee_names = '';
+		}
+	});
+	  
+});
+});
