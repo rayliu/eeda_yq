9fa5e429f8ae71228444d812272fd04a5dd68249
victor.tan@eeda123.com <kevin.dou@eeda123.com>
2016-12-29 09:32:13 +0800

    应收对账单：增加发票号回显

--- src/controllers/arap/ar/ChargeCheckOrderController.java
+++ src/controllers/arap/ar/ChargeCheckOrderController.java
@@ -310,6 +310,7 @@ public class ChargeCheckOrderController extends Controller {
 			}else{				
 			sql = " select joa.id,joa.sp_id,joa.order_type,joa.total_amount,joa.exchange_rate,joa.currency_total_amount,"
 					+" aco.order_no check_order_no, jo.id job_order_id, jo.order_no,jo.create_stamp,jo.customer_id,jo.volume,jo.net_weight,jo.type," 
+					+ " jo.ref_no ref_no,"
 						+" p.abbr sp_name,p1.abbr customer_name,jos.mbl_no,l.name fnd,joai.destination,"
 						+" ifnull((select rc.new_rate from rate_contrast rc"
 						    +"  where rc.currency_id = joa.currency_id and rc.order_id = aco.id),cast(joa.exchange_rate as char)) new_rate,"
