32261e90ef68fa95361e02e3d62551def60e44b7
Ray Liu <liuyubie@gmail.com>
2017-01-06 12:13:18 +0800

    运营报表修改

--- WebRoot/js/eeda/statusReport/profitReport/list.js
+++ WebRoot/js/eeda/statusReport/profitReport/list.js
@@ -12,96 +12,52 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "ORDER_EXPORT_DATE" },
               { "data": "CUSTOMER_NAME"},
               { "data": "PIECES"}, 
-              { "data": "GROSS_WEIGHT"}, 
-              { "data": "VOLUME"}, 
-              { "data": null,
-            	  "render": function ( data, type, full, meta ) {
-            		  var charge = full.TOTAL_CHARGE;
-            		  var cost = full.TOTAL_COST;
-            		  if(data){
-            			  var total = (charge-cost).toFixed(2);
-            			  if(total>0){
-            				  return "<span style='color:green'>"+total+"</span>"; 
-            			  }else{
-            				  return "<span style='color:red'>"+total+"</span>"; 
-            			  } 
-            		  } else
-            			  return '';
+              { "data": "GROSS_WEIGHT",
+                "render": function ( data, type, full, meta ) {
+                  if(data){
+                    return (data).toFixed(2);
+                  } else {
+                    return '';
                   }
+                }
+              }, 
+              { "data": "VOLUME",
+                "render": function ( data, type, full, meta ) {
+                  if(data){
+                    return (data).toFixed(2);
+                  } else{
+                    return '';
+                  }
+                }
               },
               { "data": null,
-            	  "render": function ( data, type, full, meta ) {
-            		  var charge = full.TOTAL_CHARGE;
-            		  var cost = full.TOTAL_COST;
-            		  if(data)
-            			  if(((charge-cost)/charge*100).toFixed(2)>0){
-            				 return ((charge-cost)/charge*100).toFixed(2)+ '%';
-            			  } else{
-            				  return 0;
-            			  }
-            		  else
-            			  return '';
-                  }
+                "render": function ( data, type, full, meta ) {
+                    return '';
+                }
               },
-              { "data": "OCEAN_COST","visible":false},
-              { "data": "LOAD_COST","visible":false},
-              { "data": "ARI_COST","visible":false},
-              { "data": "CUSTOM_COST","visible":false},
-              { "data": "INSURANCE_COST","visible":false},
-              { "data": "TOTAL_COST"},
-              { "data": "OCEAN_CHARGE","visible":false},
-              { "data": "LOAD_CHARGE","visible":false},
-              { "data": "ARI_CHARGE","visible":false},
-              { "data": "CUSTOM_CHARGE","visible":false},
-              { "data": "INSURANCE_CHARGE","visible":false},
-              { "data": "TOTAL_CHARGE"},
+              { "data": null,
+                "render": function ( data, type, full, meta ) {
+                    return '';
+                }
+              },
+              { "data": null,
+                "render": function ( data, type, full, meta ) {
+                    return '';
+                }
+              }, 
+              { "data": null,
+                "render": function ( data, type, full, meta ) {
+                    return '';
+                }
+              }, 
+              { "data": null,
+                "render": function ( data, type, full, meta ) {
+                    return '';
+                }
+              }
           ]
       });
       
-      $("#cost").click(function(){
-      	var type = $(this).attr("alt");
-      	//0代表隐藏
-      	var table = $('#eeda-table').dataTable();
-      	if(type == 1){
-      		table.fnSetColumnVis(8, false);
-      		table.fnSetColumnVis(9, false);
-      		table.fnSetColumnVis(10, false);
-      		table.fnSetColumnVis(11, false);
-      		table.fnSetColumnVis(7, false);
-      		$(this).attr("alt",0).text(">>");
-      		$('#eeda-table').attr('style','width:100%')
-      	}else{
-      		table.fnSetColumnVis(8, true);
-      		table.fnSetColumnVis(9, true);
-      		table.fnSetColumnVis(10, true);
-      		table.fnSetColumnVis(11, true);
-      		table.fnSetColumnVis(7, true);
-      		$(this).attr("alt",1).text("<<");
-      		$('#eeda-table').attr('style','width:1600px')
-      	}
-      });
-      
-      $("#charge").click(function(){
-        	var type = $(this).attr("alt");
-        	//0代表隐藏
-        	var table = $('#eeda-table').dataTable();
-        	if(type == 1){
-        		table.fnSetColumnVis(13, false);
-        		table.fnSetColumnVis(14, false);
-        		table.fnSetColumnVis(15, false);
-        		table.fnSetColumnVis(16, false);
-        		table.fnSetColumnVis(17, false);
-        		$(this).attr("alt",0).text(">>");
-        	}else{
-        		table.fnSetColumnVis(13, true);
-        		table.fnSetColumnVis(14, true);
-        		table.fnSetColumnVis(15, true);
-        		table.fnSetColumnVis(16, true);
-        		table.fnSetColumnVis(17, true);
-        		$(this).attr("alt",1).text("<<");
-        	}
-        });
-
       
       $('#resetBtn').click(function(e){
           $("#orderForm")[0].reset();
