a776f3d242b3618fdd06072182eea49a7f22274b
qiujiazhen <qiu_jiazhen@163.com>
2017-01-18 15:33:36 +0800

    付款对账单list ,付款确认，完善disabled

--- WebRoot/js/eeda/arap/CostRequest/list.js
+++ WebRoot/js/eeda/arap/CostRequest/list.js
@@ -471,14 +471,14 @@ $(document).ready(function() {
         order.payment_type="cost";
         $.post("/costRequest/confirmOrder", {params:JSON.stringify(order)}, function(data){
                         if(data){
-                            td1.next().next().next().html(data.STATUS);
-                            td1.children().children().children(".confirmBtn").attr('disabled',true);
+                            td1.next().next().html(data.STATUS);
+                            td1.next().children().children(".confirmBtn").attr('disabled',true);
                             $('#application_table .confirmBtn');
                             
                             $.scojs_message('付款成功', $.scojs_message.TYPE_OK);
 
                         }else{
-                            $("#application_table .confirmBtn").attr("disabled", false);
+                            td1.next().children().children(".confirmBtn").attr('disabled',false);
                             $.scojs_message('付款失败', $.scojs_message.TYPE_FALSE);
                         }
                     },'json');
