a17d7e98a97f7a0f65c6d2dbf053a9e74291407a
kevin <kevin.dou@eeda123.com>
2017-01-24 18:06:48 +0800

    工作单费用模板优化

--- WebRoot/js/eeda/oms/jobOrder/edit_arap_detail.js
+++ WebRoot/js/eeda/oms/jobOrder/edit_arap_detail.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
         $('#collapse'+type+'Icon').removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
         var order_type = $('#type').val();
         var customer_id = $('#customer_id').val();
-        if(order_type.trim() == ''){
-        	$.scojs_message('请先选择类型', $.scojs_message.TYPE_ERROR);
+        if(order_type.trim() == '' || customer_id == ''){
+        	$.scojs_message('请先选择类型和客户', $.scojs_message.TYPE_ERROR);
         	return
         }else{
         	$.post('/jobOrder/getArapTemplate', {order_type:order_type,customer_id:customer_id,arap_type:type}, function(data){
@@ -28,25 +28,25 @@ $(document).ready(function() {
         						+'charge_id="'+json_obj[j].CHARGE_ID+'" '
         						+'currency_id="'+json_obj[j].CURRENCY_ID+'" '
         						+'sp_id="'+json_obj[j].SP_ID+'" '
-        						+'unit_id="'+json_obj[j].UNIT_ID+'" '
-        						+'amount="'+json_obj[j].amount+'" '
+        						//+'unit_id="'+json_obj[j].UNIT_ID+'" '
+        						//+'amount="'+json_obj[j].amount+'" '
         						+'charge_name="'+json_obj[j].charge_name+'" '
         						+'charge_name_eng="'+json_obj[j].charge_eng_name+'" '
         						+'currency_name="'+json_obj[j].currency_name+'" '
-        						+'currency_total_amount="'+json_obj[j].currency_total_amount+'" '
+        						//+'currency_total_amount="'+json_obj[j].currency_total_amount+'" '
         						+'exchange_currency_id="'+json_obj[j].exchange_currency_id+'" '
         						+'exchange_currency_name="'+json_obj[j].exchange_currency_name+'" '
         						+'exchange_currency_rate="'+json_obj[j].exchange_currency_rate+'" '
         						+'exchange_rate="'+json_obj[j].exchange_rate+'" '
-        						+'exchange_total_amount="'+json_obj[j].exchange_total_amount+'" '
+        						//+'exchange_total_amount="'+json_obj[j].exchange_total_amount+'" '
         						+'order_type="'+json_obj[j].order_type+'" '
-        						+'price="'+json_obj[j].price+'" '
+        						//+'price="'+json_obj[j].price+'" '
         						+'remark="'+json_obj[j].remark+'" '
-        						+'total_amount="'+json_obj[j].total_amount+'" '
-        						+'type="'+json_obj[j].type+'" '
-        						+'unit_name="'+json_obj[j].unit_name+'" '
+        						//+'total_amount="'+json_obj[j].total_amount+'" '
+        						//+'type="'+json_obj[j].type+'" '
+        						//+'unit_name="'+json_obj[j].unit_name+'" '
         						+'></li>';
-        					li_val += '<span></span>'+json_obj[j].type+' , '+json_obj[j].sp_name+' , '+json_obj[j].charge_name+' , '+json_obj[j].charge_eng_name+'<br/>';
+        					li_val += '<span></span> '+json_obj[j].sp_name+' , '+json_obj[j].charge_name+' , '+json_obj[j].charge_eng_name+'<br/>';
         				}
         				
         				div.append('<ul class="used'+type+'Info" id="'+data[i].ID+'">'
@@ -111,25 +111,25 @@ $(document).ready(function() {
         	var row = $(li[i]);
         	var item={};
         	item.ID='';
-        	item.TYPE=row.attr('type');
+        	//item.TYPE=row.attr('type');
         	item.SP_ID=row.attr('sp_id');
         	item.CHARGE_ID= row.attr('charge_id');
         	item.CHARGE_ENG_ID= row.attr('charge_eng_id');
-        	item.PRICE= row.attr('PRICE');
-        	item.AMOUNT= row.attr('amount');
-        	item.UNIT_ID= row.attr('unit_id');
-        	item.TOTAL_AMOUNT= row.attr('total_amount');
+        	//item.PRICE= row.attr('PRICE');
+        	//item.AMOUNT= row.attr('amount');
+        	//item.UNIT_ID= row.attr('unit_id');
+        	//item.TOTAL_AMOUNT= row.attr('total_amount');
         	item.CURRENCY_ID= row.attr('currency_id');
         	item.EXCHANGE_RATE= row.attr('exchange_rate');
-        	item.CURRENCY_TOTAL_AMOUNT= row.attr('currency_total_amount');
+        	//item.CURRENCY_TOTAL_AMOUNT= row.attr('currency_total_amount');
         	item.EXCHANGE_CURRENCY_ID= row.attr('exchange_currency_id');
         	item.EXCHANGE_CURRENCY_RATE= row.attr('exchange_currency_rate');
-        	item.EXCHANGE_TOTAL_AMOUNT= row.attr('exchange_total_amount');
+        	//item.EXCHANGE_TOTAL_AMOUNT= row.attr('exchange_total_amount');
         	item.REMARK= row.attr('remark');
         	item.SP_NAME=row.attr('sp_name');
         	item.CHARGE_NAME=row.attr('charge_name');
         	item.CHARGE_NAME_ENG=row.attr('charge_name_eng');
-        	item.UNIT_NAME=row.attr('unit_name');
+        	//item.UNIT_NAME=row.attr('unit_name');
         	item.CURRENCY_NAME=row.attr('currency_name');
         	item.EXCHANGE_CURRENCY_ID_NAME=row.attr('exchange_currency_name');
         	item.AUDIT_FLAG='';
