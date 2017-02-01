2e44fa30e266d697a64a0f8d1eaedd80ab2f1e9a
Ray Liu <liuyubie@gmail.com>
2017-02-01 16:20:38 +0800

    planOrder: 查询列表新UI

--- WebRoot/js/eeda/oms/planOrder/list.js
+++ WebRoot/js/eeda/oms/planOrder/list.js
@@ -2,6 +2,9 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
 
     $(document).ready(function() {
     	document.title = '计划订单查询 | '+document.title;
+
+      $("#breadcrumb_li").text('计划订单');
+
     	if(type != ""){
     		$('#menu_order').removeClass('active').find('ul').removeClass('in');
             $('#menu_todo_list').addClass('active').find('ul').addClass('in');
@@ -19,7 +22,8 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
             columns:[
                 { "width": "30px",
                     "render": function ( data, type, full, meta ) {
-                      return '<button type="button" class="delete btn btn-default btn-xs" style="width:50px" disabled>删除</button>';
+                      return '<button type="button" class="btn table_btn btn-default btn-xs" disabled>'+
+                        '<i class="fa fa-trash-o"></i> 删除</button>';
                     }
                 },
                 {"data": "ORDER_NO", 
