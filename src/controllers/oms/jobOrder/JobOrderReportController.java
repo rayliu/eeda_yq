c12614cb0c2ab83d69ab3067ccfb611149064da3
victor.tan@eeda123.com <kevin.dou@eeda123.com>
2017-02-14 12:47:47 +0800

    应收对账单：中山昂励特殊版本  调试

--- src/controllers/oms/jobOrder/JobOrderReportController.java
+++ src/controllers/oms/jobOrder/JobOrderReportController.java
@@ -317,11 +317,11 @@ public class JobOrderReportController extends Controller {
 		String order_id = getPara("order_id");
 		String company_name = getPara("company_name");
 		String fileName = "";
-		if("昂励制冷器材（中山）有限公司".equals(company_name)){
+//		if("昂励制冷器材（中山）有限公司".equals(company_name)){
 			 fileName = "/report/checkOrder/SpecialTotaledReceivableDetails.jasper";
-		}else{
-			 fileName = "/report/checkOrder/TotaledReceivableDetails.jasper";
-		}
+//		}else{
+//			 fileName = "/report/checkOrder/TotaledReceivableDetails.jasper";
+//		}
 		String outFileName = "/download/应收对账单(合计版)PDF";
 		HashMap<String, Object> hm = new HashMap<String, Object>();
 		hm.put("order_id", order_id);
