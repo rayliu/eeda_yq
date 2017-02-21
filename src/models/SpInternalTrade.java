0498d7304e069283fe1474db14d15f05d7ee1ea0
kevin <kevin.dou@eeda123.com>
2017-02-21 17:55:31 +0800

    供应商合同内贸保存

---
+++ src/models/SpInternalTrade.java
@@ -0,0 +1,1 @@
+package models;import com.jfinal.plugin.activerecord.Model;@SuppressWarnings("serial")public class SpInternalTrade extends Model<SpInternalTrade> {	public static final SpInternalTrade dao = new SpInternalTrade();}
