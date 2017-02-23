94f69a643a33b98ea290d65f51d6b0382867e446
qiujiazhen <qiu_jiazhen@163.com>
2017-02-23 14:21:48 +0800

    no message

--- WebRoot/js/eeda/oms/jobOrder/edit_land_charge_table.js
+++ WebRoot/js/eeda/oms/jobOrder/edit_land_charge_table.js
@@ -3,19 +3,232 @@ $(document).ready(function() {
     
 
 	var deletedTableIds=[];
-	//费用明细
-    // $('#landcollapseChargeInfo').on('show.bs.collapse',function(){
-    //     var thisType=$(this).attr('id');
-    //     var type='charge';
-    //     var div=$('#land'+type+'Div').empty();
-    //     $('#collapse'+type+'Icon').removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
-    //     if(order_type.trim() == '' || customer_id == ''){
-    //         $.scojs_message('请先选择类型和客户', $.scojs_message.TYPE_ERROR);
-    //         return
-    //     }else{
+	// 常用费用
+    $('#landcollapseChargeInfo').on('show.bs.collapse',function(){
+        var thisType=$(this).attr('id');
+        var type='Charge';
+        var div=$('#land'+type+'Div').empty();
+        $('#collapse'+type+'Icon').removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
+        var order_type = $('#type').val();
+        var customer_id = $('#customer_id').val();
+        if(order_type.trim() == '' || customer_id == ''){
+            $.scojs_message('请先选择类型和客户', $.scojs_message.TYPE_ERROR);
+            return
+        }else{
+            $.post('/jobOrder/getLandArapTemplate', {order_type:order_type,customer_id:customer_id,arap_type:type}, function(data){
+                if(data){
+                    for(var i = 0;i<data.length;i++){
+                        var json_obj = JSON.parse(data[i].JSON_VALUE);
+                        var li = '';
+                        var li_val = '';
+                        for(var j = 0;j<json_obj.length;j++){
+                            li +='<li '
+                                +' sp_name="'+json_obj[j].sp_name+'" '
+                                +'charge_eng_id="'+json_obj[j].CHARGE_ENG_ID+'" '
+                                +'charge_id="'+json_obj[j].CHARGE_ID+'" '
+                                +'currency_id="'+json_obj[j].CURRENCY_ID+'" '
+                                +'sp_id="'+json_obj[j].SP_ID+'" '
+                                +'unit_id="'+json_obj[j].UNIT_ID+'" '
+                                +'amount="'+json_obj[j].amount+'" '
+                                +'charge_name="'+json_obj[j].charge_name+'" '
+                                +'charge_name_eng="'+json_obj[j].charge_eng_name+'" '
+                                +'currency_name="'+json_obj[j].currency_name+'" '
+                                +'currency_total_amount="'+json_obj[j].currency_total_amount+'" '
+                                +'exchange_currency_id="'+json_obj[j].exchange_currency_id+'" '
+                                +'exchange_currency_name="'+json_obj[j].exchange_currency_name+'" '
+                                +'exchange_currency_rate="'+json_obj[j].exchange_currency_rate+'" '
+                                +'exchange_rate="'+json_obj[j].exchange_rate+'" '
+                                +'exchange_total_amount="'+json_obj[j].exchange_total_amount+'" '
+                                +'order_type="'+json_obj[j].order_type+'" '
+                                +'price="'+json_obj[j].price+'" '
+                                +'remark="'+json_obj[j].remark+'" '
+                                +'total_amount="'+json_obj[j].total_amount+'" '
+                                +'type="'+json_obj[j].type+'" '
+                                +'unit_name="'+json_obj[j].unit_name+'" '
+                                +'></li>';
+                            li_val += '<span></span> '+json_obj[j].sp_name+' , '+json_obj[j].charge_name+' , '+json_obj[j].charge_eng_name+'<br/>';
+                        }
+                        
+                        div.append('<ul class="used'+type+'Info" id="'+data[i].ID+'">'
+                                +li
+                                +'<div class="radio">'
+                                +'  <a class="delete'+type+'Template" style="margin-right: 10px;padding-top: 5px;float: left;">删除</a>'
+                                +'  <div class="select'+type+'Template" style="margin-left: 60px;padding-top: 0px;">'
+                                +'      <input type="radio" value="1" name="used'+type+'Info">'
+                                +       li_val
+                                +'  </div>'
+                                +'</div><hr/>'
+                                +'</ul>');
+                        
+                    }
+                }
+            });
+        }
+    });
+    
+    $('#landChargeDiv,#landCostDiv').on('click','.deleteChargeTemplate,.deleteCostTemplate',function(){
+        $(this).attr('disabled',true);
+        var ul=$(this).parent().parent();
+        var id=ul.attr('id');
+        $.post('/jobOrder/deleteLandArapTemplate',{id:id},function(data){
+            if(data){
+                $.scojs_message('删除成功',$.scojs_message.TYPE_OK);
+                $(this).attr('disabled',false);
+                ul.css("display","none");
+            }
+        },'json').fail(function(){
+            $(this).attr('disabled',false);
+            $.scojs_message('删除失败',$.scojs_message.TYPE_ERROR);
+        })
+    });
 
-    //     }
-    // });
+    //回显
+    $('#landChargeDiv,#landCostDiv').on('click','.selectChargeTemplate,.selectChargeTemplate',function(){
+        $(this).parent().find('[type=radio]').prop('checked',true);
+        var thisType=$(this).attr('class');
+        var type='Charge';
+        var table='charge_table';
+        if('selectChargeTemplate'!=thisType){
+            type='Cost';
+            table='cost_table';
+        }
+
+        var li=$(this).parent().parent().find('li');
+        var dataTable=$('#land_'+table).DataTable();
+
+        for (var i = 0; i < li.length; i++) {
+            var row=$(li[i]);
+            var item={};
+            item.ID='';
+            item.TYPE=row.attr('type');
+            item.SP_ID=row.attr('sp_id');
+            item.CHARGE_ID= row.attr('charge_id');
+            item.CHARGE_ENG_ID= row.attr('charge_eng_id');
+            item.PRICE= row.attr('PRICE');
+            item.AMOUNT= row.attr('amount');
+            item.UNIT_ID= row.attr('unit_id');
+            item.TOTAL_AMOUNT= row.attr('total_amount');
+            item.CURRENCY_ID= row.attr('currency_id');
+            item.EXCHANGE_RATE= row.attr('exchange_rate');
+            item.CURRENCY_TOTAL_AMOUNT= row.attr('currency_total_amount');
+            item.EXCHANGE_CURRENCY_ID= row.attr('exchange_currency_id');
+            item.EXCHANGE_CURRENCY_RATE= row.attr('exchange_currency_rate');
+            item.EXCHANGE_TOTAL_AMOUNT= row.attr('exchange_total_amount');
+            item.REMARK= row.attr('remark');
+            item.SP_NAME=row.attr('sp_name');
+            item.CHARGE_NAME=row.attr('charge_name');
+            item.CHARGE_NAME_ENG=row.attr('charge_name_eng');
+            item.UNIT_NAME=row.attr('unit_name');
+            item.CURRENCY_NAME=row.attr('currency_name');
+            item.EXCHANGE_CURRENCY_ID_NAME=row.attr('exchange_currency_name');
+            item.AUDIT_FLAG='';
+            dataTable.row.add(item).draw();
+        };
+    });
+
+    itemOrder.buildLandChargeTemplate=function(){
+        var cargo_table_rows = $("#land_charge_table tr");
+        var cargo_items_array=[];
+        for(var index=0; index<cargo_table_rows.length; index++){
+            if(index==0)
+                continue;
+
+            var row = cargo_table_rows[index];
+            var empty = $(row).find('.dataTables_empty').text();
+            if(empty)
+                continue;
+            
+            var id = $(row).attr('id');
+            if(!id){
+                id='';
+            }
+            
+            var item={}
+            item.order_type = "charge";//应收
+            for(var i = 1; i < row.childNodes.length; i++){
+                if($(row.childNodes[i]).find('.notsave').size()==0){
+                    var el = $(row.childNodes[i]).find('input,select');
+                    var name = el.attr('name'); 
+                    
+                    if(el && name){
+                        if(name=='exchange_currency_id'&&el.val()==''){
+                            el.val(el.parent().parent().parent().find('[name=CURRENCY_ID]').val());
+                        }
+                        if(name=='exchange_currency_rate'&&el.val()==''){
+                            el.val(1);
+                        }
+                        if(name=='exchange_total_amount'&&el.val()==''){
+                            el.val(el.parent().parent().find('[name=total_amount]').val());
+                        }
+                        
+                        if(name.toLowerCase()!='unit_id'){
+                            var value = el.val();//元素的值
+                            item[name] = value;
+                        }
+                        
+
+                        if(name.toLowerCase().indexOf("_id")>=0){
+                            var id_value = $(row.childNodes[i]).find('[name='+name+'_input]').val();
+                            var abbr = name.toLowerCase().replace('id','name');
+                            if(abbr!='unit_name'){
+                                item[abbr] = id_value;
+                            }
+                        }
+                    }
+                }
+            }
+            cargo_items_array.push(item);
+        }
+        return cargo_items_array;
+    };
+
+    itemOrder.buildLandAllChargeTemplate=function(){
+        var cargo_table_rows = $("#land_charge_table tr");
+        var cargo_items_array=[];
+        for(var index=0; index<cargo_table_rows.length; index++){
+            if(index==0)
+                continue;
+
+            var row = cargo_table_rows[index];
+            var empty = $(row).find('.dataTables_empty').text();
+            if(empty)
+                continue;
+            
+            var id = $(row).attr('id');
+            if(!id){
+                id='';
+            }
+            
+            var item={}
+            item.order_type = "charge";//应收
+            for(var i = 1; i < row.childNodes.length; i++){
+                    var el = $(row.childNodes[i]).find('input,select');
+                    var name = el.attr('name'); 
+                    
+                    if(el && name){
+                        if(name=='exchange_currency_id'&&el.val()==''){
+                            el.val(el.parent().parent().parent().find('[name=CURRENCY_ID]').val());
+                        }
+                        if(name=='exchange_currency_rate'&&el.val()==''){
+                            el.val(1);
+                        }
+                        if(name=='exchange_total_amount'&&el.val()==''){
+                            el.val(el.parent().parent().find('[name=total_amount]').val());
+                        }
+                        var value = el.val();//元素的值
+                        item[name] = value;
+
+                        if(name.toLowerCase().indexOf("_id")>=0){
+                            var id_value = $(row.childNodes[i]).find('[name='+name+'_input]').val();
+                            var abbr = name.toLowerCase().replace('id','name');
+                            item[abbr] = id_value;
+                        }
+                    }
+            }
+            cargo_items_array.push(item);
+        }
+        return cargo_items_array;
+    };
 
     //删除一行
     $("#land_charge_table").on('click', '.delete', function(e){
@@ -124,7 +337,7 @@ $(document).ready(function() {
             { "data": "TYPE", "width": "80px", 
                 "render": function ( data, type, full, meta ) {
                 	if(full.AUDIT_FLAG == 'Y'){
-                		var str = '<select name="type" class="form-control search-control" style="width:100px" disabled>'
+                		var str = '<select name="type" class="form-control search-control notsave" style="width:100px" disabled>'
 	                        +'<option value="陆运" '+(data=='陆运' ? 'selected':'')+'>陆运</option>'
 	                        +'<option value="海运" '+(data=='海运' ? 'selected':'')+'>海运</option>'
 	                        +'<option value="空运" '+(data=='空运' ? 'selected':'')+'>空运</option>'
@@ -133,7 +346,7 @@ $(document).ready(function() {
 	                        +'</select>';
 	                	return str;
                 	}else{
-                    var str = '<select name="type" class="form-control search-control" style="width:100px">'
+                    var str = '<select name="type" class="form-control search-control notsave" style="width:100px">'
 	                    	+'<option value="陆运" '+(data=='陆运' ? 'selected':'')+'>陆运</option>'
 	                        +'<option value="海运" '+(data=='海运' ? 'selected':'')+'>海运</option>'
 	                        +'<option value="空运" '+(data=='空运' ? 'selected':'')+'>空运</option>'
@@ -227,9 +440,9 @@ $(document).ready(function() {
                     else
                     	str = '';
                 	if(full.AUDIT_FLAG == 'Y'){
-                    	return '<input type="text" name="price" style="width:120px" value="'+str+'" class="form-control" disabled />';
+                    	return '<input type="text" name="price" style="width:120px" value="'+str+'" class="form-control notsave" disabled />';
                      }else{
-	                    return '<input type="text" name="price" style="width:120px" value="'+str+'" class="form-control" />';
+	                    return '<input type="text" name="price" style="width:120px" value="'+str+'" class="form-control notsave" />';
 	                 }
                   }
             },
@@ -238,9 +451,9 @@ $(document).ready(function() {
                 	if(!data)
                         data='';
                 	if(full.AUDIT_FLAG == 'Y'){
-                        return '<input type="text" name="amount" style="width:80px" value="'+data+'" class="form-control " disabled/>';
+                        return '<input type="text" name="amount" style="width:80px" value="'+data+'" class="form-control notsave" disabled/>';
                      }else{
-	                    return '<input type="text" name="amount" style="width:80px" value="'+data+'" class="form-control"/>';
+	                    return '<input type="text" name="amount" style="width:80px" value="'+data+'" class="form-control notsave"/>';
 	                }
                 }
             },
@@ -280,7 +493,7 @@ $(document).ready(function() {
                         var str =  parseFloat(data).toFixed(2);
                     else
                     	str = '';
-	                return '<input type="text" name="total_amount" style="width:150px" value="'+str+'" class="form-control" disabled/>';
+	                return '<input type="text" name="total_amount" style="width:150px" value="'+str+'" class="form-control notsave" disabled/>';
                 }
             },
             { "data": "CURRENCY_ID", "width": "60px","className":"currency_name",
@@ -332,7 +545,7 @@ $(document).ready(function() {
                         var str =  parseFloat(data).toFixed(2);
                     else
                     	str = '';
-	                return '<input type="text" name="currency_total_amount" style="width:150px" value="'+str+'" class="form-control" disabled/>';
+	                return '<input type="text" name="currency_total_amount" style="width:150px" value="'+str+'" class="form-control notsave" disabled/>';
                 }
             },
             { "data": "EXCHANGE_CURRENCY_ID", "width":"80px","className":"cny_to_other",
@@ -384,7 +597,7 @@ $(document).ready(function() {
             			var str =  parseFloat(data).toFixed(2);
             		else
             			str = '';
-            		return '<input type="text" name="exchange_total_amount" style="width:150px" value="'+str+'" class="form-control" disabled />';
+            		return '<input type="text" name="exchange_total_amount" style="width:150px" value="'+str+'" class="form-control notsave" disabled />';
             	}
             },
             { "data": "REMARK","width": "180px",
@@ -458,6 +671,14 @@ $(document).ready(function() {
     	order.order_id = order_id;
     	order.land_item_id = $('#land_item_id').val();
     	order.land_charge_item = itemOrder.buildLandChargeDetail();
+
+        //陆运常用费用
+        order.customer_id = $('#customer_id').val();
+        order.type = $('#type').val();
+        order.land_charge_template = itemOrder.buildLandChargeTemplate();
+        // order.land_cost_template = itemOrder.buildLandCostTemplate();
+        order.land_allCharge_template = itemOrder.buildLandAllChargeTemplate();
+        // order.land_allCost_template = itemOrder.buildLandAllCostTemplate();
     	$.post('/jobOrder/saveLandCharge', {params:JSON.stringify(order)}, function(data){
     		 
     		 var url = "/jobOrder/tableListOfLandCharge?order_id="+order_id+"&land_item_id="+land_item_id;
@@ -474,6 +695,7 @@ $(document).ready(function() {
     
     $('#land_table').on('click','.land_charge',function(){
     		var land_item_id = $(this).parent().parent().attr('id');
+            $('#landcollapseChargeInfo').removeClass('in');
     		$('#land_item_id').val(land_item_id);
     		var order_id = $('#order_id').val();
     		var url = "/jobOrder/tableListOfLandCharge?order_id="+order_id+"&land_item_id="+land_item_id;
@@ -495,6 +717,9 @@ $(document).ready(function() {
             });
     })
     
+    $('input[type="checkbox"]').prop("checked",function(i,val){
+        return !val;
+    })
   //数量和单价自动补零
     $('#land_charge_table').on('blur','[name=price],[name=amount]',function(){
     	var amount = $(this).val();
