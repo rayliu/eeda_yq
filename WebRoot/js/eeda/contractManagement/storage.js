3500023639f9f05a8aa4c76895d1821f3ec43318
kevin <kevin.dou@eeda123.com>
2017-02-24 14:18:44 +0800

    供应商仓储模块开发

--- WebRoot/js/eeda/contractManagement/storage.js
+++ WebRoot/js/eeda/contractManagement/storage.js
@@ -1,44 +1,119 @@
-define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco'], function ($, metisMenu) { 
+define(['jquery', 'metisMenu', 'template', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco', 'jq_blockui'], function ($, metisMenu, template) { 
+	$(document).ready(function() {
+	    var deletedTableIds=[];
 
-  $(document).ready(function() {
-  		 var dataTable = eeda.dt({
+	    //删除一行
+	    $("#storage_table").on('click', '.delete', function(e){
+	        e.preventDefault();
+	        var tr = $(this).parent().parent();
+	        deletedTableIds.push(tr.attr('id'));
+	        
+	        cargoTable.row(tr).remove().draw();
+	    }); 
+	    
+	    itemOrder.buildStorageDetail=function(){
+	    	var cargo_table_rows = $("#storage_table tr");
+	        var cargo_items_array=[];
+	        for(var index=0; index<cargo_table_rows.length; index++){
+	            if(index==0)
+	                continue;
+
+	            var row = cargo_table_rows[index];
+	            var empty = $(row).find('.dataTables_empty').text();
+	            if(empty)
+	            	continue;
+	            
+	            var id = $(row).attr('id');
+	            if(!id){
+	                id='';
+	            }
+	            
+	            var item={}
+	            
+	            item.id = id;
+	           
+	            for(var i = 1; i < row.childNodes.length; i++){
+	            	var name = $(row.childNodes[i]).find('input,select').attr('name');
+	            	var value = $(row.childNodes[i]).find('input,select').val();
+	            	if(name){
+	            		item[name] = value;
+	            	}
+	            }
+	            item.action = id.length > 0?'UPDATE':'CREATE';
+	            cargo_items_array.push(item);
+	        }
+
+	        //add deleted items
+	        for(var index=0; index<deletedTableIds.length; index++){
+	            var id = deletedTableIds[index];
+	            var item={
+	                id: id,
+	                action: 'DELETE'
+	            };
+	            cargo_items_array.push(item);
+	        }
+	        deletedTableIds = [];
+	        return cargo_items_array;
+	    };
+	    
+	    var bindFieldEvent=function(){
+	    	eeda.bindTableField('cargo_table','UNIT_ID','/serviceProvider/searchUnit','');
+	    	eeda.bindTableField('cargo_table','POR','/location/searchPort','');
+	    	eeda.bindTableField('cargo_table','POL','/location/searchPort','');
+	    	eeda.bindTableField('cargo_table','POD','/location/searchPort','');
+	    	eeda.bindTableField('cargo_table','CARRIER','/serviceProvider/searchCarrier','');
+	    };
+
+	    //------------事件处理
+	    var cargoTable = eeda.dt({
             id: 'storage_table',
-            paging: true,
-            serverSide: true, //不打开会出现排序不对
-            // ajax: "/serviceProvider/list",
+            autoWidth: false,
+            "drawCallback": function( settings ) {
+		        bindFieldEvent();
+		        $.unblockUI();
+		    },
             columns:[
-                { "data": "COMPANY_NAME","width": "10%",
-                    "render": function ( data, type, full, meta ) {
-                        if(Provider.isUpdate){
-                             return "<a href='/supplierContract/edit?id="+full.ID+"' target='_blank'>" + data+ "</a>";
-                        }else{
-                             return data;
-                        }
-                    }
-                },
-                { "data": "ABBR", "width": "10%"}, 
-                { "data": "CODE", "width": "10%"},
-                { "data": "CODE", "width": "10%"}
-            ]
-        });
+				{ "width": "30px",
+				    "render": function ( data, type, full, meta ) {
+				    	return '<button type="button" class="delete btn table_btn delete_btn btn-xs">'+
+				        '<i class="fa fa-trash-o"></i> 删除</button>';
+				    }
+				},
+	            { "data": "REGION", 
+	            	"render": function ( data, type, full, meta ) {
+	            		if(!data)
+	            			data='';
+	            		return '<input type="text" name="region"  value="'+data+'" class="form-control" />';
+	            	}
+            	},
+            	{ "data": "INFRASTRUCTURE", 
+	            	"render": function ( data, type, full, meta ) {
+	            		if(!data)
+	            			data='';
+	            		return '<input type="text" name="infrastructure"  value="'+data+'" class="form-control" />';
+	            	}
+            	},
+            	{ "data": "REMARK", 
+	            	"render": function ( data, type, full, meta ) {
+	            		if(!data)
+	            			data='';
+	            		return '<input type="text" name="remark"  value="'+data+'" class="form-control" />';
+	            	}
+            	}
+	        ]
+	    });
 
-  		 //添加
-  		 $('#addstorage_item').click(function(){
-  				$('#storage_table tbody').append('<tr>'+'<td><button type="button" class="btn table_btn delete_btn btn-xs">'
-        		                  +'<i class="fa fa-trash-o"></i> 删除</button></td>'
-        		+'<td><textarea class="remark" rows="3" cols="20"></textarea></td>'
-            +'<td><textarea class="remark" rows="3" cols="20"></textarea></td>'
-  					+'<td><textarea class="remark" rows="3" cols="20"></textarea></td>'
-  					+'</tr>');
-  		 });
-  		 //删除
-  		 $("#storage_table").on('click', '.delete_btn', function(e){
-  				$(this).parent().parent().remove();
-  				// var id= $(this).val();
-  				// $.post('/serviceProvider/markCustormerDelete',{id:id},function(data){
-  				// 		freshItemTable();
-  				// })
-  	});
-
-  });
+	    $('#add_storage_item').on('click', function(){
+	        var item={};
+	        cargoTable.row.add(item).draw(true);
+	    });
+	    
+	    //刷新明细表
+	    itemOrder.refleshStorageTable = function(order_id){
+	    	var url = "/supplierContract/tableList?order_id="+order_id+"&type=storage";
+	    	cargoTable.ajax.url(url).load();
+	    }
+	    
+	   
+	});
 });
