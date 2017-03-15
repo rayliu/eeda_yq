5aac8008c5e6b61f67a28e8436dca83c9243cc97
victor.tan@eeda123.com <kevin.dou@eeda123.com>
2017-03-15 11:54:07 +0800

    tms 工作单 提柜码头，还柜码头 自动添加到陆运表一二行

--- WebRoot/js/eeda/tms/transJobOrder/edit.js
+++ WebRoot/js/eeda/tms/transJobOrder/edit.js
@@ -204,6 +204,12 @@ $(document).ready(function() {
         });
     	
 	});
-  
+    
+    $('#take_wharf_list').on('mousedown', '.fromLocationItem', function(e){
+        $('#land_table tr:eq(1)').find('[name=take_address]').val($(this).text());
+    });
+    $('#back_wharf_list').on('mousedown', '.fromLocationItem', function(e){
+        $('#land_table tr:eq(2)').find('[name=delivery_address]').val($(this).text());
+    });
 });
 });
