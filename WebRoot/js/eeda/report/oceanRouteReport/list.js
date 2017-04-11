59d9362de1bb56810556397daf265441f66ef365
victor.tan@eeda123.com <kevin.dou@eeda123.com>
2017-04-11 17:48:58 +0800

    海运报表汇总

--- WebRoot/js/eeda/report/oceanRouteReport/list.js
+++ WebRoot/js/eeda/report/oceanRouteReport/list.js
@@ -1,6 +1,8 @@
 define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco'], function ($, metisMenu) {
   $(document).ready(function() {
-  	document.title = '运营报表  | '+document.title;
+  	document.title = '海运路线运营报表  | '+document.title;
+  	$('#breadcrumb_li').text('海运路线运营报表');
+  	
   	
   	//datatable, 动态处理
       var dataTable = eeda.dt({
@@ -16,7 +18,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "PIECES", 
                 "render": function ( data, type, full, meta ) {
                   if(data){
-                    return "<span class='pull-right'>"+data + "</span>";
+                    return "<span class=''>"+eeda.numFormat(data,3) + "</span>";
                   } else {
                     return '';
                   }
@@ -25,7 +27,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "GROSS_WEIGHT",
                 "render": function ( data, type, full, meta ) {
                   if(data){
-                    return "<span class='pull-right'>"+(data).toFixed(2) + "</span>";
+                    return "<span class=''>"+eeda.numFormat(data.toFixed(2),3) + "</span>";
                   } else {
                     return '';
                   }
@@ -34,7 +36,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "VOLUME", 
                 "render": function ( data, type, full, meta ) {
                   if(data){
-                    return "<span class='pull-right'>"+(data).toFixed(2) + "</span>";
+                    return "<span class=''>"+eeda.numFormat(data.toFixed(2),3) + "</span>";
                   } else{
                     return '';
                   }
@@ -43,7 +45,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "OCEAN_FCL_TEU",
                 "render": function ( data, type, full, meta ) {
                     if(data){
-                      return "<span class='pull-right'>"+(data).toFixed(0) + "</span>";
+                      return "<span class=''>"+eeda.numFormat(data.toFixed(0),3) + "</span>";
                     } else{
                       return '';
                   }
@@ -52,7 +54,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "OCEAN_LCL_CBM",
                 "render": function ( data, type, full, meta ) {
                     if(data){
-                      return "<span class='pull-right'>"+(data).toFixed(2) + "</span>";
+                      return "<span class=''>"+eeda.numFormat(data.toFixed(2),3) + "</span>";
                     } else{
                       return '';
                     }
@@ -86,7 +88,31 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
         	  begin_date = $("#month_begin_time").val();
               end_date = $("#month_end_time").val();
           }
-         
+          
+          
+        //合计字段
+          $.post('oceanRouteReport/listTotal',{
+        	  customer_id:customer_id,
+        	  date_type:date_type,
+        	  end_date:end_date,
+        	  end_date:end_date
+          },function(data){
+        	  var pieces_total = parseFloat(data.PIECES_TOTAL);
+        	  var gross_weight_total = parseFloat(data.GROSS_WEIGHT_TOTAL);
+        	  var volume_total = parseFloat(data.VOLUME_TOTAL);
+        	  var ocean_fcl_teu_total = parseFloat(data.OCEAN_FCL_TEU_TOTAL);
+        	 
+        	  var ocean_lcl_cbm_total = parseFloat(data.OCEAN_LCL_CBM_TOTAL);
+
+        	  var total=parseFloat(data.TOTAL);
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(0).html('共'+total+'项汇总：');
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(3).html(eeda.numFormat(pieces_total,3));
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(4).html(eeda.numFormat(gross_weight_total,3));
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(5).html(eeda.numFormat(volume_total,3));
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(6).html(eeda.numFormat(ocean_fcl_teu_total,3));
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(7).html(eeda.numFormat(ocean_lcl_cbm_total,3));
+          });
+  
           
           //增加出口日期查询
           var url = "/oceanRouteReport/list?order_no="+order_no
@@ -116,6 +142,6 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
     	  }
       })
  
-
+      searchData();
   });
 });
--- src/controllers/report/OceanRouteReportController.java
+++ src/controllers/report/OceanRouteReportController.java
@@ -21,7 +21,6 @@ import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
 
 import controllers.profile.LoginUserController;
-import controllers.util.DbUtils;
 
 @RequiresAuthentication
 @Before(SetAttrLoginUserInterceptor.class)
@@ -34,7 +33,7 @@ public class OceanRouteReportController extends Controller {
     	render("eeda/report/oceanRouteReport/list.html");
     }
    
-    public void list() {
+    public long list() {
         String sLimit = "";
         String pageIndex = getPara("draw");
         if (getPara("start") != null && getPara("length") != null) {
@@ -151,7 +150,7 @@ public class OceanRouteReportController extends Controller {
         String sqlTotal = "select count(1) total from ("+sql+") B";
         Record rec = Db.findFirst(sqlTotal);
         logger.debug("total records:" + rec.getLong("total"));
-        
+        long total=rec.getLong("total");
         List<Record> orderList = Db.find(sql +sLimit);
         Map orderListMap = new HashMap();
         orderListMap.put("draw", pageIndex);
@@ -159,7 +158,124 @@ public class OceanRouteReportController extends Controller {
         orderListMap.put("recordsFiltered", rec.getLong("total"));
         orderListMap.put("data", orderList);
         renderJson(orderListMap); 
+        return total;
     }
     
+    public void listTotal(){
+    	UserLogin user = LoginUserController.getLoginUser(this);
+        long office_id=user.getLong("office_id");
+        String customer_id = getPara("customer_id");
+        String begin_date = getPara("begin_date");
+        String end_date = getPara("end_date");
+        String date_type = getPara("date_type");
+        
+        String condition = "";
+        String group_condition="";
+        if(StringUtils.isNotEmpty(customer_id)){
+            condition += " and jo.customer_id = "+customer_id;
+        }
+        
+        if("year".equals(date_type)){
+            if(StringUtils.isNotEmpty(begin_date)){
+                begin_date = begin_date+"-01-01 00:00:00";
+            }else{
+                begin_date = "1970-01-01 00:00:00";
+            }
+            
+            if(StringUtils.isNotEmpty(end_date)){
+                end_date = end_date+"-12-31 23:59:59";
+            }else{
+                end_date = "2037-12-31 23:59:59";
+            }
+            
+            group_condition = " cast(year(jo.order_export_date) as char)";
+        }else {
+            if(StringUtils.isNotEmpty(begin_date)){
+                begin_date = begin_date+"-01 00:00:00";
+            }else{
+                begin_date = "1970-01-01 00:00:00";
+            }
+            
+            if(StringUtils.isNotEmpty(end_date)){
+                end_date = end_date+"-31 23:59:59";
+            }else{
+                end_date = "2037-12-31 23:59:59";
+            }
+            
+            if("season".equals(date_type)){
+                group_condition = " (case"
+                        + " when cast(month(jo.order_export_date) as char)"
+                        + " between 1 and 3 "
+                        + " then cast(CONCAT(year(jo.order_export_date),'-','第一季') as char)"
+                        + " when cast(month(jo.order_export_date) as char)"
+                        + " between 4 and 6 "
+                        + " then cast(CONCAT(year(jo.order_export_date),'-','第二季') as char)"
+                        + " when cast(month(jo.order_export_date) as char)"
+                        + " between 7 and 9 "
+                        + " then cast(CONCAT(year(jo.order_export_date),'-','第三季') as char)"
+                        + " when cast(month(jo.order_export_date) as char)"
+                        + " between 10 and 12 "
+                        + " then cast(CONCAT(year(jo.order_export_date),'-','第四季') as char)"
+                        + " end)";
+            }else{
+                group_condition = " cast(CONCAT(year(jo.order_export_date),'-',month(jo.order_export_date)) as char)";
+            }
+            
+        }
+        
+        condition += " and jo.order_export_date between '"+begin_date+"' and '"+end_date+"' "; 
+        
+        
+        String sql = "select SUM(pieces) pieces_total,SUM(gross_weight) gross_weight_total,SUM(volume) volume_total,"
+        		+ "  SUM(ocean_fcl_teu) ocean_fcl_teu_total,SUM(ocean_lcl_cbm) ocean_lcl_cbm_total"
+        		+ " from (SELECT  "
+                + "        order_export_date, "
+                + "        customer_id, "
+                + "        customer_name, "
+                + "        route, "
+                + "        SUM(IFNULL(pieces, 0)) pieces, "
+                + "        SUM(IFNULL(gross_weight, 0)) gross_weight, "
+                + "        SUM(IFNULL(volume, 0)) volume, "
+                + "        SUM(IFNULL(ocean_fcl_teu, 0)) ocean_fcl_teu, "
+                + "        SUM(IFNULL(ocean_lcl_cbm, 0)) ocean_lcl_cbm "
+                + "    FROM "
+                + "        (SELECT  "
+                + "            CAST(CONCAT(YEAR(jo.order_export_date), '-', MONTH(jo.order_export_date)) "
+                + "                    AS CHAR) order_export_date, "
+                + "                jo.customer_id, "
+                + "                concat(ifnull(pol.name,''), ' -', ifnull(hub.name,''), '- ', ifnull(pod.name,'')) route, "
+                + "                jo.pieces, "
+                + "                jo.gross_weight, "
+                + "                jo.volume, "
+                + "                p.abbr customer_name, "
+                + "                (select ( "
+                + "                    count(case when container_type = '20''GP' then container_type end) + "
+                + "                    count(case when container_type = '40''GP' then container_type end)*2 + "
+                + "                    count(case when container_type = '45''GP' then container_type end)*2 + "
+                + "                    count(case when container_type = '40''HQ' then container_type end)*2) gp20 "
+                + "                 from job_order_shipment_item josi where josi.load_type='FCL'and josi.order_id =jo.id  "
+                + "                    and jo.type in('出口柜货', '进口柜货') "
+                + "                ) ocean_fcl_teu, "
+                + "                (select sum(volume) from job_order jo1 where jo1.id=jo.id and jo1.type in('出口散货', '进口散货')) ocean_lcl_cbm "
+                + "        FROM "
+                + "            job_order jo "
+                + "        left join job_order_shipment jos on jos.order_id = jo.id "
+                + "        left join location pol on jos.pol = pol.id "
+                + "        left join location hub on jos.hub = hub.id "
+                + "        left join location pod on jos.pod = pod.id "
+                + "        LEFT JOIN party p ON p.id = jo.customer_id "
+                + "        WHERE jo.type in ('出口柜货', '进口柜货','出口散货', '进口散货') "
+                + "           and jo.office_id="+office_id
+                + condition
+                + " ) A where 1=1"
+                + " GROUP BY A.order_export_date,A.customer_id, A.route"
+                + " ORDER BY A.customer_id , A.order_export_date"
+                + " )B";
+        Record rec = Db.findFirst(sql);
+        
+        long total=list();
+        rec.set("total", total);
+        renderJson(rec);
+    }  
     
 }
