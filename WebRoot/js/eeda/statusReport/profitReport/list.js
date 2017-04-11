89ee00a541315a476599a78e36f6fff62ac13b00
victor.tan@eeda123.com <kevin.dou@eeda123.com>
2017-04-11 17:12:24 +0800

    运营报表汇总

--- WebRoot/js/eeda/statusReport/profitReport/list.js
+++ WebRoot/js/eeda/statusReport/profitReport/list.js
@@ -1,6 +1,7 @@
 define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco'], function ($, metisMenu) {
   $(document).ready(function() {
   	document.title = '运营报表  | '+document.title;
+  	$('#breadcrumb_li').text('运营报表');
   	
   	//datatable, 动态处理
       var dataTable = eeda.dt({
