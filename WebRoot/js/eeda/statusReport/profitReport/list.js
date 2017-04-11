43bf24ba0caedbd8698bc1f9fc9bd7ada58391a8
victor.tan@eeda123.com <kevin.dou@eeda123.com>
2017-04-11 17:06:27 +0800

    运营报表汇总

--- WebRoot/js/eeda/statusReport/profitReport/list.js
+++ WebRoot/js/eeda/statusReport/profitReport/list.js
@@ -14,7 +14,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "PIECES","width":"50px",
                 "render": function ( data, type, full, meta ) {
                   if(data){
-                    return "<span class='pull-right'>" + data + "</span>";
+                    return "<span class=''>" + eeda.numFormat(data,3) + "</span>";
                   } else {
                     return '';
                   }
@@ -23,7 +23,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "GROSS_WEIGHT","width":"90px",
                 "render": function ( data, type, full, meta ) {
                   if(data){
-                    return "<span class='pull-right' >" + (data).toFixed(2) + "</span>";
+                    return "<span class='' >" + eeda.numFormat(data.toFixed(2),3) + "</span>";
                   } else {
                     return '';
                   }
@@ -32,7 +32,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "VOLUME","width":"90px",
                 "render": function ( data, type, full, meta ) {
                   if(data){
-                    return "<span class='pull-right'>" + (data).toFixed(2) + "</span>";
+                    return "<span class=''>" + eeda.numFormat(data.toFixed(2),3) + "</span>";
                   } else{
                     return '';
                   }
@@ -41,7 +41,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "OCEAN_FCL_TEU","width":"90px",
                 "render": function ( data, type, full, meta ) {
                     if(data){
-                      return "<span class='pull-right'>"+(data).toFixed(0) + "</span>";
+                      return "<span class=''>"+eeda.numFormat(data.toFixed(0),3) + "</span>";
                     } else{
                       return '';
                   }
@@ -50,7 +50,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "OCEAN_FCL_BILL","width":"120px",
                   "render": function ( data, type, full, meta ) {
                       if(data){
-                        return "<span class='pull-right'>"+(data).toFixed(0) + "</span>";
+                        return "<span class=''>"+eeda.numFormat(data.toFixed(0),3) + "</span>";
                       } else{
                         return '';
                     }
@@ -59,7 +59,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "OCEAN_LCL_CBM","width":"100px",
                 "render": function ( data, type, full, meta ) {
                     if(data){
-                      return "<span class='pull-right'>"+(data).toFixed(2) + "</span>";
+                      return "<span class=''>"+eeda.numFormat(data.toFixed(2),3) + "</span>";
                     } else{
                       return '';
                     }
@@ -68,7 +68,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "OCEAN_LCL_BILL","width":"120px",
                   "render": function ( data, type, full, meta ) {
                       if(data){
-                        return "<span class='pull-right'>"+(data).toFixed(2) + "</span>";
+                        return "<span class=''>"+eeda.numFormat(data.toFixed(0),3) + "</span>";
                       } else{
                         return '';
                       }
@@ -77,7 +77,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "ARI_KG","width":"100px",
                 "render": function ( data, type, full, meta ) {
                     if(data){
-                      return "<span class='pull-right'>"+(data).toFixed(2) + "</span>";
+                      return "<span class=''>"+eeda.numFormat(data.toFixed(2),3) + "</span>";
                     } else{
                       return '';
                     }
@@ -86,7 +86,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
               { "data": "ARI_KG_BILL","width":"120px",
                   "render": function ( data, type, full, meta ) {
                       if(data){
-                        return "<span class='pull-right'>"+(data).toFixed(0) + "</span>";
+                        return "<span class=''>"+eeda.numFormat(data.toFixed(0),3)+ "</span>";
                       } else{
                         return '';
                       }
@@ -104,6 +104,28 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
       $('#searchBtn').click(function(){
           searchData(); 
       })
+      
+      
+      
+//      if($('#eeda-table td').length>1){
+//      	var col = [3, 4, ,11,12, 13, 16, 19,21];
+//      	for (var i=0;i<col.length;i++){
+//  	    	var arr = cargoTable.column(col[i]).data();
+//      		$('#eeda-table tfoot').find('th').eq(col[i]).html(
+//  	    		arr.reduce(function (a, b) {
+//  		    		a = parseFloat(a);
+//  		    		if(isNaN(a)){ a = 0; }                   
+//  		    		b = parseFloat(b);
+//  		    		if(isNaN(b)){ b = 0; }
+//  		    		return (a + b).toFixed(3);
+//  		    	})
+//  	    	);
+//      	}
+//      }
+      
+      
+      
+      
 
      var searchData=function(){
           var order_no = $.trim($("#order_no").val()); 
@@ -121,7 +143,37 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
         	  begin_date = $("#month_begin_time").val();
               end_date = $("#month_end_time").val();
           }
-         
+          
+          
+        //合计字段
+          $.post('profitReport/listTotal',{
+        	  customer_id:customer_id,
+        	  date_type:date_type,
+        	  end_date:end_date,
+        	  end_date:end_date
+          },function(data){
+        	  var pieces_total = parseFloat(data.PIECES_TOTAL);
+        	  var gross_weight_total = parseFloat(data.GROSS_WEIGHT_TOTAL);
+        	  var volume_total = parseFloat(data.VOLUME_TOTAL);
+        	  var ocean_fcl_teu_total = parseFloat(data.OCEAN_FCL_TEU_TOTAL);
+        	  var ocean_fcl_bill_total = parseFloat(data.OCEAN_FCL_BILL_TOTAL);
+        	  var ocean_lcl_cbm_total = parseFloat(data.OCEAN_LCL_CBM_TOTAL);
+        	  var ocean_lcl_bill_total = parseFloat(data.OCEAN_LCL_BILL_TOTAL);
+        	  var ari_kg_total = parseFloat(data.ARI_KG_TOTAL);
+        	  var ari_kg_bill_total = parseFloat(data.ARI_KG_BILL_TOTAL);
+        	  var total=parseFloat(data.TOTAL);
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(0).html('共'+total+'项汇总：');
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(2).html(eeda.numFormat(pieces_total,3));
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(3).html(eeda.numFormat(gross_weight_total,3));
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(4).html(eeda.numFormat(volume_total,3));
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(5).html(eeda.numFormat(ocean_fcl_teu_total,3));
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(6).html(eeda.numFormat(ocean_fcl_bill_total,3));
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(7).html(eeda.numFormat(ocean_lcl_cbm_total,3));
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(8).html(eeda.numFormat(ocean_lcl_bill_total,3));
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(9).html(eeda.numFormat(ari_kg_total,3));
+        	  $($('.dataTables_scrollFoot tr')[0]).find('th').eq(10).html(eeda.numFormat(ari_kg_bill_total,3));
+
+          });
           
           //增加出口日期查询
           var url = "/profitReport/list?order_no="+order_no
@@ -150,7 +202,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
     		  $('#season').hide();
     	  }
       })
- 
+      searchData();
 
   });
 });
--- src/controllers/report/ProfitReportController.java
+++ src/controllers/report/ProfitReportController.java
@@ -27,13 +27,13 @@ import controllers.profile.LoginUserController;
 public class ProfitReportController extends Controller {
     private Log logger = Log.getLog(ProfitReportController.class);
     Subject currentUser = SecurityUtils.getSubject();
-    
+    long total=0;
     @Before(EedaMenuInterceptor.class)
     public void index() {
     	render("eeda/statusReport/profitReport.html");
     }
 
-    public void list() {
+    public  long list() {
     	String sLimit = "";
         String pageIndex = getPara("draw");
         if (getPara("start") != null && getPara("length") != null) {
@@ -151,13 +151,133 @@ public class ProfitReportController extends Controller {
         String sqlTotal = "select count(1) total from ("+sql+") B";
         Record rec = Db.findFirst(sqlTotal);
         logger.debug("total records:" + rec.getLong("total"));
-        
         List<Record> orderList = Db.find(sql +sLimit);
+        long total = rec.getLong("total");
         Map orderListMap = new HashMap();
         orderListMap.put("draw", pageIndex);
         orderListMap.put("recordsTotal", rec.getLong("total"));
         orderListMap.put("recordsFiltered", rec.getLong("total"));
         orderListMap.put("data", orderList);
         renderJson(orderListMap); 
+        return total;
     }
+    
+    
+    public void listTotal(){
+    	UserLogin user = LoginUserController.getLoginUser(this);
+        long office_id=user.getLong("office_id");
+        String customer_id = getPara("customer_id");
+        String begin_date = getPara("begin_date");
+        String end_date = getPara("end_date");
+        String date_type = getPara("date_type");
+        
+        String condition = "";
+        String group_condition="";
+        if(StringUtils.isNotEmpty(customer_id)){
+        	condition += " and jo.customer_id = "+customer_id;
+        }
+        
+        if("year".equals(date_type)){
+        	if(StringUtils.isNotEmpty(begin_date)){
+            	begin_date = begin_date+"-01-01 00:00:00";
+            }else{
+            	begin_date = "1970-01-01 00:00:00";
+            }
+            
+            if(StringUtils.isNotEmpty(end_date)){
+            	end_date = end_date+"-12-31 23:59:59";
+            }else{
+            	end_date = "2037-12-31 23:59:59";
+            }
+            
+            group_condition = " cast(year(jo.order_export_date) as char)";
+        }else {
+        	if(StringUtils.isNotEmpty(begin_date)){
+            	begin_date = begin_date+"-01 00:00:00";
+            }else{
+            	begin_date = "1970-01-01 00:00:00";
+            }
+            
+            if(StringUtils.isNotEmpty(end_date)){
+            	end_date = end_date+"-31 23:59:59";
+            }else{
+            	end_date = "2037-12-31 23:59:59";
+            }
+            
+            if("season".equals(date_type)){
+            	group_condition = " (case"
+            			+ " when cast(month(jo.order_export_date) as char)"
+            			+ " between 1 and 3 "
+            			+ " then cast(CONCAT(year(jo.order_export_date),'-','第一季') as char)"
+            			+ " when cast(month(jo.order_export_date) as char)"
+            			+ " between 4 and 6 "
+            			+ " then cast(CONCAT(year(jo.order_export_date),'-','第二季') as char)"
+            			+ " when cast(month(jo.order_export_date) as char)"
+            			+ " between 7 and 9 "
+            			+ " then cast(CONCAT(year(jo.order_export_date),'-','第三季') as char)"
+            			+ " when cast(month(jo.order_export_date) as char)"
+            			+ " between 10 and 12 "
+            			+ " then cast(CONCAT(year(jo.order_export_date),'-','第四季') as char)"
+            			+ " end)";
+            }else{
+            	group_condition = " cast(CONCAT(year(jo.order_export_date),'-',month(jo.order_export_date)) as char)";
+            }
+            
+        }
+        
+        condition += " and jo.order_export_date between '"+begin_date+"' and '"+end_date+"' "; 
+        
+        String sql = "  SELECT "
+        		+" 	SUM(gross_weight) gross_weight_total,SUM(pieces) pieces_total,SUM(volume) volume_total,SUM(ocean_fcl_bill) ocean_fcl_bill_total, "
+        		+" 	SUM(ocean_lcl_bill) ocean_lcl_bill_total,SUM(ari_kg_bill) ari_kg_bill_total,SUM(ocean_fcl_teu) ocean_fcl_teu_total,SUM(ocean_lcl_cbm)  "
+        		+" ocean_lcl_cbm_total,SUM(ari_kg) ari_kg_total "
+    	        + " from (select order_export_date,customer_id,"
+    			+ " sum(ifnull(pieces,0)) pieces,"
+    			+ " sum(ifnull(gross_weight,0)) gross_weight, "
+    			+ " sum(ifnull(volume,0)) volume, "
+    			+ " SUM(IFNULL(ocean_fcl_bill, 0)) ocean_fcl_bill, "
+    			+ " SUM(IFNULL(ocean_lcl_bill, 0)) ocean_lcl_bill, "
+    			+ " SUM(IFNULL(ari_kg_bill, 0)) ari_kg_bill, "
+    			+ " customer_name,"
+    			+ " SUM(IFNULL(ocean_fcl_teu, 0)) ocean_fcl_teu,"
+    			+ " SUM(IFNULL(ocean_lcl_cbm, 0)) ocean_lcl_cbm,"
+    			+ " SUM(IFNULL(ari_kg, 0)) ari_kg,"
+    			+ " group_concat(CAST(id as CHAR) separator ', ' ) truck_order_ids"
+    			+ " from ("
+    			+ " select "+group_condition+" order_export_date, jo.customer_id, jo.pieces,"
+    			+ " jo.gross_weight, jo.volume,p.abbr customer_name,"
+    			+ " (select ("
+    			+ "    count(case when container_type = '20''GP' then container_type end) +"
+    			+ "    count(case when container_type = '40''GP' then container_type end)*2 +"
+    			+ "    count(case when container_type = '45''GP' then container_type end)*2 +"
+    			+ "    count(case when container_type = '40''HQ' then container_type end)*2) gp20"
+    			+ " from job_order_shipment_item josi where josi.load_type='FCL'and josi.order_id =jo.id "
+    			+ "    and jo.type in('出口柜货', '进口柜货')"
+    			+ " ) ocean_fcl_teu,"
+    			+ " (SELECT   COUNT(container_type) FROM job_order_shipment_item josi WHERE "
+    			+ "	josi.load_type = 'FCL' AND josi.order_id = jo.id and container_type is not null "
+    			+ "	AND jo.type IN ('出口柜货','进口柜货') ) ocean_fcl_bill, "
+    			+ " (SELECT count(volume) FROM job_order jo1 WHERE jo1.id = jo.id and volume is NOT null "
+    			+ "	 AND jo1.type IN ('出口散货','进口散货')) ocean_lcl_bill, "
+    			+ " (select sum(volume) from job_order jo1 where jo1.id=jo.id and jo1.type in('出口散货', '进口散货')) ocean_lcl_cbm,"
+    			+ " (select sum(gross_weight) from job_order jo1 where jo1.id=jo.id and jo1.type in('出口空运', '进口空运')) ari_kg,"
+    			+ " (SELECT	count(gross_weight) FROM job_order jo1 WHERE jo1.id = jo.id and jo1.gross_weight is not null "
+    			+ "	 AND jo1.type IN ('出口空运','进口空运')) ari_kg_bill, "
+    			+ " jo.id "
+    			+ " from job_order jo"
+    			+ " left join party p on p.id = jo.customer_id"
+    			+ " WHERE jo.office_id="+office_id
+    			+ condition
+    			+ " ) A where 1=1"
+    			+ " GROUP BY A.order_export_date,A.customer_id"
+    			+ " )B ORDER BY B.customer_id , B.order_export_date"
+    			+ "  ";
+        
+          Record rec = Db.findFirst(sql);
+          
+          long total=list();
+          rec.set("total", total);
+          renderJson(rec);   
+    }
+    
 }
