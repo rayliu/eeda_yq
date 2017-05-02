584897d5a7160946fd4257ffda0c936f8d4d58af
Ray Liu <liuyubie@gmail.com>
2017-05-02 23:38:20 +0800

    库存查询: 点击清空按钮，隐藏产品名称行

--- WebRoot/js/eeda/wms/inventory/list.js
+++ WebRoot/js/eeda/wms/inventory/list.js
@@ -50,6 +50,8 @@ define(['jquery', 'metisMenu', 'sb_admin','dataTables',  'dataTablesBootstrap', 
       
         $('#resetBtn').click(function(e){
         	$("#orderForm")[0].reset();
+            $('.itemShow').hide();
+            $('#orderText').text("");
         });
 
         $('#searchBtn').click(function(){
