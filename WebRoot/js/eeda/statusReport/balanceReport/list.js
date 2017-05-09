2626934053b4e30ccf3872a595780bbd76676faf
henry.li <henry.li@eeda123.com>
2017-05-09 13:52:22 +0800

    远桥，运营报表,查询条件 默认展开

--- WebRoot/js/eeda/statusReport/balanceReport/list.js
+++ WebRoot/js/eeda/statusReport/balanceReport/list.js
@@ -1,6 +1,7 @@
 define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco',  'dtColReorder'], function ($, metisMenu) {
   $(document).ready(function() {
   	document.title = '结算统计报表  | '+document.title;
+  	$("#breadcrumb_li").text('结算统计报表');
   	//datatable, 动态处理
       var dataTable = eeda.dt({
           id: 'eeda-table',
