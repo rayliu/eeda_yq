d380133ee47b3c39b4804660def01a0f0a74b772
victor.tan@eeda123.com <kevin.dou@eeda123.com>
2017-05-09 09:35:28 +0800

    去掉office条件

--- src/controllers/profile/ServiceProviderController.java
+++ src/controllers/profile/ServiceProviderController.java
@@ -651,7 +651,7 @@ public class ServiceProviderController extends Controller {
     	UserLogin user = LoginUserController.getLoginUser(this);
         long office_id = user.getLong("office_id");
     	String sql = "select cr.office_id,c.id,c.code,c.name,cast( if(cr.from_stamp<'"+d+"' and cr.to_stamp>'"+d+"',cr.rate,'') as char ) rate from currency c"
-    			+ " left join currency_rate cr on cr.currency_code = c.code and cr.office_id=c.office_id where 1=1 and cr.office_id= "+office_id;
+    			+ " left join currency_rate cr on cr.currency_code = c.code  where 1=1 and cr.office_id= "+office_id;
     	if(!StringUtils.isBlank(input)){
     		sql+=" and (c.name like '%" + input + "%' or c.english_name like '%" + input + "%' or c.code like '%" + input + "%') ";
     	}
