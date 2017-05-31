951f8844b15569134130b2da591093f9adfc8b2d
henry.li <henry.li@eeda123.com>
2017-05-31 09:51:37 +0800

    供应商合同管理，

---
+++ WebRoot/js/eeda/profile/customer/add_dock_item_table.js
@@ -0,0 +1,120 @@
+define(['jquery', 'metisMenu', 'template', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco'], function ($, metisMenu, template) { 
+	$(document).ready(function() {
+
+	    var deletedTableIds=[];
+
+	    //删除一行
+	    $("#cargo_table").on('click', '.delete', function(e){
+	        e.preventDefault();
+	        var tr = $(this).parent().parent();
+	        deletedTableIds.push(tr.attr('id'));
+	        
+	        cargoTable.row(tr).remove().draw();
+	    }); 
+	    
+	    //构造函数，获得json
+	    itemOrder.buildDockItem=function(){
+	    	var cargo_table_rows = $("#cargo_table tr");
+	        var cargo_items_array=[];
+	        for(var index=0; index<cargo_table_rows.length; index++){
+	            if(index==0)
+	                continue;
+
+	            var row = cargo_table_rows[index];
+	            var empty = $(row).find('.dataTables_empty').text();
+	            if(empty)
+	            	continue;
+	            
+	            var id = $(row).attr('id');
+	            if(!id){
+	                id='';
+	            }
+	            
+	            var item={}
+	            item.id = id;
+	            item.party_type = 'customer';
+	           
+	            for(var i = 1; i < row.childNodes.length; i++){
+	            	var name = $(row.childNodes[i]).find('input,select').attr('name');
+	            	var value = $(row.childNodes[i]).find('input,select').val();
+	            	if(name){
+	            		item[name] = value;
+	            	}
+	            }
+	            item.action = id.length > 0?'UPDATE':'CREATE';
+	            cargo_items_array.push(item);
+	        }
+
+	        //add deleted items
+	        for(var index=0; index<deletedTableIds.length; index++){
+	            var id = deletedTableIds[index];
+	            var item={
+	                id: id,
+	                action: 'DELETE'
+	            };
+	            cargo_items_array.push(item);
+	        }
+	        deletedTableIds = [];
+	        return cargo_items_array;
+	    };
+	    
+
+	    //------------事件处理
+	    var cargoTable = eeda.dt({
+            id: 'cargo_table',
+            autoWidth: false,
+            "drawCallback": function( settings ) {
+		        
+		    },
+            columns:[
+	            {"width": "5%",
+	                "render": function ( data, type, full, meta ) {
+	                		return '<button type="button" class="delete btn btn-default btn-xs" style="width:100%" >删除</button> ';
+	                }
+	            },
+	            { "data": "DOCK_NAME","width": "25%",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                    return '<input style="width:100%"  type="text" name="dock_name" value="'+data+'" class="form-control search-control" />';
+	                }
+	            },
+	            { "data": "DOCK_NAME_ENG","width": "25%",
+	                "render": function ( data, type, full, meta ) {
+	                    if(!data)
+	                        data='';
+	                    return '<input type="text" name="dock_name_eng" value="'+data+'" class="form-control search-control" style="width:100%"/>';
+	                }
+	            },
+	            { "data": "QUICK_SEARCH_CODE","width": "25%",
+	                "render": function ( data, type, full, meta ) {
+	                   if(!data)
+	                	   data='';
+	                   return '<input type="text" name="quick_search_code" value="'+data+'" class="form-control search-control" style="width:100%"/>';
+	                }
+	            },
+	            { "data": "DOCK_REGION","width": "20%",
+	                "render": function ( data, type, full, meta ) {
+	                   if(!data)
+	                	   data='';
+	                   return '<input type="text" name="dock_region" value="'+data+'" class="form-control search-control" style="width:100%"/>';
+	                }
+	            }
+	        ]
+	    });
+	    
+
+
+	    $('#add_cargo').on('click', function(){
+	        var item={};
+	        cargoTable.row.add(item).draw(true);
+	    });
+	    
+	    //刷新明细表
+	    itemOrder.refleshTable = function(order_id){
+	    	var url = "/customer/tableList?order_id="+order_id+"&type='dock'";
+	    	cargoTable.ajax.url(url).load();
+	    }
+	    
+	});
+});
