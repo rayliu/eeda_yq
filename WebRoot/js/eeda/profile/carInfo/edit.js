73bd67f30f2ebb57f47f40658894cd3d107f7049
henry.li <henry.li@eeda123.com>
2017-06-06 14:03:12 +0800

    车队，车辆司机信息，样式修改

--- WebRoot/js/eeda/profile/carInfo/edit.js
+++ WebRoot/js/eeda/profile/carInfo/edit.js
@@ -3,7 +3,41 @@ define(['jquery', 'metisMenu', 'sb_admin', 'validate_cn', 'sco'], function ($, m
         document.title = '车辆司机信息 | '+document.title;
         $("#breadcrumb_li").text('车辆司机信息');
 
-        var car_no = $("#car_number").val();
+        
+        //保存
+        //自动提交改为手动提交
+        $("#save").click(function(){
+      	 if(!$("#leadsForm").valid()){
+      		 $.scojs_message('请填上面的必填字段', $.scojs_message.TYPE_ERROR);
+      		  return false;
+      	 }
+      	 $("#save").attr("disabled",true);
+//      	 var order={};
+//      	 order.cars_json =itemOrder.buildCarsDetail();
+//      	 $("#acount_json").val(JSON.stringify(order));
+      	 $.post("/carInfo/save", $("#leadsForm").serialize(),function(data){
+      		if(data=='abbrError'){
+      			$.scojs_message('该车牌已存在', $.scojs_message.TYPE_ERROR);
+      			$("#save").attr("disabled",false);
+      		}else if(data.ID != null && data.ID != ""){
+       			eeda.contactUrl("edit?id",data.ID);
+       			$.scojs_message('保存成功', $.scojs_message.TYPE_OK);
+       			$("#carId").val(data.ID);
+       			$("#save").attr("disabled",false);
+       		}else{
+       			$.scojs_message('数据有误', $.scojs_message.TYPE_ERROR);
+       			$("#save").attr("disabled",false);
+       		}
+      		//异步刷新明细表
+      		if(data!='abbrError'){
+//      			itemOrder.refleshTable(data.ID);
+      		}
+      		
+           });
+      	  
+        });
+        
+        var car_no = $("#car_no").val();
         if(car_no){
             document.title = car_no +' | '+document.title;
         }
@@ -14,20 +48,9 @@ define(['jquery', 'metisMenu', 'sb_admin', 'validate_cn', 'sco'], function ($, m
 
         $('#leadsForm').validate({
             rules: {
-                username: {
-                required: true,
-                stringCheck:true,
-                minlength: 5
-                },
-              password:{//form 中 name为必填
-                required: true,
-                minlength: 5
-              },
-              confirm_password: { 
-                  required: true, 
-                  minlength: 5, 
-                  equalTo: "#password" 
-                  } 
+            	car_no:{//form 中 abbr为必填
+                    required: true
+                  }
              },
             highlight: function(element) {
                 $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
@@ -37,6 +60,7 @@ define(['jquery', 'metisMenu', 'sb_admin', 'validate_cn', 'sco'], function ($, m
             }
         });
         
+        //公司车，街车二选一；
         $("[name=gongsiche],[name=jieche]").on('click',function(){
         	if(this.checked){
         		if(this.name=='gongsiche'){
