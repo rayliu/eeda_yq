9e66a28d7ed3f5ad3676cb1731c0375249361ec2
kevin <kevin.dou@eeda123.com>
2017-06-08 11:14:40 +0800

    用户界面优化

--- WebRoot/js/eeda/profile/loginUser/add.js
+++ WebRoot/js/eeda/profile/loginUser/add.js
@@ -1,133 +1,133 @@
 define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 
         'validate_cn', 'sco', 'datetimepicker_CN', 'jq_blockui'], function ($, metisMenu) { 
-
-var queryOffice=function(){
-	var offices=[];
-	$.post('/loginUser/searchAllOffice',function(data){
-   		 if(data.length > 0){
-   			 var officeSelect = $("select[name='officeSelect']");
-   			$("select[name='officeSelect']").each(function(){
-   				if($(this).val()!=null&&$(this).val()!=""){
-   					var value=$(this).val();
-   					var txt = $(this).find("option:selected").text();
-   					offices.push($(this).val());
-   					$(this).empty();
-					$(this).append("<option ></option>");
-   					$(this).append("<option value='"+value+"' selected='selected'>"+txt+"</option>");
-   				}else{
-   					$(this).empty();
-   					$(this).append("<option ></option>");
-   				}
-	   		});
-   			
-   			for(var i=0; i<data.length; i++){
-   				var n=0;
-   				for(var j=0;j<offices.length;j++){
-   					if(data[i].ID==offices[j]){
-   						n=offices[j];
-   					}
-   				}
-		 		if(data[i].ID!=n){
-		 			if(data[i].IS_STOP != true){
-		 				officeSelect.append("<option value='"+data[i].ID+"'>"+data[i].OFFICE_NAME+"</option>");
-		 			}
-							 		
-				}
-   				
-   			}
-   			
-   		 }
-	 },'json');
-};
-
-var queryCustomer=function(){
-	var customers=[];
-	$.post('/loginUser/searchAllCustomer',function(data){
-   		 if(data.length > 0){
-   			 var customerSelect = $("select[name='customerSelect']");
-   			$("select[name='customerSelect']").each(function(){
-   				if($(this).val()!=null&&$(this).val()!=""){
-   					var value=$(this).val();
-   					var txt = $(this).find("option:selected").text();
-   					customers.push($(this).val());
-   					$(this).empty();
-					$(this).append("<option ></option>");
-   					$(this).append("<option value='"+value+"' selected='selected'>"+txt+"</option>");
-   					
-   				}else{
-   					$(this).empty();
-   					$(this).append("<option > </option>");
-   				}
-	   		});
-   			for(var i=0; i<data.length; i++){
-   				var n=0;
-   				for(var j=0;j<customers.length;j++){
-   					if(data[i].PID==customers[j]){
-   						n=customers[j];
-   					}
-   				}
-		 		if(data[i].PID!=n){
-		 			if(data[i].IS_STOP != true){
-		 				customerSelect.append("<option value='"+data[i].PID+"'>"+data[i].COMPANY_NAME+"</option>");
-		 			}
-		 					 		
-				}
-   				
-   			}
-   			
-   		 }
-	 },'json');
-};
-
-var officeList = function(){
-	var userId = $("#userId").val();
-	$.post('/loginUser/officeList',{userId:userId},function(data){
-		var tobdy = $("#tobdy");
-		tobdy.empty();
-		if(data.userOffice!=null){
-			if(data.userOffice.length>0){
-				for(var i =0;i<data.userOffice.length;i++){
-					//console.log(data.userOffice[i].IS_MAIN);
-					if(data.userOffice[i].IS_MAIN !=null && data.userOffice[i].IS_MAIN != false){
-						tobdy.append('<tr><td><select class="form-control sOffice" name="officeSelect"></select>'
-			                    +' </td><td><input type="radio" checked class="is_main" name="isMain_radio" value="'+data.userOffice[i].OFFICE_ID+'"></td>'
-			                    +' <td><a class="btn removeOffice" title="删除"><i class="fa fa-trash-o fa-fw"></i></a></td></tr>');
-					}else{
-						tobdy.append('<tr><td><select class="form-control sOffice" name="officeSelect"></select>'
-			                    +' </td><td><input type="radio"  class="is_main" name="isMain_radio" value="'+data.userOffice[i].OFFICE_ID+'"></td>'
-			                    +' <td><a class="btn removeOffice" title="删除"><i class="fa fa-trash-o fa-fw"></i></a></td></tr>');
+$(document).ready(function() {
+	var queryOffice=function(){
+		var offices=[];
+		$.post('/loginUser/searchAllOffice',function(data){
+	   		 if(data.length > 0){
+	   			 var officeSelect = $("select[name='officeSelect']");
+	   			$("select[name='officeSelect']").each(function(){
+	   				if($(this).val()!=null&&$(this).val()!=""){
+	   					var value=$(this).val();
+	   					var txt = $(this).find("option:selected").text();
+	   					offices.push($(this).val());
+	   					$(this).empty();
+						$(this).append("<option ></option>");
+	   					$(this).append("<option value='"+value+"' selected='selected'>"+txt+"</option>");
+	   				}else{
+	   					$(this).empty();
+	   					$(this).append("<option ></option>");
+	   				}
+		   		});
+	   			
+	   			for(var i=0; i<data.length; i++){
+	   				var n=0;
+	   				for(var j=0;j<offices.length;j++){
+	   					if(data[i].ID==offices[j]){
+	   						n=offices[j];
+	   					}
+	   				}
+			 		if(data[i].ID!=n){
+			 			if(data[i].IS_STOP != true){
+			 				officeSelect.append("<option value='"+data[i].ID+"'>"+data[i].OFFICE_NAME+"</option>");
+			 			}
+								 		
 					}
-					$("select[name='officeSelect']:last").append("<option value='"+data.userOffice[i].OFFICE_ID+"'>"+data.userOffice[i].OFFICE_NAME+"</option>");
-					queryOffice();
-				}
-				
-			}
-		}
-	},'json');
-};
-var customerList = function(){
-	var userId = $("#userId").val();
-	$.post('/loginUser/customerList',{userId:userId},function(data){
-		
-		var tobdy = $("#customerTbody");
-		tobdy.empty();
-		if(data.customerlist!=null){
-			if(data.customerlist.length>0){
-				for(var i =0;i<data.customerlist.length;i++){
-					tobdy.append('<tr><td>'
-						+'<select class="form-control customer" name="customerSelect"></select></td>'
-		                +' <td><a class="btn removeCustomer" title="删除"><i class="fa fa-trash-o fa-fw"></i></a></td></tr>');
+	   				
+	   			}
+	   			
+	   		 }
+		 },'json');
+	};
+	
+	var queryCustomer=function(){
+		var customers=[];
+		$.post('/loginUser/searchAllCustomer',function(data){
+	   		 if(data.length > 0){
+	   			 var customerSelect = $("select[name='customerSelect']");
+	   			$("select[name='customerSelect']").each(function(){
+	   				if($(this).val()!=null&&$(this).val()!=""){
+	   					var value=$(this).val();
+	   					var txt = $(this).find("option:selected").text();
+	   					customers.push($(this).val());
+	   					$(this).empty();
+						$(this).append("<option ></option>");
+	   					$(this).append("<option value='"+value+"' selected='selected'>"+txt+"</option>");
+	   					
+	   				}else{
+	   					$(this).empty();
+	   					$(this).append("<option > </option>");
+	   				}
+		   		});
+	   			for(var i=0; i<data.length; i++){
+	   				var n=0;
+	   				for(var j=0;j<customers.length;j++){
+	   					if(data[i].PID==customers[j]){
+	   						n=customers[j];
+	   					}
+	   				}
+			 		if(data[i].PID!=n){
+			 			if(data[i].IS_STOP != true){
+			 				customerSelect.append("<option value='"+data[i].PID+"'>"+data[i].COMPANY_NAME+"</option>");
+			 			}
+			 					 		
+					}
+	   				
+	   			}
+	   			
+	   		 }
+		 },'json');
+	};
+	
+	var officeList = function(){
+		var userId = $("#userId").val();
+		$.post('/loginUser/officeList',{userId:userId},function(data){
+			var tobdy = $("#tobdy");
+			tobdy.empty();
+			if(data.userOffice!=null){
+				if(data.userOffice.length>0){
+					for(var i =0;i<data.userOffice.length;i++){
+						//console.log(data.userOffice[i].IS_MAIN);
+						if(data.userOffice[i].IS_MAIN !=null && data.userOffice[i].IS_MAIN != false){
+							tobdy.append('<tr><td><select class="form-control sOffice" name="officeSelect"></select>'
+				                    +' </td><td><input type="radio" checked class="is_main" name="isMain_radio" value="'+data.userOffice[i].OFFICE_ID+'"></td>'
+				                    +' <td><a class="btn removeOffice" title="删除"><i class="fa fa-trash-o fa-fw"></i></a></td></tr>');
+						}else{
+							tobdy.append('<tr><td><select class="form-control sOffice" name="officeSelect"></select>'
+				                    +' </td><td><input type="radio"  class="is_main" name="isMain_radio" value="'+data.userOffice[i].OFFICE_ID+'"></td>'
+				                    +' <td><a class="btn removeOffice" title="删除"><i class="fa fa-trash-o fa-fw"></i></a></td></tr>');
+						}
+						$("select[name='officeSelect']:last").append("<option value='"+data.userOffice[i].OFFICE_ID+"'>"+data.userOffice[i].OFFICE_NAME+"</option>");
+						queryOffice();
+					}
 					
-					$("select[name='customerSelect']:last").append("<option value='"+data.customerlist[i].CUSTOMER_ID+"'>"+data.customerlist[i].COMPANY_NAME+"</option>");
+				}
+			}
+		},'json');
+	};
+	var customerList = function(){
+		var userId = $("#userId").val();
+		$.post('/loginUser/customerList',{userId:userId},function(data){
+			
+			var tobdy = $("#customerTbody");
+			tobdy.empty();
+			if(data.customerlist!=null){
+				if(data.customerlist.length>0){
+					for(var i =0;i<data.customerlist.length;i++){
+						tobdy.append('<tr><td>'
+							+'<select class="form-control customer" name="customerSelect"></select></td>'
+			                +' <td><a class="btn removeCustomer" title="删除"><i class="fa fa-trash-o fa-fw"></i></a></td></tr>');
+						
+						$("select[name='customerSelect']:last").append("<option value='"+data.customerlist[i].CUSTOMER_ID+"'>"+data.customerlist[i].COMPANY_NAME+"</option>");
+						
+					}
 					
 				}
-				
+				queryCustomer();
 			}
-			queryCustomer();
-		}
-	},'json');
-};
-$(document).ready(function(){
+		},'json');
+	};
+
   	$("#addOffice").on('click',function(){
 		$("#tobdy").append('<tr><td><select class="form-control sOffice" name="officeSelect"></select>'
                    +' </td><td><input type="radio"  class="is_main" name="isMain_radio" value=""></td>'
@@ -246,11 +246,12 @@ $(document).ready(function(){
 		$("#officeIds").val(officeIds.toString());
 		$("#customerIds").val(customerIds.toString());
 		
+		
 		$("#leadsForm").submit();
 	});
 	if($("#userId").val() != "" && $("#userId").val() != null){
 		$("#assigning_role").show();
 	}
-	});
 	$.unblockUI();
+});
 });
