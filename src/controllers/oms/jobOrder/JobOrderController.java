c02674780f46d68e84c076f2eb1adbdd17cc2e23
victor.tan@eeda123.com <kevin.dou@eeda123.com>
2017-06-14 13:45:55 +0800

    货代：按照出货时间排序

--- src/controllers/oms/jobOrder/JobOrderController.java
+++ src/controllers/oms/jobOrder/JobOrderController.java
@@ -1789,7 +1789,7 @@ public class JobOrderController extends Controller {
         String sColumn =  getPara("order[0][column]");
         String sName =  getPara("columns["+sColumn+"][data]")==null?"create_stamp":getPara("columns["+sColumn+"][data]") ;
         if("0".equals(sName)){
-        	sName = "create_stamp";
+        	sName = "order_export_date";
         }
         
         if (getPara("start") != null  && getPara("length") != null) {
