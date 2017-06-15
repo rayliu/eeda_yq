03449f547ace98396f75f1834d89e858864a4737
victor.tan@eeda123.com <kevin.dou@eeda123.com>
2017-06-15 17:41:26 +0800

    报关：应收对账报关日期截取

--- WebRoot/js/eeda/cms/cmsChargeCheckOrder/edit_item_table.js
+++ WebRoot/js/eeda/cms/cmsChargeCheckOrder/edit_item_table.js
@@ -60,7 +60,15 @@ $(document).ready(function() {
                         return "<a href='/customPlanOrder/edit?id="+full.ORDER_ID+"' target='_blank'>"+data+"</a>";
                     }
             },
-            { "data": "DATE_CUSTOM", "width": "100px"},
+            { "data": "DATE_CUSTOM", "width": "100px",
+            	"render":function(data,type,full,meta){
+            		if(!data){
+  	                  data='';
+  	                  return data;
+  	                }
+            		return data.substr(0,10);
+            	}
+            },
             { "data": "TRACKING_NO", "width": "180px"},
             { "data": "ABBR_NAME", "width": "120px"},
             { "data": "FIN_NAME", "width": "200px"},
