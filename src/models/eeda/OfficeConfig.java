cde968f7a9a216627023c515c3d45636a8b1a564
Ray Liu <liuyubie@gmail.com>
2017-06-19 10:03:41 +0800

    model: OfficeConfig

---
+++ src/models/eeda/OfficeConfig.java
@@ -0,0 +1,1 @@
+package models.eeda;import com.jfinal.plugin.activerecord.Model;public class OfficeConfig extends Model<OfficeConfig> {	public static final OfficeConfig dao = new OfficeConfig();}
