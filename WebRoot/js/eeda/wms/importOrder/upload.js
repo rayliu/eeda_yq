cabe979740600f0ecc2585ae10f0f972aa6fc8f1
kevin <kevin.dou@eeda123.com>
2017-06-23 13:11:25 +0800

    上传优化

--- WebRoot/js/eeda/wms/importOrder/upload.js
+++ WebRoot/js/eeda/wms/importOrder/upload.js
@@ -1,6 +1,6 @@
 define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco', 'file_upload'], function ($, metisMenu) {
 	$(document).ready(function() {
-		var isImporting = false;
+		var isImporting = import_status;
 		// 导入运输单
 	    $("#uploadBtn").click(function(){
 	    	$("#toFileUpload").click();
@@ -22,6 +22,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
 	        	 $("#uploadBtn").attr('disabled',true);
 	        	 $("#uploadBtn").html("<img src='/images/loading.gif' style='height: 25px;'/>正在导入...");
 	        	 isImporting = true;
+	        	 interval();
 	        } 
 	    },'json').error(function (jqXHR, textStatus, errorThrown) {
 	        alert("出错了，请刷新页面重新尝试。")
@@ -29,24 +30,21 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
 	    });
 		
 		
-//		if(true){
-//			$.scojs_message('in', $.scojs_message.OK);
-//			window.setInterval(updateStatus(),3000)
-//		}
-//		
-//		function updateStatus(){
-//			$.scojs_message('d', $.scojs_message.TYPE_FALSE);
-//			$.post('/importOrder/getImportStatus',function(data){
-//				if(data){
-//					$("#uploadBtn").attr('disabled',false);
-//		        	$("#uploadBtn").text('导入单据');
-//		        	isImporting = false;
-//		        	order.refleshTable();
-//		        	alert('导入成功');
-//				}
-//			});
-//		}
 		
+		var interval = window.setInterval(function(){
+			if(isImporting){
+				$.post('/importOrder/getImportStatus',function(data){
+					if(!data){
+						$("#uploadBtn").attr('disabled',false);
+			        	$("#uploadBtn").text('导入单据');
+			        	isImporting = false;
+			        	order.refleshTable();
+			        	alert('导入成功');
+			        	clearInterval(interval);
+					}
+				});
+			}
+		},10000)
 	});
 });
 
