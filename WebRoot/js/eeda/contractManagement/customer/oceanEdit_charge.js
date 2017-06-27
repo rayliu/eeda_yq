9a6a349b42848cad2648eeb27788b8c6a3eb19d6
henry.li <henry.li@eeda123.com>
2017-06-27 14:30:59 +0800

    客户合同：体积重量分成区间

--- WebRoot/js/eeda/contractManagement/customer/oceanEdit_charge.js
+++ WebRoot/js/eeda/contractManagement/customer/oceanEdit_charge.js
@@ -182,19 +182,61 @@ define(['jquery', 'metisMenu', 'template', 'sb_admin',  'dataTablesBootstrap', '
     	                    return str;
                 	}
                 },
-                { "data": "VOLUME", "width": "60px",
+//                { "data": "VOLUME", "width": "60px",
+//                	"render": function ( data, type, full, meta ) {
+//                        if(!data)
+//                            data='';
+//                        return '<input type="text" name="volume" value="'+data+'" class="form-control" style="width:80px"/>';
+//                    }
+//                },
+                { "data": "VOLUME1", "width": "50px",
                 	"render": function ( data, type, full, meta ) {
                         if(!data)
                             data='';
-                        return '<input type="text" name="volume" value="'+data+'" class="form-control" style="width:80px"/>';
+                        return '<input type="text" name="volume1" value="'+data+'" class="form-control" style="width:55px"/>';
                     }
                 },
-                { "data": "GROSS_WEIGHT", "width": "60px",
+                { "data": "-","width": "5px",
+                	"render": function ( data, type, full, meta ) {
+                        
+                        return "<span style='width:5px'>-</span>"
+                    }
+                },
+                { "data": "VOLUME2", "width": "30px",
+                	"render": function ( data, type, full, meta ) {
+                        if(!data)
+                            data='';
+                        return '<input type="text" name="volume2" value="'+data+'" class="form-control" style="width:50px"/>';
+                    }
+                },
+                { "data": "GROSS_WEIGHT1", "width": "40px",
                     "render": function ( data, type, full, meta ) {
                     	 if(!data)
     	                        data='';
     	                    
-    	                    return '<input type="text" style="width:80px" name="gross_weight" value = "'+data+'" class="form-control notsave" >';
+    	                    return '<input type="text" style="width:50px" name="gross_weight1" value = "'+data+'" class="form-control notsave" >';
+                    }
+                },
+                { "data": "-","width": "5px",
+                	"render": function ( data, type, full, meta ) {
+                        
+                        return "<span style='width:5px'>-</span>"
+                    }
+                },
+//                { "data": "GROSS_WEIGHT", "width": "60px",
+//                    "render": function ( data, type, full, meta ) {
+//                    	 if(!data)
+//    	                        data='';
+//    	                    
+//    	                    return '<input type="text" style="width:80px" name="gross_weight" value = "'+data+'" class="form-control notsave" >';
+//                    }
+//                },
+                { "data": "GROSS_WEIGHT2", "width": "30px",
+                    "render": function ( data, type, full, meta ) {
+                    	 if(!data)
+    	                        data='';
+    	                    
+    	                    return '<input type="text" style="width:50px" name="gross_weight2" value = "'+data+'" class="form-control notsave" >';
                     }
                 },
                 { "data": "POL_NAME", "visible": false,
