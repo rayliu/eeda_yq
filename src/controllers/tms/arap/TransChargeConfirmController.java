7d0eb1d640e379142f534049c8c444d191380c19
victor.tan@eeda123.com <kevin.dou@eeda123.com>
2017-07-13 22:39:32 +0800

    运输模块：应收费用明细确认，提柜日期显示正确

--- src/controllers/tms/arap/TransChargeConfirmController.java
+++ src/controllers/tms/arap/TransChargeConfirmController.java
@@ -51,10 +51,10 @@ public class TransChargeConfirmController extends Controller {
      
         String sql = "select * from( "
         		+ " select tjoa.*,tjo.order_no,tjo.id jobid,tjo.container_no,tjo.so_no,CONVERT(substring(GROUP_CONCAT(tjo.create_stamp), 1, 10),char) create_stamp,tjo.customer_id,tjo.cabinet_type,"
-        		+ " CONVERT(substring(tjol.cabinet_date,1,10),char) cabinet_date,p.company_name customer,p1.company_name sp_name,f.name charge_name,u.name unit_name,c.name currency_name "
+        		+ " CONVERT(substring(REPLACE(GROUP_CONCAT(IF(unload_type!='移柜' and unload_type!='收重柜',tjol.cabinet_date,'')),',',''), 1, 11),char) cabinet_date,p.company_name customer,p1.company_name sp_name,f.name charge_name,u.name unit_name,c.name currency_name "
 				+ " from trans_job_order_arap tjoa "
 				+ " right join trans_job_order tjo on tjo.id=tjoa.order_id "
-				+ " LEFT JOIN trans_job_order_land_item tjol on tjol.order_id = tjoa.order_id"
+				+ " LEFT JOIN trans_job_order_land_item tjol on tjol.order_id = tjo.id"
 				+ " left join party p on p.id=tjo.customer_id "
 				+ " left join party p1 on p1.id=tjoa.sp_id "
 				+ " left join fin_item f on f.id=tjoa.charge_id "
@@ -67,11 +67,11 @@ public class TransChargeConfirmController extends Controller {
         
         String condition = DbUtils.buildConditions(getParaMap());
 
-        String sqlTotal = "select count(1) total from ("+sql+ condition+") B";
+        String sqlTotal = "select count(1) total from ("+sql+ condition+" ) B ";
         Record rec = Db.findFirst(sqlTotal);
         logger.debug("total records:" + rec.getLong("total"));
         //+sLimit
-        List<Record> orderList = Db.find(sql+ condition  );
+        List<Record> orderList = Db.find(sql+ condition +"order by cabinet_date"  );
         Map orderListMap = new HashMap();
         orderListMap.put("draw", pageIndex);
         orderListMap.put("recordsTotal", rec.getLong("total"));
