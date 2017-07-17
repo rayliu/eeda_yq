c1165b9d940feb535620bcd1d9e2ec55b5ad4ffb
beck <kevin.dou@eeda123.com>
2017-07-17 15:59:51 +0800

    增加案例上传功能和列表

--- WebRoot/js/BusinessAdmin/case/list.js
+++ WebRoot/js/BusinessAdmin/case/list.js
@@ -1,20 +1,83 @@
-define(['jquery', 'dataTablesBootstrap', 'validate_cn', 'sco'], function ($, metisMenu) {
-  $(document).ready(function() {
 
-      var dataTable = eeda.dt({
-          id: 'eeda_table',
-          paging: false,
-          serverSide: true, 
-          ajax: "/BusinessAdmin/ad/cu/list",
-          columns: [
-            { "data": "TITLE", "width": "100px"},
-            { "data": "DATE_FROM","width": "100px"},
-            { "data": null, "width": "100px",
-              render: function(data,type,full,meta){
-                return "<a class='stdbtn btn_blue' href='#'>编辑</a> <a class='stdbtn btn_blue' href='#'>删除</a>";
-              }
-            }
-          ]
-	    });
+define(['jquery', 'dataTablesBootstrap', 'validate_cn', 'sco','file_upload'], function ($, metisMenu) {
+  $(document).ready(function() {
+	  $("#picture_up").on("click",function(){
+		  $(this).fileupload({
+			  validation: {allowedExtensions: ['*']},
+			  autoUpload:true,
+			  url:"/BusinessAdmin/case/saveFile",
+			  dataType:"json",
+			  done:function (e, data) {
+	        		if(data){
+			    		$('#example_img').val(data.result.NAME);
+			    	
+			    		var imgPre =Id("example_img");
+			  		    imgPre.src = '/upload/'+data.result.NAME;
+			    	}else{
+			    		$.scojs_message('上传失败', $.scojs_message.TYPE_ERROR);
+			    	}
+			     },error:function () {
+		            alert('上传的时候出现了错误！');
+		        }
+		  })
+	  })
+	  
+	  function Id(id){
+		  return document.getElementById(id);
+	  }
+	  $("#upload").click(function(){
+		 var self=$(this);
+		 var example={};
+		 example.id=$("id").val();
+		 example.name=$("#example_name").val();
+		 example.picture=$("#example_img").val();
+		 $.post("/BusinessAdmin/case/save",{jsonStr:JSON.stringify(example)},function(data){
+  			 if(data){
+				 $.scojs_message('保存成功', $.scojs_message.TYPE_OK);
+				 self.attr('disabled',false);
+				 refleshTable();
+			 }else{
+				 $.scojs_message('保存失败', $.scojs_message.TYPE_ERROR);
+			 }  
+  		  }) 
+	  });
+	  
+
+		var dataTable = eeda.dt({
+	          id: 'eeda_table',
+	          paging: true,
+	          aLengthMenu:[5,10],
+	          serverSide: true, 
+	          ajax: "/BusinessAdmin/case/list",
+	          columns: [
+	            { "data": "NAME" ,"width": "50px"},
+	            { "data": "CREATE_TIME","class":"title", "width": "100px"},
+	            { "data": "BEGIN_DATE", "width": "100px",
+	            	render: function(data,type,full,meta){
+	            		
+	            			data =  "<a class='stdbtn btn_warning delBtn' " +
+	              				" data-id="+full.ID+" href='#eeda_table'>删除</a>";
+	            	
+	            		return data;
+	            	} 
+	            }
+	          ]
+			});
+			
+		 var refleshTable = function(){
+		    	  dataTable.ajax.url("/BusinessAdmin/case/list").load();
+		     }
+		 $("#eeda_table").on("click",".delBtn",function(){
+			 var self=$(this);
+			 var id=self.data("id");
+			 $.post("/BusinessAdmin/case/delete",{"id":id},function(data){
+				 if(data){
+					  $.scojs_message('删除成功', $.scojs_message.TYPE_OK);
+					  refleshTable();
+				 }else{
+					 $.scojs_message('删除失败', $.scojs_message.TYPE_ERROR);
+				 }
+			 })
+		 })
   });
 });
--- src/controllers/bizadmin/weddingcase/CaseController.java
+++ src/controllers/bizadmin/weddingcase/CaseController.java
@@ -1,5 +1,10 @@
 package controllers.bizadmin.weddingcase;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import interceptor.SetAttrLoginUserInterceptor;
 
 import org.apache.log4j.Logger;
@@ -7,8 +12,14 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.subject.Subject;
 
+import com.google.gson.Gson;
 import com.jfinal.aop.Before;
 import com.jfinal.core.Controller;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+import com.jfinal.upload.UploadFile;
+
+import controllers.profile.LoginUserController;
 
 @RequiresAuthentication
 @Before(SetAttrLoginUserInterceptor.class)
@@ -20,8 +31,23 @@ public class CaseController extends Controller {
 	public void index() {
 		render(getRequest().getRequestURI()+"/list.html");
 	}
-	
-
+	public void list(){
+	    String sLimit = "";
+        String pageIndex = getPara("draw");
+        if (getPara("start") != null && getPara("length") != null) {
+            sLimit = " LIMIT " + getPara("start") + ", " + getPara("length");
+        }
+		Long user_id = LoginUserController.getLoginUserId(this);
+		String sqlTol="select count(1) total from wc_case where creator = "+user_id+" order by create_time desc "+sLimit;
+		Record rec=Db.findFirst(sqlTol);
+		String sql="select * from wc_case where creator = "+user_id;
+		List example_list=Db.find(sql);
+		Map map=new HashMap();
+	    map.put("recordsTotal", rec.getLong("total"));
+	    map.put("recordsFiltered", rec.getLong("total"));
+		map.put("data", example_list);
+		renderJson(map);
+	}
 	public void modify_info(){
         String id = getPara("id");
         render(getRequest().getRequestURI()+"/edit.html");
@@ -31,4 +57,32 @@ public class CaseController extends Controller {
         String id = getPara("id");
         render(getRequest().getRequestURI()+"/edit.html");
     }
+	public void saveFile(){
+		Record re = new Record();
+    	try {
+            UploadFile file = getFile();
+            re.set("name", file.getFileName());
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    	renderJson(re);
+	}
+	public void save(){
+		String jsonStr = getPara("jsonStr");
+        Long userId = LoginUserController.getLoginUserId(this);
+        Gson gson = new Gson();  
+        Map<String, String> dto= gson.fromJson(jsonStr, HashMap.class);
+        Record example = new Record();
+        example.set("name", dto.get("name"));
+        example.set("picture_name", dto.get("picture"));
+        example.set("creator",userId);
+        example.set("create_time",new Date());
+        Db.save("wc_case", example);
+        renderJson(example);
+	}
+	public void delete(){
+		String id=getPara("id");
+		Db.deleteById("wc_case", id);
+		renderJson(true);
+	}
 }
