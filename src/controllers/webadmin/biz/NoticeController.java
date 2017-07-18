6104bd630db8bcfd0e5eca52b060ecf3b4316ffe
beck <kevin.dou@eeda123.com>
2017-07-18 15:38:08 +0800

    公告模块

--- src/controllers/webadmin/biz/NoticeController.java
+++ src/controllers/webadmin/biz/NoticeController.java
@@ -1,5 +1,8 @@
 package controllers.webadmin.biz;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import interceptor.SetAttrLoginUserInterceptor;
 
 import org.apache.log4j.Logger;
@@ -11,6 +14,7 @@ import com.jfinal.aop.Before;
 import com.jfinal.core.Controller;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
+import com.jfinal.plugin.activerecord.tx.Tx;
 
 import controllers.profile.LoginUserController;
 
@@ -24,6 +28,13 @@ public class NoticeController extends Controller {
 	public void index() {
 		render(getRequest().getRequestURI()+"/list.html");
 	}
-	
+	@Before(Tx.class)
+	public void update(){
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String content=getPara("content");
+		String sql="update price_maintain set remark= '"+content+"' , update_time= '"+df.format(new Date())+"' where type='公共信息'";
+		Db.update(sql);
+		renderJson(true);
+	}
 	
 }
