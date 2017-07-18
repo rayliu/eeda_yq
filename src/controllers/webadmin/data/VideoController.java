76cba67f40df14cc6c137bdc3cb60fa26847c4c0
beck <kevin.dou@eeda123.com>
2017-07-18 15:40:26 +0800

    视屏展示模块

--- src/controllers/webadmin/data/VideoController.java
+++ src/controllers/webadmin/data/VideoController.java
@@ -85,28 +85,25 @@ public class VideoController extends Controller {
     public void list(){
     	
     	UserLogin user = LoginUserController.getLoginUser(this);
+    	Long user_id = LoginUserController.getLoginUserId(this);
         long office_id=user.getLong("office_id");
         String sLimit = "";
         String pageIndex = getPara("draw");
         if (getPara("start") != null && getPara("length") != null) {
         	sLimit = " LIMIT " + getPara("start") + ", " + getPara("length");
         }
-         
-    	String sql = "select * from ("
-    			+ " select m.*, u.c_name create_name, u1.c_name update_name"
-        		+ " from msg_board m "
-        		+ " left join user_login u on u.id = m.creator"
-        		+ " left join user_login u1 on u1.id = m.updator"
-        		+ " where m.office_id="+office_id
-        		+ " ) A where 1=1 ";
-    	
+  
+    	String sql = " SELECT vc.*, ul.c_name productor "
+    			+ " FROM video_case vc LEFT JOIN user_login ul ON vc.creator = ul.id where vc.creator="+user_id+" "+sLimit;
+
     	String condition = DbUtils.buildConditions(getParaMap());
 
-        String sqlTotal = "select count(1) total from ("+sql+ condition+") B";
+        String sqlTotal = "select count(1) total from ("+sql+") B";
+        System.out.println(sqlTotal);
         Record rec = Db.findFirst(sqlTotal);
         logger.debug("total records:" + rec.getLong("total"));
         
-        List<Record> orderList = Db.find(sql+ condition + " order by create_stamp desc " +sLimit);
+        List<Record> orderList = Db.find(sql);
         Map map = new HashMap();
         map.put("draw", pageIndex);
         map.put("recordsTotal", rec.getLong("total"));
