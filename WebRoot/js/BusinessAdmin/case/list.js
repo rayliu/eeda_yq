fa6d4987b7ffeedc4bf47bb20fb7365a157fcd9a
beck <kevin.dou@eeda123.com>
2017-07-24 14:32:49 +0800

    案例图片的多图片上传

--- WebRoot/js/BusinessAdmin/case/list.js
+++ WebRoot/js/BusinessAdmin/case/list.js
@@ -1,5 +1,6 @@
 
 define(['jquery', 'dataTablesBootstrap', 'validate_cn', 'sco','file_upload'], function ($, metisMenu) {
+	var img_num;
   $(document).ready(function() {
 	  $("#picture_up").on("click",function(){
 		  $(this).fileupload({
@@ -21,16 +22,48 @@ define(['jquery', 'dataTablesBootstrap', 'validate_cn', 'sco','file_upload'], fu
 		        }
 		  })
 	  })
+	  $("#file_photo").on('click',function(){
+		  img_num = $('[name=img_photo]').size();
+		  img_num = img_num+1;
+		  var img = "<img style='width: 200px; height: 150px;margin-left:20px;display:none' name='img_photo'  class='shadow' id='img_photo"+img_num+"'>";
+		  $('#img_item').append(img);
+		  
+		  $(this).fileupload({
+				validation: {allowedExtensions: ['*']},
+				autoUpload: true, 
+			    url: '/BusinessAdmin/case/saveFile',
+			    dataType: 'json',
+		        done: function (e, data) {
+	        		if(data){
+	        			$('#img_photo'+img_num).show();
+			    		$.scojs_message('已选择', $.scojs_message.TYPE_OK);
+			    		$('#img_photo'+img_num).attr('value',data.result.NAME);
+			    		var imgPre =Id('img_photo'+img_num);
+			  		    imgPre.src = '/upload/'+data.result.NAME;
+			    	}else{
+			    		$.scojs_message('上传失败', $.scojs_message.TYPE_ERROR);
+			    	}
+			     },error: function () {
+		            alert('上传的时候出现了错误！');
+		        }
+		   });
 	  
+		  
+	  
+	  })
 	  function Id(id){
 		  return document.getElementById(id);
 	  }
-	  $("#upload").click(function(){
+	  $("#save_btn").click(function(){
 		 var self=$(this);
 		 var example={};
-		 example.id=$("id").val();
+		 example.img_num = img_num;
+		 example.id=$("#example_id").val();
 		 example.name=$("#example_name").val();
 		 example.picture=$("#example_img").val();
+		  	for(var i = 1;i <= img_num;i++){
+	    		example['photo'+i] = $('#img_photo'+i).attr('value');
+	    	}
 		 $.post("/BusinessAdmin/case/save",{jsonStr:JSON.stringify(example)},function(data){
   			 if(data){
 				 $.scojs_message('保存成功', $.scojs_message.TYPE_OK);
--- src/controllers/bizadmin/weddingcase/CaseController.java
+++ src/controllers/bizadmin/weddingcase/CaseController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import interceptor.SetAttrLoginUserInterceptor;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
@@ -29,6 +30,7 @@ public class CaseController extends Controller {
 	Subject currentUser = SecurityUtils.getSubject();
 
 	public void index() {
+		setAttr("example", new Record());
 		render(getRequest().getRequestURI()+"/list.html");
 	}
 	public void list(){
@@ -71,13 +73,51 @@ public class CaseController extends Controller {
 		String jsonStr = getPara("jsonStr");
         Long userId = LoginUserController.getLoginUserId(this);
         Gson gson = new Gson();  
-        Map<String, String> dto= gson.fromJson(jsonStr, HashMap.class);
-        Record example = new Record();
-        example.set("name", dto.get("name"));
-        example.set("picture_name", dto.get("picture"));
-        example.set("creator",userId);
-        example.set("create_time",new Date());
-        Db.save("wc_case", example);
+        Map<String, ?> dto= gson.fromJson(jsonStr, HashMap.class);
+        String id=(String) dto.get("id");
+        Double img_num = (Double) dto.get("img_num");
+        String name=(String) dto.get("name");
+        String picture_name=(String) dto.get("picture_name");
+        Record example=null;
+        if(StringUtils.isNotBlank(id)){
+        		example=Db.findById("wc_case", id);
+        		example.set("name", name);
+        		example.set("picture_name", picture_name);
+        		example.set("creator",userId);
+        		example.set("create_time",new Date());
+        		Db.update("wc_case", example);
+         	List<Record> orderItem = Db.find("select * from wc_case_item where order_id = ?",id);
+        	for (int i = 1; i <= img_num; i++) {
+        		int order_num = orderItem.size();
+        		Record re = null;
+        		if(i > order_num){
+        			re = new Record();
+        			re.set("photo", (String) dto.get("photo"+i));
+            		re.set("seq", i);
+            		re.set("order_id", id);
+                	Db.save("wc_case_item", re);
+        		}
+    		}
+        }else{
+        		example = new Record();
+        	 	example.set("name", name);
+     			example.set("picture_name", picture_name);
+     			example.set("creator",userId);
+     			example.set("create_time",new Date());
+     			 Db.save("wc_case", example);
+     			Record orderItem =null;
+            	for (int i = 1; i <= img_num; i++) {
+            	orderItem=new Record();
+            	orderItem.set("photo", (String) dto.get("photo"+i));
+            	orderItem.set("seq", i);
+            	orderItem.set("order_id", example.get("id"));
+            	Db.save("wc_case_item", orderItem);
+        		}
+        }
+       
+    
+       
+       
         renderJson(example);
 	}
 	public void delete(){
