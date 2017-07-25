1691074c52f74178b720f82b80c6eb10ade83932
Manning <kevin.dou@eeda123.com>
2017-07-25 16:19:59 +0800

    收款申请单：简单查询

--- WebRoot/js/eeda/arap/ChargeRequest/list.js
+++ WebRoot/js/eeda/arap/ChargeRequest/list.js
@@ -418,14 +418,14 @@ $(document).ready(function() {
     //返回标记
     var back=$('#back').val(); 
       //查询已申请单
-    $("#searchBtn1").click(function(){
+    $("#searchBtn").click(function(){
     	$('#checked_application_table').empty();
         
         back="";
         refreshData(back);
     });
 
-    $("#resetBtn1").click(function(){
+    $("#resetBtn").click(function(){
         $('#applicationForm')[0].reset();
         saveConditions();
     });
@@ -497,7 +497,29 @@ $(document).ready(function() {
     
     
     
-    
+    $('.complex_search').click(function(event) {
+        if($('.search_single').is(':visible')){
+          $('.search_single').hide();
+        }else{
+          $('.search_single').show();
+        }
+    });
+  //简单查询
+    $('#singleSearchBtn').click(function(){
+    	$('#checked_application_table').empty();
+    	singleSearchData();
+    });
+	var singleSearchData = function(){ 
+		var sp_id = $("#single_sp_id").val();
+		var service_stamp = $("#service_stamp_n").val();
+		var status = $("#status").val();
+	    var url = "/chargeRequest/applicationList?sp_id="+sp_id
+	     		 +"&service_stamp_between="+service_stamp
+	     		 +"&status="+status;
+	     application_table.ajax.url(url).load();
+	     totalMoney();
+	     saveConditions();
+	}
 
     //查询动作
     var refreshData=function(back){
