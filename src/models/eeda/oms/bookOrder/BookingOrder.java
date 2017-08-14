33418e2e6e9fa7df049fa01f1a4410bcaefaa3c7
victor.tan@eeda123.com <kevin.dou@eeda123.com>
2017-08-14 09:18:10 +0800

    补交文件

---
+++ src/models/eeda/oms/bookOrder/BookingOrder.java
@@ -0,0 +1,1 @@
+package models.eeda.oms.bookOrder;import com.jfinal.plugin.activerecord.Model;@SuppressWarnings("serial")public class BookingOrder extends Model<BookingOrder> {	public static final BookingOrder dao = new BookingOrder();}
