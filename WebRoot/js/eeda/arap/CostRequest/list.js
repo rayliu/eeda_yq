5ce36aa14f13f64f6e1cd8c0092564575eab104b
Ray Liu <liuyubie@gmail.com>
2017-08-26 16:11:23 +0800

    货代模块: 付款申请单-复核,  添加教程

--- WebRoot/js/eeda/arap/CostRequest/list.js
+++ WebRoot/js/eeda/arap/CostRequest/list.js
@@ -1,7 +1,11 @@
 
-define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco','datetimepicker_CN','jq_blockui','dtColReorder'], function ($, metisMenu) {
+define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco','datetimepicker_CN','jq_blockui','dtColReorder', 'pageguide'], function ($, metisMenu) {
 $(document).ready(function() {
 	
+    tl.pg.init({
+        pg_caption: '本页教程'
+    });
+    
 	var flash = function(){    
 	 	 $("#allCheck").prop("checked",$("#application_table .checkBox").length == $("#application_table .checkBox:checked").length ? true : false);
 	  };
