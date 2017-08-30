3499eebe7793e3c7751fa17626900949f775863d
Manning <kevin.dou@eeda123.com>
2017-08-30 11:27:19 +0800

    货代：应收余额汇总表导出Excel

--- src/controllers/arap/ChargeBalanceReportController.java
+++ src/controllers/arap/ChargeBalanceReportController.java
@@ -23,6 +23,7 @@ import com.jfinal.plugin.activerecord.Record;
 import controllers.eeda.ListConfigController;
 import controllers.profile.LoginUserController;
 import controllers.util.DbUtils;
+import controllers.util.PoiUtils;
 
 @RequiresAuthentication
 @Before(SetAttrLoginUserInterceptor.class)
@@ -230,6 +231,73 @@ public class ChargeBalanceReportController extends Controller {
 		renderJson(re);
 	}
 	
+	public void downloadExcelList(){
+		UserLogin user = LoginUserController.getLoginUser(this);
+		long office_id = user.getLong("office_id");
+		String sp_id = getPara("sp_id");
+		String begin_time = getPara("begin_time");
+		String end_time = getPara("end_time");
+		String spId = "";
+		String order_export_date = "";
+		if (StringUtils.isBlank(sp_id)) {
+			spId = "";
+		} else {
+			spId = " and A.sp_id = " + sp_id;
+		}
+		if (StringUtils.isBlank(begin_time)||StringUtils.isBlank(end_time)) {
+			order_export_date = "";
+		} else {
+			order_export_date =  " and (order_export_date between '"+begin_time+"' and '"+end_time+"')";
+		}
+
+		String condition = spId+order_export_date;
+
+		String sql = " SELECT A.id,A.customer_id,A.abbr,A.sp_id,A.employee_name,A.employee_id,sum(charge_cny) charge_cny,"
+        		+ " SUM(charge_usd) charge_usd,SUM(charge_jpy) charge_jpy,sum(charge_hkd) charge_hkd,"
+        		+ " SUM(uncharge_cny) uncharge_cny,SUM(uncharge_usd) uncharge_usd,sum(uncharge_jpy) uncharge_jpy,"
+        		+ " SUM(uncharge_hkd) uncharge_hkd,SUM(charge_rmb) charge_rmb,sum(uncharge_rmb) uncharge_rmb, "
+        		+ " round(((SUM(charge_rmb)-sum(uncharge_rmb))/sum(charge_rmb))*100,2)  payment_rate "
+        		+ " FROM (SELECT jo.id,jo.customer_id,jo.order_export_date,p.abbr,joa.sp_id,em.employee_name,em.id employee_id,IF (joa.order_type = 'charge'"
+        		+ " AND joa.exchange_currency_id = 3,exchange_total_amount,0) charge_cny,"
+        		+ " IF (joa.order_type = 'charge' AND joa.exchange_currency_id = 6,"
+        		+ " exchange_total_amount,0) charge_usd,IF (joa.order_type = 'charge'"
+        		+ " AND joa.exchange_currency_id = 8,exchange_total_amount,0) charge_jpy,"
+        		+ " IF (joa.order_type = 'charge' AND joa.exchange_currency_id = 9,"
+        		+ " exchange_total_amount,0) charge_hkd,"
+        		+ " IF (joa.order_type = 'charge' AND joa.exchange_currency_id = 3 AND pay_flag!='Y',"
+        		+ " exchange_total_amount,0) uncharge_cny,"
+        		+ " IF (joa.order_type = 'charge' AND joa.exchange_currency_id = 6 AND pay_flag!='Y',"
+        		+ " exchange_total_amount,0) uncharge_usd,"
+        		+ " IF (joa.order_type = 'charge' AND joa.exchange_currency_id = 8 AND pay_flag!='Y',"
+        		+ " exchange_total_amount,0) uncharge_jpy,"
+        		+ " IF (joa.order_type = 'charge' AND joa.exchange_currency_id = 9 AND pay_flag!='Y',"
+        		+ " exchange_total_amount,0) uncharge_hkd,"
+        		+ " IF (joa.order_type = 'charge',currency_total_amount,0) charge_rmb,"
+        		+ " IF (joa.order_type = 'charge' AND pay_flag!='Y',currency_total_amount,0) uncharge_rmb"
+        		+ " FROM job_order jo"
+        		+ " LEFT JOIN job_order_arap joa ON jo.id = joa.order_id"
+        		+ " LEFT JOIN party p ON p.id = joa.sp_id"
+        		+ " LEFT JOIN customer_salesman cs on cs.party_id = joa.sp_id"
+        		+ " LEFT JOIN employee em on em.id = cs.salesman_id"
+        		+ " WHERE p.office_id =" +office_id+" "
+        		+ " and jo.delete_flag = 'N'"
+				+ " ) A"
+        		+ " WHERE A.sp_id IS NOT NULL AND A.charge_rmb!=0"+condition
+        		+ " GROUP BY A.sp_id"
+        		+ " ORDER BY uncharge_rmb desc";
+
+        String sqlExport = sql;
+		String total_name_header = "结算公司,CNY(应收),USD(应收),JPY(应收),HKD(应收),折合CNY(应付),CNY(应付),USD(应付),JPY(应付),HKD(应付),折合CNY(应付),回款率";
+		String[] headers = total_name_header.split(",");
+
+		String[] fields = { "ABBR", "CHARGE_CNY", "CHARGE_USD", "CHARGE_JPY",
+				"CHARGE_HKD", "CHARGE_RMB", "UNCHARGE_CNY","UNCHARGE_USD","UNCHARGE_JPY","UNCHARGE_HKD","UNCHARGE_RMB","PAYMENT_RATE"};
+		
+		String exportName = "";
+		
+		String fileName = PoiUtils.generateExcel(headers, fields, sqlExport,exportName);
+		renderText(fileName);
+	}
 	
 	
 	
