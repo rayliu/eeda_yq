f43ba1e65bcd75654cb2fd34a10c584e11b8299a
kevin <kevin.dou@eeda123.com>
2017-08-31 12:18:03 +0800

    供应链:利润表导出提示有误(提示错误)

--- WebRoot/js/eeda/arap/ProfitAndPaymentRate/profitList.js
+++ WebRoot/js/eeda/arap/ProfitAndPaymentRate/profitList.js
@@ -298,11 +298,11 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
     	  $.post('/profit/downloadExcelList',{customer_id:customer_id,begin_time:begin_time,end_time:end_time}, function(data){
               $('#exportTotaledExcel').prop('disabled', false);
               $('#singlexportTotaledExcel').prop('disabled', false);
-              $.scojs_message('生成应收Excel对账单成功', $.scojs_message.TYPE_OK);
+              $.scojs_message('导出利润表excel文件成功', $.scojs_message.TYPE_OK);
               window.open(data);
           }).fail(function() {
               $('#exportTotaledExcel').prop('disabled', false);
-              $.scojs_message('生成应收Excel对账单失败', $.scojs_message.TYPE_ERROR);
+              $.scojs_message('导出利润表excel文件失败', $.scojs_message.TYPE_ERROR);
           });
       }
   });
