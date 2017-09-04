45d774420dd93212f8f4207789c10cb752bafe43
Manning <kevin.dou@eeda123.com>
2017-09-04 14:48:15 +0800

    车队：应收应付余额统计报表导出Excel

--- WebRoot/js/eeda/cms/arap/customProfitAndPaymentRate/list.js
+++ WebRoot/js/eeda/cms/arap/customProfitAndPaymentRate/list.js
@@ -48,6 +48,14 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
 					return eeda.numFormat(data,3);
 				  }
 	            },
+	            { "data": "CHARGE_RMB", "width": "120px","class":"charge_rmb",
+	            	"render": function(data, type, full, meta) {
+            	    if(data==0){
+            	    	return '';
+            	    }
+					return eeda.numFormat(data,3);
+				  }
+	            },
 	            { "data": "COST_CNY", "width": "100px" ,"class":"cost_cny",
 	            	"render": function(data, type, full, meta) {
             	    if(data==0){
@@ -73,14 +81,6 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
 				  }
 	            },
 	            { "data": "COST_HKD", "width": "100px","class":"cost_hkd",
-	            	"render": function(data, type, full, meta) {
-            	    if(data==0){
-            	    	return '';
-            	    }
-					return eeda.numFormat(data,3);
-				  }
-	            },
-	            { "data": "CHARGE_RMB", "width": "120px","class":"charge_rmb",
 	            	"render": function(data, type, full, meta) {
             	    if(data==0){
             	    	return '';
@@ -114,6 +114,96 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
           });
       });
       
+      $('.complex_search').click(function(event) {
+          if($('.search_single').is(':visible')){
+            $('.search_single').hide();
+          }else{
+            $('.search_single').show();
+          }
+      });
+    //简单查询
+      $('#selected_field').change(function(event) {
+	      var selectField = $('#selected_field').val();
+	      if(selectField=='sp_id'){
+	    	  $("#single_date_custom_begin_time").val("");
+	    	  $("#single_date_custom_end_time").val("");
+	    	  $("#date_custom_show").hide();
+	    	  $("#sp_id_show").show();
+	      }
+	      if(selectField=="date_custom"){
+	    	  $("#single_sp_id_input").val("");
+	    	  $("#single_sp_id").val("");
+	    	  $("#sp_id_show").hide();
+	    	  $("#date_custom_show").show();
+	      }
+     });
+	
+	$('#singleSearchBtn').click(function(){
+		var selectField = $("#selected_field").val();
+		  if(selectField=='sp_id'){
+			  var sp_id = $("#single_sp_id").val();
+	      }
+	      if(selectField=="date_custom"){
+	    	  var date_custom_begin_time = $("#single_date_custom_begin_time").val();
+			  var date_custom_end_time = $("#single_date_custom_end_time").val();
+	      }
+	      
+	      
+	      
+	      var url = "/customProfitAndPaymentRate/list?sp_id="+sp_id
+			+"&date_custom_begin_time="+date_custom_begin_time
+	        +"&date_custom_end_time="+date_custom_end_time;
+	      	dataTable.ajax.url(url).load(tableStyle);
+	      	
+	      //合计字段
+	          $.post('customProfitAndPaymentRate/listTotal',{
+	        	  sp_id:sp_id,
+	        	  date_custom_begin_time:date_custom_begin_time,
+	        	  date_custom_end_time:date_custom_end_time
+	          },function(data){
+	        	  var charge_cny = parseFloat(data.CHARGE_CNY).toFixed(2);
+	        	  var charge_usd = parseFloat(data.CHARGE_USD).toFixed(2);
+	        	  var charge_jpy = parseFloat(data.CHARGE_JPY).toFixed(2);
+	        	  var charge_hkd = parseFloat(data.CHARGE_HKD).toFixed(2);
+	        	  var total_charge = parseFloat(data.TOTAL_CHARGE).toFixed(2);
+	        	  var cost_cny = parseFloat(data.COST_CNY).toFixed(2);
+	        	  var cost_usd = parseFloat(data.COST_USD).toFixed(2);
+	        	  var cost_jpy = parseFloat(data.COST_JPY).toFixed(2);
+	        	  var cost_hkd = parseFloat(data.COST_HKD).toFixed(2);
+	        	  var total_cost = parseFloat(data.TOTAL_COST).toFixed(2);
+	        	  $('#CNY_charge_tatol').text(charge_cny);
+	        	  $('#USD_charge_tatol').text(charge_usd);
+	        	  $('#JPY_charge_tatol').text(charge_jpy);
+	        	  $('#HKD_charge_tatol').text(charge_hkd);
+	        	  $('#total_charge').text(total_charge);
+	        	  $('#CNY_cost_tatol').text(cost_cny);
+	        	  $('#USD_cost_tatol').text(cost_usd);
+	        	  $('#JPY_cost_tatol').text(cost_jpy);
+	        	  $('#HKD_cost_tatol').text(cost_hkd);
+	        	  $('#total_cost').text(total_cost);
+	        	  var total=parseFloat(data.TOTAL);
+//	        	  $($('.dataTables_scrollFoot tr')[0]).find('th[class=abbr]').html('共'+total+'项汇总：');
+//	        	  $($('.dataTables_scrollFoot tr')[0]).find('th[class=charge_cny]').html("应收CNY"+eeda.numFormat(charge_cny,3));
+//	        	  $($('.dataTables_scrollFoot tr')[0]).find('th[class=charge_usd]').html("应收USD"+eeda.numFormat(charge_usd,3));
+//	        	  $($('.dataTables_scrollFoot tr')[0]).find('th[class=charge_jpy]').html("应收JPY"+eeda.numFormat(charge_jpy,3));
+//	        	  $($('.dataTables_scrollFoot tr')[0]).find('th[class=charge_hkd]').html("应收HKD"+eeda.numFormat(charge_hkd,3));
+//	        	  $($('.dataTables_scrollFoot tr')[0]).find('th[class=cost_cny]').html("应付CNY"+eeda.numFormat(total_charge,3));
+//	        	  $($('.dataTables_scrollFoot tr')[0]).find('th[class=cost_usd]').html("应付USD"+eeda.numFormat(cost_cny,3));
+//	        	  $($('.dataTables_scrollFoot tr')[0]).find('th[class=cost_jpy]').html("应付JPY"+eeda.numFormat(cost_usd,3));
+//	        	  $($('.dataTables_scrollFoot tr')[0]).find('th[class=cost_hkd]').html("应付HKD"+eeda.numFormat(cost_jpy,3));
+//	        	  $($('.dataTables_scrollFoot tr')[0]).find('th[class=charge_rmb]').html("折合应收(RMB)"+eeda.numFormat(cost_hkd,3));
+//	        	  $($('.dataTables_scrollFoot tr')[0]).find('th[class=cost_rmb]').html("折合应付(RMB)"+eeda.numFormat(total_cost,3));
+	        	  
+	        	  var total_profit=parseFloat(total_charge-total_cost).toFixed(2);
+	        	  if(total_profit<0){
+	        		  $('#total_profit').text(total_profit).css('color','red');
+	        	  }else(
+	        		  $('#total_profit').text(total_profit)
+	        	  )
+
+	          });
+	}); 
+      
       $('#resetBtn').click(function(e){
           $("#orderForm")[0].reset();
       });
@@ -124,8 +214,8 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
 
      var searchData=function(){
           var sp_id = $("#sp_id").val(); 
-          var order_export_date_begin_time = $("#order_export_date_begin_time").val();
-          var order_export_date_end_time = $("#order_export_date_end_time").val();
+          var date_custom_begin_time = $("#date_custom_begin_time").val();
+          var date_custom_end_time = $("#date_custom_end_time").val();
           /*  
               查询规则：参数对应DB字段名
               *_no like
@@ -137,8 +227,8 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
           //合计字段
           $.post('customProfitAndPaymentRate/listTotal',{
         	  sp_id:sp_id,
-        	  order_export_date_begin_time:order_export_date_begin_time,
-        	  order_export_date_end_time:order_export_date_end_time
+        	  date_custom_begin_time:date_custom_begin_time,
+        	  date_custom_end_time:date_custom_end_time
           },function(data){
         	  var charge_cny = parseFloat(data.CHARGE_CNY).toFixed(2);
         	  var charge_usd = parseFloat(data.CHARGE_USD).toFixed(2);
@@ -183,22 +273,63 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
           });
           
           var cssTd=function(){
-        	  $("#eeda_table th:eq(6)").css('background-color','#f5f5dc');
         	  $("#eeda_table td:nth-child(6)").css('background-color','#f5f5dc');
-        	  $("#eeda_table td:nth-child(7)").css('background-color','#f5f5dc');
-        	  $("#eeda_table td:nth-child(8)").css('background-color','#f5f5dc');
-        	  $("#eeda_table td:nth-child(9)").css('background-color','#f5f5dc');
+        	  $("#eeda_table td:nth-child(11)").css('background-color','#f5f5dc');
           }
          
           
           var url = "/customProfitAndPaymentRate/list?sp_id="+sp_id
-				          +"&order_export_date_begin_time="+order_export_date_begin_time
-				          +"&order_export_date_end_time="+order_export_date_end_time;
+				          +"&date_custom_begin_time="+date_custom_begin_time
+				          +"&date_custom_end_time="+date_custom_end_time;
           dataTable.ajax.url(url).load(cssTd);
           
-         
-          
       };
+      
       searchData(); 
+      
+      var tableStyle = function(){
+      	  $('.oneRow').css('line-height','30px');
+      	  $('.doubleRow').css('text-align','center');
+      	  
+      	  var tableName = "eeda_table";
+      	  //格式【合成表头的第一列位置，合成的列数，颜色】
+      	  var array= [[2,5,'#f8fff0'],[7,5,'#eeffff']];
+      	  for (var i = 0; i < array.length; i++) {
+      		  var firstChild = array[i][0];
+          	  var cols = array[i][1];
+          	  var bgColor = array[i][2];
+          	  for (var j = firstChild; j < (firstChild+cols); j++) {
+          		  $("#"+tableName+" td:nth-child("+j+")").css('background-color',bgColor);
+          	  }
+    		  }
+        }
+      
+    //导出excel
+      $('#exportTotaledExcel1').click(function(){
+    	  $(this).attr('disabled', true);
+          var sp_id = $("#single_sp_id").val();
+          var begin_time = $("#single_date_custom_begin_time").val();
+          var end_time = $("#single_date_custom_end_time").val();
+          excel_method(sp_id,begin_time,end_time);
+      });
+      $('#exportTotaledExcel').click(function(){
+    	  $(this).attr('disabled', true);
+          var sp_id = $("#sp_id").val();
+          var begin_time = $("#date_custom_begin_time").val();
+          var end_time = $("#date_custom_end_time").val();
+          excel_method(sp_id,begin_time,end_time);
+      });
+      var excel_method = function(sp_id,begin_time,end_time){
+		  $.post('/customProfitAndPaymentRate/downloadExcelList',{sp_id:sp_id,begin_time:begin_time,end_time:end_time}, function(data){
+	          $('#exportTotaledExcel1').prop('disabled', false);
+	          $('#exportTotaledExcel').prop('disabled', false);
+	          $.scojs_message('生成应收Excel对账单成功', $.scojs_message.TYPE_OK);
+	          window.open(data);
+	      }).fail(function() {
+	          $('#exportTotaledExcel').prop('disabled', false);
+	          $('#singlexportTotaledExcel').prop('disabled', false);
+	          $.scojs_message('生成应收Excel对账单失败', $.scojs_message.TYPE_ERROR);
+	      });
+      }
   });
 });
--- src/controllers/cms/arap/CustomProfitAndPaymentRateController.java
+++ src/controllers/cms/arap/CustomProfitAndPaymentRateController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 import models.UserLogin;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.subject.Subject;
@@ -22,6 +23,7 @@ import com.jfinal.plugin.activerecord.Record;
 import controllers.eeda.ListConfigController;
 import controllers.profile.LoginUserController;
 import controllers.util.DbUtils;
+import controllers.util.PoiUtils;
 
 @RequiresAuthentication
 @Before(SetAttrLoginUserInterceptor.class)
@@ -65,6 +67,7 @@ public class CustomProfitAndPaymentRateController extends Controller {
         		+"  LEFT JOIN party p ON p.id = joa.sp_id"
         		+"  WHERE jo.office_id ="+office_id+" "+condition
         		+ " and jo.delete_flag = 'N'"
+        		+ " and joa.sp_id IS NOT NULL"
     			+" ) A where A.sp_id is NOT NULL GROUP BY A.sp_id  ORDER BY abbr";
 		
         String sqlTotal = "select count(1) total from ("+sql+") C";
@@ -84,29 +87,29 @@ public class CustomProfitAndPaymentRateController extends Controller {
 	
 	public void listTotal() {
 		String spid =(String) getPara("sp_id");
-		String order_export_date_begin_time =(String) getPara("order_export_date_begin_time");
-		String order_export_date_end_time =(String) getPara("order_export_date_end_time");
+		String date_custom_begin_time =(String) getPara("date_custom_begin_time");
+		String date_custom_end_time =(String) getPara("date_custom_end_time");
 		
 		UserLogin user = LoginUserController.getLoginUser(this);
         long office_id=user.getLong("office_id");
 		
 		String sp_id =" and sp_id="+spid;
-		if(" and sp_id=".equals(sp_id)){
+		if(" and sp_id=".equals(sp_id)||spid==null){
 			sp_id="";
 		}
-		if(order_export_date_begin_time==null){
-			order_export_date_begin_time="";
+		if(date_custom_begin_time==null){
+			date_custom_begin_time="";
 		}
-		if(order_export_date_end_time==null){
-			order_export_date_end_time="";
+		if(date_custom_end_time==null){
+			date_custom_end_time="";
 		}
 		
-		String order_export_date =  " and (order_export_date between '"+order_export_date_begin_time+"' and '"+order_export_date_end_time+"')";
+		String date_custom =  " and (date_custom between '"+date_custom_begin_time+"' and '"+date_custom_end_time+"')";
 
-		if(order_export_date_begin_time==""||order_export_date_begin_time==""){
-			order_export_date="";
+		if(date_custom_begin_time==""||date_custom_end_time==""){
+			date_custom="";
 		}
-		String condition = sp_id+order_export_date;
+		String condition = sp_id+date_custom;
 		
 		String sql=" SELECT "
 			+"	(SELECT "
@@ -153,6 +156,7 @@ public class CustomProfitAndPaymentRateController extends Controller {
 			+"	  WHERE jo.office_id = "+office_id+" and joa.currency_id = 3 "
 			+"	  and joa.order_type = 'cost' "+condition
 			+ " and jo.delete_flag = 'N'"
+			+ " and joa.sp_id IS NOT NULL"
 			+"	) cost_cny,"
 			+"	(SELECT "
 			+"	IFNULL(SUM(joa.total_amount),0)"
@@ -189,6 +193,7 @@ public class CustomProfitAndPaymentRateController extends Controller {
 			+"	WHERE 	jo.office_id = "+office_id
 			+"	AND joa.order_type = 'charge' "+condition
 			+ " and jo.delete_flag = 'N'"
+			+ " and joa.sp_id IS NOT NULL"
 			+") total_charge,"
 			+"	(SELECT "
 			+"		IFNULL(SUM(joa.total_amount),	0) "
@@ -198,12 +203,67 @@ public class CustomProfitAndPaymentRateController extends Controller {
 			+"	WHERE 	jo.office_id = "+office_id
 			+"	AND joa.order_type = 'cost' "+condition
 			+ " and jo.delete_flag = 'N'"
+			+ " and joa.sp_id IS NOT NULL"
 			+") total_cost";
 		
 		Record re = Db.findFirst(sql);
 		long total=list();
 		re.set("total", total);
 		renderJson(re);
+	}
+	
+	public void downloadExcelList(){
+		UserLogin user = LoginUserController.getLoginUser(this);
+		long office_id = user.getLong("office_id");
+		String sp_id = getPara("sp_id");
+		String begin_time = getPara("begin_time");
+		String end_time = getPara("end_time");
+		String spId = "";
+		String date_custom = "";
+		if (StringUtils.isBlank(sp_id)) {
+			spId = "";
+		} else {
+			spId = " and joa.sp_id = " + sp_id;
+		}
+		if (StringUtils.isBlank(begin_time)||StringUtils.isBlank(end_time)) {
+			date_custom = "";
+		} else {
+			date_custom =  " and (date_custom between '"+begin_time+"' and '"+end_time+"')";
+		}
+
+		String condition = spId+date_custom;
+
+		 String sql = " SELECT A.id,A.receive_sent_consignee,A.abbr,A.sp_id,sum(charge_cny) charge_cny,SUM(charge_usd) charge_usd,SUM(charge_jpy) charge_jpy,sum(charge_hkd) charge_hkd,SUM(cost_cny) cost_cny,SUM(cost_usd) cost_usd,"
+	        		+" sum(cost_jpy) cost_jpy,SUM(cost_hkd) cost_hkd,SUM(charge_rmb) charge_rmb,sum(cost_rmb) cost_rmb FROM ("
+	        		+" SELECT jo.id,jo.receive_sent_consignee,p.abbr,joa.sp_id,"
+	        		+" IF(joa.order_type='charge' and joa.currency_id = 3,total_amount,0) charge_cny,"
+	        		+"	IF(joa.order_type='charge' and joa.currency_id = 6,total_amount,0) charge_usd,"
+	        		+"	IF(joa.order_type='charge' and joa.currency_id = 8,total_amount,0) charge_jpy,"
+		    		+"	IF(joa.order_type='charge' and joa.currency_id = 9,total_amount,0) charge_hkd,"
+		    		+"	IF(joa.order_type='cost' and joa.currency_id = 3,total_amount,0) cost_cny,"
+		    		+"	IF(joa.order_type='cost' and joa.currency_id = 6,total_amount,0) cost_usd,"
+		    		+"	IF(joa.order_type='cost' and joa.currency_id = 8,total_amount,0) cost_jpy,"
+		    		+"	IF(joa.order_type='cost' and joa.currency_id = 9,total_amount,0) cost_hkd,"
+		    		+"	if(joa.order_type='charge',total_amount,0) charge_rmb,"
+		    		+"	if(joa.order_type='cost',total_amount,0) cost_rmb"
+	        		+"  from custom_plan_order jo "
+	        		+"  LEFT JOIN custom_plan_order_arap joa on jo.id = joa.order_id "
+	        		+"  LEFT JOIN party p ON p.id = joa.sp_id"
+	        		+"  WHERE jo.office_id ="+office_id+" "+condition
+	        		+ " and jo.delete_flag = 'N'"
+	    			+" ) A where A.sp_id is NOT NULL GROUP BY A.sp_id  ORDER BY abbr";
+
+        String sqlExport = sql;
+		String total_name_header = "结算公司,应收CNY(),应收(USD),应收(JPY),应收(HKD),折合应收(RMB),应付(CNY),应付(USD),应付(JPY),应付(HKD),折合应付(RMB)";
+		String[] headers = total_name_header.split(",");
+
+		String[] fields = { "ABBR", "CHARGE_CNY", "CHARGE_USD", "CHARGE_JPY",
+				"CHARGE_HKD","CHARGE_RMB","COST_CNY","COST_USD","COST_JPY","COST_HKD","COST_RMB"};
+		
+		String exportName = "";
+		
+		String fileName = PoiUtils.generateExcel(headers, fields, sqlExport,exportName);
+		renderText(fileName);
 	}
 	
 }
