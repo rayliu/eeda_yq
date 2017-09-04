ef3676562b14c91c880dadf0ed725a74328a0f1b
kevin <Keivn.dou@eeda123.com>
2017-09-04 16:00:05 +0800

    供应链：贸易工作单贸易信息明细导入优化（无需刷新即可正常导入）

--- WebRoot/js/eeda/tr/trJobOrder/edit_file_upload.js
+++ WebRoot/js/eeda/tr/trJobOrder/edit_file_upload.js
@@ -28,35 +28,41 @@ define(['jquery', 'file_upload' ,'sco'], function ($, metisMenu) {
 		// 贸易商品信息表
 		var order_id = $('#order_id').val();
 	    $("#import_tradeItem").click(function(){
+	    	order_id = $('#order_id').val();
+	    	fileUpload(order_id);
 	    	if($('#order_id').val() == ''){
 	    		$.scojs_message('先保存订单才可导入', $.scojs_message.TYPE_ERROR);
 	    		return false;
 	    	}
 	    	
 	    	$("#importFileUpload").click();
-	    	order_id = $('#order_id').val();
+	    	
 	    });
-	    var str = null;
-	    var errCustomerNo = null;
-	    var errCustomerNoArr = [];
-		$('#importFileUpload').fileupload({
-	        dataType: 'json',
-	        url: '/importOrder?order_type=tradeJobOrder&order_id='+$('#order_id').val(),
-	        done: function (e,data) {
-	        	$("#footer").show();
-	        	$("#msgLoad").empty().append('<h4>'+data.result.CAUSE+'</h4>');
-	        },  
-	        progressall: function (e, data) {//设置上传进度事件的回调函数  
-	        	str = null;
-	            errCustomerNo = null;
-	            errCustomerNoArr = [];
-	        	$('#msgLoad').empty().append('<center><img src="/yh/image/loading5.gif" width="20%"><h4>导入过程可能需要一点时间，请勿退出页面！</h4></center>');
-	        	$('#myModal').modal('show');
-	        	$("#footer").hide();
-	        } 
-	    },'json').error(function (jqXHR, textStatus, errorThrown) {
-	        alert("出错了，请刷新页面重新尝试。")
-	        console.log(errorThrown);
-	    });
+	    
+	    var fileUpload = function(order_id){
+	    	var str = null;
+		    var errCustomerNo = null;
+		    var errCustomerNoArr = [];
+			$('#importFileUpload').fileupload({
+		        dataType: 'json',
+		        url: '/importOrder?order_type=tradeJobOrder&order_id='+order_id,
+		        done: function (e,data) {
+		        	$("#footer").show();
+		        	$("#msgLoad").empty().append('<h4>'+data.result.CAUSE+'</h4>');
+		        },  
+		        progressall: function (e, data) {//设置上传进度事件的回调函数  
+		        	str = null;
+		            errCustomerNo = null;
+		            errCustomerNoArr = [];
+		        	$('#msgLoad').empty().append('<center><img src="/yh/image/loading5.gif" width="20%"><h4>导入过程可能需要一点时间，请勿退出页面！</h4></center>');
+		        	$('#myModal').modal('show');
+		        	$("#footer").hide();
+		        } 
+		    },'json').error(function (jqXHR, textStatus, errorThrown) {
+		        alert("出错了，请刷新页面重新尝试。")
+		        console.log(errorThrown);
+		    });
+	    }
+	    
 	});
 });
