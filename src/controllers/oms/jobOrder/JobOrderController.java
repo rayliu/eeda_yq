531ba44b5f01f2ea9cce1db48e7a1051c8f0e4e6
kevin <kevin.dou@eeda123.com>
2017-09-06 09:02:53 +0800

    供应链：工作单保存优化

--- src/controllers/oms/jobOrder/JobOrderController.java
+++ src/controllers/oms/jobOrder/JobOrderController.java
@@ -2444,7 +2444,7 @@ public class JobOrderController extends Controller {
     //常用明细列表字段保存进入历史记录
     @Before(Tx.class)
     private void saveItemParamHistory(List<Map<String, String>> list,List<Record> listRes){
-    	if(list.size() > 0){
+    	if(list != null){
     		for(Record listRe : listRes){
     			String type = listRe.getStr("type");
     			String param = listRe.getStr("param");
