79e4cc3ae41c9c757ebc7f9a7de5fbfa5b4a290d
beck <kevin.dou@eeda123.com>
2017-09-13 15:00:50 +0800

    报关：所有导出Excel提醒修复

--- WebRoot/js/eeda/cms/arap/customProfitAndPaymentRate/list.js
+++ WebRoot/js/eeda/cms/arap/customProfitAndPaymentRate/list.js
@@ -323,12 +323,12 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn'
 		  $.post('/customProfitAndPaymentRate/downloadExcelList',{sp_id:sp_id,begin_time:begin_time,end_time:end_time}, function(data){
 	          $('#exportTotaledExcel1').prop('disabled', false);
 	          $('#exportTotaledExcel').prop('disabled', false);
-	          $.scojs_message('生成应收Excel对账单成功', $.scojs_message.TYPE_OK);
+	          $.scojs_message('生成Excel成功', $.scojs_message.TYPE_OK);
 	          window.open(data);
 	      }).fail(function() {
 	          $('#exportTotaledExcel').prop('disabled', false);
 	          $('#singlexportTotaledExcel').prop('disabled', false);
-	          $.scojs_message('生成应收Excel对账单失败', $.scojs_message.TYPE_ERROR);
+	          $.scojs_message('生成Excel失败', $.scojs_message.TYPE_ERROR);
 	      });
       }
   });
