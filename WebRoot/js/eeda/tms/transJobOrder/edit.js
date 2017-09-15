ba522912dd3b114e5d9dce62f7077b6ba0de2bc4
beck <kevin.dou@eeda123.com>
2017-09-15 17:15:24 +0800

    车队：工作单加一列封条号

--- WebRoot/js/eeda/tms/transJobOrder/edit.js
+++ WebRoot/js/eeda/tms/transJobOrder/edit.js
@@ -196,6 +196,7 @@ $(document).ready(function() {
         order.remark = $('#remark').val();
         order.container_no = $("#container_no").val();
         order.so_no = $("#so_no").val();
+        order.seal_no = $("#seal_no").val();
         order.lading_no = $("#lading_no").val();
         order.cabinet_type = $("#cabinet_type").val();
         order.head_carrier = $("#head_carrier").val();
