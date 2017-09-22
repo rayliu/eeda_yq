3b9ea0deef75895608ab29a1dbf2523dea1a7c31
Manning <kevin.dou@eeda123.com>
2017-09-22 14:44:04 +0800

    office_id控制修复有to_office_id的一样可以看

--- src/controllers/util/OrderCheckOfficeUtil.java
+++ src/controllers/util/OrderCheckOfficeUtil.java
@@ -1,5 +1,6 @@
 package controllers.util;
 
+
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
 
@@ -7,7 +8,14 @@ public class OrderCheckOfficeUtil {
 	public static boolean checkOfficeEqual(String tableName, Long orderId, Long userOfficeId) {
 		Record rec = Db.findById(tableName, orderId);
 		Long officeId = rec.getLong("office_id");
-		if(officeId == userOfficeId)
+		Long toOfficeId = (long)897984654;
+		if(rec.getStr("to_office_id")==null){
+			toOfficeId = (long)897984654;
+		}else{
+			toOfficeId = Long.parseLong(rec.getStr("to_office_id"));
+		}
+		
+		if(officeId == userOfficeId||userOfficeId == toOfficeId)
 		    return true;
 		return false;
 	}
