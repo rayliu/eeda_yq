211a04da1ba9719ac05e245a846d294a4f5f440d
Manning <kevin.dou@eeda123.com>
2017-09-26 12:48:26 +0800

    货代：箱形加office_id控制

--- src/controllers/profile/ContainerTypeController.java
+++ src/controllers/profile/ContainerTypeController.java
@@ -33,6 +33,7 @@ import com.jfinal.plugin.activerecord.Record;
 
 import controllers.eeda.ListConfigController;
 import controllers.util.DbUtils;
+import controllers.util.OrderCheckOfficeUtil;
 import controllers.util.ParentOffice;
 import controllers.util.PermissionConstant;
 
@@ -64,12 +65,14 @@ public class ContainerTypeController extends Controller {
     
     public void list() {
         String sLimit = "";
+        UserLogin user1 = LoginUserController.getLoginUser(this);
+        long office_id=user1.getLong("office_id");
         String pageIndex = getPara("sEcho");
         if (getPara("iDisplayStart") != null && getPara("iDisplayLength") != null) {
             sLimit = " LIMIT " + getPara("iDisplayStart") + ", " + getPara("iDisplayLength");
         }
 
-        String sql = "SELECT * from container_type where 1 =1 ";
+        String sql = "SELECT * from container_type where 1 =1 and office_id="+office_id;
         
         String condition = DbUtils.buildConditions(getParaMap());
 
@@ -92,6 +95,13 @@ public class ContainerTypeController extends Controller {
     @Before(EedaMenuInterceptor.class)
     public void edit() {
         String id = getPara("id");
+        UserLogin user1 = LoginUserController.getLoginUser(this);
+        long office_id=user1.getLong("office_id");
+        //判断与登陆用户的office_id是否一致
+        if(office_id !=1 && !OrderCheckOfficeUtil.checkOfficeEqual("container_type", Long.valueOf(id), office_id)){
+        	renderError(403);// no permission
+            return;
+        }
         ContainerType u = ContainerType.dao.findById(id);
         setAttr("order", u);
         
@@ -121,7 +131,8 @@ public class ContainerTypeController extends Controller {
             PermissionConstant.PERMSSION_T_UPDATE }, logical = Logical.OR)
     public void save() {
         String jsonStr=getPara("params");
-        
+        UserLogin user1 = LoginUserController.getLoginUser(this);
+        long office_id=user1.getLong("office_id");
         Gson gson = new Gson();  
         Map<String, ?> dto= gson.fromJson(jsonStr, HashMap.class);  
         
@@ -129,8 +140,9 @@ public class ContainerTypeController extends Controller {
         String container_type = (String) dto.get("name");
         if (StringUtils.isBlank(id)) {
             ContainerType r = new ContainerType();
-
-            boolean s = r.set("container_type", container_type).save();
+            r.set("container_type", container_type);
+            r.set("office_id", office_id);
+            boolean s = r.save();
             if (s == true) {
                 renderJson(r);
             }
