9f783355a4c0ec04b644206d6614887e63ba733c
Manning <kevin.dou@eeda123.com>
2017-09-29 13:31:18 +0800

    货代：发票明细文件补交

---
+++ src/models/eeda/oms/jobOrder/ChargeRequestInvoices.java
@@ -0,0 +1,9 @@
+package models.eeda.oms.jobOrder;
+
+import com.jfinal.plugin.activerecord.Model;
+
+@SuppressWarnings("serial")
+public class ChargeRequestInvoices  extends Model<ChargeRequestInvoices>{
+		public static final ChargeRequestInvoices dao = new ChargeRequestInvoices();
+
+}
