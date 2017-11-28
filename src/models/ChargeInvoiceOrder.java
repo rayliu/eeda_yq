98983a7ee8a3050d88c2cca9f6be4dd632ddbfd5
Manning <kevin.dou@eeda123.com>
2017-11-28 11:22:12 +0800

    货代开票文件补交

---
+++ src/models/ChargeInvoiceOrder.java
@@ -0,0 +1,8 @@
+package models;
+
+import com.jfinal.plugin.activerecord.Model;
+
+
+public class ChargeInvoiceOrder extends Model<ChargeInvoiceOrder>{
+	public static final ChargeInvoiceOrder dao = new ChargeInvoiceOrder();
+}
---
+++ src/models/ChargeInvoiceOrderItem.java
@@ -0,0 +1,7 @@
+package models;
+
+import com.jfinal.plugin.activerecord.Model;
+
+public class ChargeInvoiceOrderItem extends Model<ChargeInvoiceOrderItem>{
+	public static final ChargeInvoiceOrderItem dao = new ChargeInvoiceOrderItem();
+}
