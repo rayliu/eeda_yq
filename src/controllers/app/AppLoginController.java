982b3b68cd998a72c67773e51a65546934db3d3e
kevin <kevin.dou@eeda123.com>
2017-12-20 15:02:16 +0800

    登录邀请码增加商家的影响力

--- src/controllers/app/AppLoginController.java
+++ src/controllers/app/AppLoginController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.shiro.codec.Base64;
 
@@ -44,7 +45,7 @@ public class AppLoginController extends Controller {
     	String pwd = URLDecoder.decode(getPara("pwd"), "UTF-8");
     	String mobile = URLDecoder.decode(getPara("mobile"), "UTF-8");
     	
-    	Record user = Db.findFirst("select * from user_login where phone = ?", mobile);
+    	Record user = Db.findFirst("select * from user_login where phone = ? and system_type = 'mobile'", mobile);
     	if(user == null){
     		Record user_login = new Record();
         	user_login.set("invitation_code", invite_code);
@@ -54,7 +55,17 @@ public class AppLoginController extends Controller {
         	user_login.set("system_type", "mobile");
         	user_login.set("password", pwd);
         	user_login.set("password_hint", pwd);
+        	user_login.set("create_time", new Date());
         	Db.save("user_login", user_login);
+        	
+        	if(StringUtils.isNotBlank(invite_code)){
+        		Record re = Db.findFirst("select * from user_login where invitation_code = ? and  system_type = '商家后台'",invite_code);
+        		if(re != null){
+        			String code = re.getStr("influence");
+        			re.set("influence", Integer.parseInt(code) + 1);
+        			Db.update("user_login",re);
+        		}
+        	}
         	result = true;
     	} else {
     		errMsg = "手机号码已被注册";
