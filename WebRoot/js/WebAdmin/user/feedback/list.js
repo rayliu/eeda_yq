e94d342d4d052e9c47e81124382b129f457e4a6b
kevin <kevin.dou@eeda123.com>
2018-01-19 18:00:54 +0800

    用户反馈

---
+++ WebRoot/js/WebAdmin/user/feedback/list.js
@@ -0,0 +1,29 @@
+define(['jquery', 'metisMenu',  'dataTablesBootstrap', 'sco'], function ($, metisMenu) { 
+    $(document).ready(function() {
+    	//datatable, 动态处理
+        var dataTable = eeda.dt({
+            id: 'eeda_table',
+            paging: true,
+            serverSide: true, //不打开会出现排序不对
+            ajax: "/WebAdmin/user/feedback/list",
+            columns: [
+	                     { "data": "USER_NAME", "width":"60px"},
+	                     { "data": "VALUE", "width":"60px"},
+	                     { "data": "CREATE_TIME", "width":"60px"}
+                     ]
+        });
+
+        $("#eeda_table").on("click",".delete",function(){
+        	var id = $(this).attr("id");
+        	$.post("/WebAdmin/user/ask/deleteQuestion",{id:id},function(data){
+        		if(data.result){
+        			window.location.reload();
+        		}
+            });
+        });
+        
+ 	 var refleshTable = function(){
+   	  dataTable.ajax.url("/WebAdmin/user/quotation/list").load();
+    }
+});
+});
