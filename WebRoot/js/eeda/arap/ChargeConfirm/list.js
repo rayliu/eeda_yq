6310e8d7fb78a655120437b0187893b1610b548d
kevin <kevin.dou@eeda123.com>
2018-05-16 19:00:26 +0800

    应收明细确认

--- WebRoot/js/eeda/arap/ChargeConfirm/list.js
+++ WebRoot/js/eeda/arap/ChargeConfirm/list.js
@@ -1,99 +1,184 @@
-define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap','sco'], function ($, metisMenu) { 
-
-    $(document).ready(function() {
-
-        var dataTable = eeda.dt({
-            id: 'eeda_table',
-            paging: true,
-            serverSide: false, //不打开会出现排序不对 
-            ajax: "/chargeConfirm/list?audit_flag="+$("#audit_flag").val(),
-            columns: [
-					{ "width": "10px",
-					    "render": function ( data, type, full, meta ) {
-					    	if(full.AUDIT_FLAG != 'Y')
-					    		return '<input type="checkbox" class="checkBox">';
-					    	else 
-					    		return '<input type="checkbox" disabled>';
-					    }
-					},
-					{ "data": "ORDER_NO", "width": "100px",
-						"render": function ( data, type, full, meta ) {
-			            	if(data){
-			            		return '<a href="/jobOrder/edit?id='+full.JOBID+'" target="_blank">'+data+'</a>';
-			            	}else{
-			            		return '';
-			            	}
-						}
-					},
-					{ "data": "CREATE_STAMP", "width": "100px"},
-					{ "data": "AUDIT_FLAG", "width": "60px",
-					"render": function ( data, type, full, meta ) {
-						if(data != 'Y')
-							return '未确认';
-						else 
-							return '已确认';
-					}
-					},
-					{ "data": "CUSTOMER", "width": "100px"},
-					{ "data": "TYPE", "width": "60px"},
-					{ "data": "SP_NAME", "width": "100px"},
-					{ "data": "CHARGE_NAME", "width": "60px"},
-					{ "data": "PRICE", "width": "60px"},
-					{ "data": "AMOUNT","width": "60px"},
-					{ "data": "UNIT_NAME", "width": "60px"},
-					{ "data": "TOTAL_AMOUNT", "width": "60px"},
-					{ "data": "CURRENCY_NAME", "width": "60px"},
-					{ "data": "EXCHANGE_RATE", "width": "60px"},
-					{ "data": "CURRENCY_TOTAL_AMOUNT", "width": "60px"},
-					{ "data": "CURRENCY_TOTAL_AMOUNT", "width": "60px"},
-					{ "data": "REMARK", "width": "180px"},
-            ]
-        });
-        
-        //全选
-        $('#AllCheck').click(function(){
-      	  var ischeck = this.checked;
-        	$(".checkBox").each(function () {  
-                this.checked = ischeck;  
-             });  
-        	if(ischeck==true){
-        		$('#confirmBtn').attr('disabled',false);
-        	}else{
-        		$('#confirmBtn').attr('disabled',true);
-        	}
-        });
-        
-        $("#eeda_table").on('click','.checkBox',function(){
-		    $("#AllCheck").prop("checked",$(".checkBox").length == $(".checkBox:checked").length ? true : false);
+define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap', 'validate_cn', 'sco',  'dtColReorder'], function ($, metisMenu) {
+  $(document).ready(function() {
+	  //new page
+      var dataTable = eeda.dt({
+          id: 'eeda_table',
+          colReorder: true,
+          serverSide: true, //不打开会出现排序不对 
+          initComplete: function (settings) {
+              eeda.dt_float_header('eeda_table');        
+              
+          },
+          "drawCallback": function( settings ) {
+        	  if($('#AllCheck').prop("checked")){
+            	  $('#AllCheck').prop("checked",false)
+              }
+              $('#AllCheck').click();
+		  },
+          columns: [
+      			{ "width": "10px",
+      				    "render": function ( data, type, full, meta ) {
+      				    	if(full.AUDIT_FLAG != 'Y')
+      				    		return '<input type="checkbox" checked class="checkBox">';
+      				    	else 
+      				    		return '<input type="checkbox" disabled>';
+      				    }
+      			},
+      			{ "data": "ORDER_NO", "width": "100px",
+      		    	  "render": function ( data, type, full, meta ) {
+                          return "<a href='/jobOrder/edit?id="+full.JOBID+"'target='_blank'>"+data+"</a>";
+                      }
+      			},
+      			{ "data": "ORDER_EXPORT_DATE", "width": "100px"},
+                  { "data": "CREATE_STAMP", "width": "100px"},
+                  { "data": "AUDIT_FLAG", "width": "60px",
+                  	"render": function ( data, type, full, meta ) {
+      			    	if(data != 'Y')
+      			    		return '未确认';
+      			    	else 
+      			    		return '已确认';
+      			    }
+            },
+            { "data": "CUSTOMER_NAME", "width": "100px"},
+            { "data": "TYPE", "width": "60px"},
+            { "data": "SP_NAME", "width": "100px","class":"SP_NAME"},
+            { "data": "CHARGE_NAME", "width": "60px"},
+            { "data": "PRICE", "width": "60px","class":"PRICE flag"},
+            { "data": "AMOUNT","width": "60px","class":"AMOUNT"},
+            { "data": "UNIT_NAME", "width": "60px"},
+            { "data": "TOTAL_AMOUNT", "width": "60px"},
+            { "data": "CURRENCY_NAME", "width": "60px","class":"CURRENCY_NAME"},
+            { "data": "EXCHANGE_RATE", "width": "60px"},
+            { "data": "CURRENCY_TOTAL_AMOUNT", "width": "60px"},
+            { "data": "EXCHANGE_CURRENCY_NAME", "width": "60px"},
+            { "data": "EXCHANGE_CURRENCY_RATE", "width": "60px" },
+            { "data": "EXCHANGE_TOTAL_AMOUNT", "width": "60px"},
+            { "data": "REMARK", "width": "180px"}
+          ]
       });
-      
+
+
+              
+    //base on config hide cols
+      dataTable.columns().eq(0).each( function(index) {
+          var column = dataTable.column(index);
+          $.each(cols_config, function(index, el) {
+              
+              if(column.dataSrc() == el.COL_FIELD){
+                
+                if(el.IS_SHOW == 'N'){
+                  column.visible(false, false);
+                }else{
+                  column.visible(true, false);
+                }
+              }
+          });
+      });
+
       
       $('#resetBtn').click(function(e){
           $("#orderForm")[0].reset();
       });
 
       $('#searchBtn').click(function(){
-          searchData(); 
+    	  searchData()
       })
 
-      var searchData=function(){
+     var searchData=function(){
           var order_no = $.trim($("#order_no").val()); 
-          var customer = $("#customer").val(); 
-          var sp = $("#sp").val(); 
+          
+          var order_export_date_begin_time = $("#order_export_date_begin_time").val();
+          var order_export_date_end_time = $("#order_export_date_end_time").val();
+          
+          var customer = $("#customer").val();
+          var customer_name = $("#customer_input").val().trim(); 
+          var sp = $("#sp").val();
+          var sp_name = $("#sp_input").val().trim(); 
           var start_date = $("#create_stamp_begin_time").val();
           var end_date = $("#create_stamp_end_time").val();
           var audit_flag = $("#audit_flag").val();
-       
+          /*  
+              查询规则：参数对应DB字段名
+              *_no like
+              *_id =
+              *_status =
+              时间字段需成双定义  *_begin_time *_end_time   between
+          */
           var url = "/chargeConfirm/list?order_no="+order_no
+          			   +"&order_export_date_begin_time="+order_export_date_begin_time
+          			   +"&order_export_date_end_time="+order_export_date_end_time
 			           +"&customer_id="+customer
+			           +"&customer_name_like="+customer_name
 			           +"&sp_id="+sp
+			           +"&sp_name_like="+sp_name
 		               +"&create_stamp_begin_time="+start_date
 		               +"&create_stamp_end_time="+end_date
           			   +"&audit_flag="+audit_flag;
-
-          dataTable.ajax.url(url).load();
+          	dataTable.ajax.url(url).load(false);
+          	$("#cny_totalAmountSpan").text(0.00)
+			$("#usd_totalAmountSpan").text(0.00)
+			$("#hkd_totalAmountSpan").text(0.00)
+			$("#jpy_totalAmountSpan").text(0.00)
       };
+     
+		var count_total = function(){
+			var sum_cny = 0.00;
+			var sum_usd = 0.00;
+			var sum_hkd = 0.00;
+			var sum_jpy = 0.00;
+			$("#eeda_table").find(".PRICE").each(function(){
+				self = $(this)
+				var total_money = parseFloat($(this).parent().find(".AMOUNT").text());
+				var price = parseFloat(self.text());
+				var currency_name = self.parent().find(".CURRENCY_NAME").text();
+				if(!isNaN(price)){
+					if(currency_name=='CNY'){
+						sum_cny += price*total_money;
+					}else if(currency_name=='USD'){
+						sum_usd += price*total_money;
+					}else if(currency_name=='HKD'){
+						sum_hkd += price*total_money;
+					}else if(currency_name=='JPY'){
+						sum_jpy += price*total_money;
+					}
+				}
+			})
+			$("#cny_totalAmountSpan").text(sum_cny.toFixed(2))
+			$("#usd_totalAmountSpan").text(sum_usd.toFixed(2))
+			$("#hkd_totalAmountSpan").text(sum_hkd.toFixed(2))
+			$("#jpy_totalAmountSpan").text(sum_jpy.toFixed(2))
+		}
+		
+		$("#eeda_table").on("change",".checkBox",function(){
+			self = $(this)
+			if(!self.prop("checked")){
+				self.parent().parent().find(".flag").removeClass('PRICE')
+			}else{
+				self.parent().parent().find(".flag").addClass('PRICE')
+			}
+			count_total()
+		})
       
+      //全选
+      $('#AllCheck').click(function(){
+    	  	$(".checkBox").prop("checked",this.checked);
+	      	if($('#AllCheck').prop('checked')){
+        		$('#confirmBtn').attr('disabled',false);
+        		$(".flag").addClass("PRICE");
+        	}else{
+        		$('#confirmBtn').attr('disabled',true);
+        		$(".flag").removeClass("PRICE");
+        	}
+	      	count_total();
+      });
+      
+      $("#eeda_table").on('click','.checkBox',function(){
+		    $("#AllCheck").prop("checked",$(".checkBox").length == $(".checkBox:checked").length ? true : false);
+      });
+      
+      
+      
+
       	//checkbox选中则button可点击
 		$('#eeda_table').on('click','.checkBox',function(){
 			
@@ -110,17 +195,17 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap','sco'], functi
 				$('#confirmBtn').attr('disabled',true);
 			}
 		});
-		
+		//确认费用
 		$('#confirmBtn').click(function(){
 			$('#confirmBtn').attr('disabled',true);
-	      	var itemIds=[];
-	      	$('#eeda_table input[type="checkbox"]').each(function(){
-	      		var checkbox = $(this).prop('checked');
-	      		if(checkbox){
-	      			var itemId = $(this).parent().parent().attr('id');
-	      			itemIds.push(itemId);
-	      		}
-	      	});
+        	var itemIds=[];
+        	$('#eeda_table input[type="checkbox"]').each(function(){
+        		var checkbox = $(this).prop('checked');
+        		if(checkbox){
+        			var itemId = $(this).parent().parent().attr('id');
+        			itemIds.push(itemId);
+        		}
+        	});
 	    	 $.post('/chargeConfirm/chargeConfirm?itemIds='+itemIds, function(data){
 	    		 if(data.result==true){
 	    			 $.scojs_message('单据确认成功', $.scojs_message.TYPE_OK);
@@ -131,9 +216,7 @@ define(['jquery', 'metisMenu', 'sb_admin',  'dataTablesBootstrap','sco'], functi
 	                $.scojs_message('单据确认失败', $.scojs_message.TYPE_ERROR);
 	                $('#confirmBtn').attr('disabled', false);
 	              });
-      })
+        })
       
-      
-    	
-    });
+  });
 });
--- src/controllers/arap/ar/ChargeConfirmController.java
+++ src/controllers/arap/ar/ChargeConfirmController.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import models.UserLogin;
 import models.eeda.oms.jobOrder.JobOrderArap;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
@@ -20,6 +21,8 @@ import com.jfinal.core.Controller;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
 
+import controllers.eeda.ListConfigController;
+import controllers.oms.jobOrder.JobOrderController;
 import controllers.profile.LoginUserController;
 import controllers.util.DbUtils;
 
@@ -32,11 +35,16 @@ public class ChargeConfirmController extends Controller {
 
 	@Before(EedaMenuInterceptor.class)
 	public void index() {		
+		UserLogin user = LoginUserController.getLoginUser(this);
+        long user_id = user.getLong("id");
+		List<Record> configList = ListConfigController.getConfig(user_id, "/chargeConfirmList");
+        setAttr("listConfigList", configList);
 		render("/eeda/arap/ChargeConfirm/ChargeConfirmList.html");
 	}
 	
-    public void list() {
-        String sLimit = "";
+	
+	public void list() {
+		String sLimit = "";
         String pageIndex = getPara("draw");
         if (getPara("start") != null && getPara("length") != null) {
             sLimit = " LIMIT " + getPara("start") + ", " + getPara("length");
@@ -44,9 +52,33 @@ public class ChargeConfirmController extends Controller {
         
         UserLogin user = LoginUserController.getLoginUser(this);
         long office_id=user.getLong("office_id");
-     
+        String sort = getPara("order[0][dir]")==null?"desc":getPara("order[0][dir]");
+        String sColumn =  getPara("order[0][column]");
+        String sName =  getPara("columns["+sColumn+"][data]")==null?"order_export_date":getPara("columns["+sColumn+"][data]") ;
+        String ref_office = "";
+        Record relist = Db.findFirst("select DISTINCT CAST(group_concat(ref_office_id) AS char) office_id from party where type='CUSTOMER' and ref_office_id is not null and office_id=?",office_id);
+        if(relist!=null){
+        	ref_office = " or jo.office_id in ("+relist.getStr("office_id")+")";
+        }
+        
+        String customer_id = getPara("customer_id");
+        Long userId = LoginUserController.getLoginUserId(this);
+        if(StringUtils.isNotEmpty(getPara("customer_id"))){
+    		//常用party查询保存进入历史记录
+          	JobOrderController.addHistoryRecord(userId,customer_id,"ARAP_COM");
+    	}
+        
+        String sp_id = getPara("sp_id");
+        if(StringUtils.isNotEmpty(getPara("sp_id"))){
+    		//常用party查询保存进入历史记录
+          	JobOrderController.addHistoryRecord(userId,sp_id,"ARAP_COM");
+    	}
+        
+        
+        
         String sql = "select * from( "
-        		+ " select joa.*,jo.order_no,jo.id jobid,jo.create_stamp,jo.customer_id,p.company_name customer,p1.company_name sp_name,f.name charge_name,u.name unit_name,c.name currency_name "
+        		+ " select joa.*,jo.id jobid,jo.order_no,jo.order_export_date,jo.create_stamp,jo.customer_id,p.abbr customer_name,p1.abbr sp_name,f.name charge_name,u.name unit_name,"
+        		+ " c.name currency_name, c1. NAME exchange_currency_name "
 				+ " from job_order_arap joa "
 				+ " left join job_order jo on jo.id=joa.order_id "
 				+ " left join party p on p.id=jo.customer_id "
@@ -54,26 +86,141 @@ public class ChargeConfirmController extends Controller {
 				+ " left join fin_item f on f.id=joa.charge_id "
 				+ " left join unit u on u.id=joa.unit_id "
 				+ " left join currency c on c.id=joa.currency_id "
-				+ " where joa.order_type='charge' and jo.office_id = "+office_id
+				+ " LEFT JOIN currency c1 ON c1.id = joa.exchange_currency_id"
+				+ " where joa.order_type='charge' and (jo.office_id = "+office_id + ref_office+")"
 				+ " and jo.delete_flag = 'N'"
 				+ " ) A where 1=1 ";
-        
         String condition = DbUtils.buildConditions(getParaMap());
-
         String sqlTotal = "select count(1) total from ("+sql+ condition+") B";
         Record rec = Db.findFirst(sqlTotal);
         logger.debug("total records:" + rec.getLong("total"));
+        List<Record> orderList = Db.find(sql+ condition + " order by "+ sName +" "+ sort);
+        Map map = new HashMap();
+        map.put("draw", pageIndex);
+        map.put("recordsTotal", rec.getLong("total"));
+        map.put("recordsFiltered", rec.getLong("total"));
+        map.put("data", orderList);
+        renderJson(map); 
+		
+	} 
+	
+	public void listTotal(){
+		String sLimit = "";
+        String pageIndex = getPara("draw");
+        if (getPara("start") != null && getPara("length") != null) {
+            sLimit = " LIMIT " + getPara("start") + ", " + getPara("length");
+        }
         
-        List<Record> orderList = Db.find(sql+ condition  +sLimit);
-        Map orderListMap = new HashMap();
-        orderListMap.put("draw", pageIndex);
-        orderListMap.put("recordsTotal", rec.getLong("total"));
-        orderListMap.put("recordsFiltered", rec.getLong("total"));
-
-        orderListMap.put("data", orderList);
-
-        renderJson(orderListMap); 
-    }   
+        UserLogin user = LoginUserController.getLoginUser(this);
+        long office_id=user.getLong("office_id");
+        String sort = getPara("order[0][dir]")==null?"desc":getPara("order[0][dir]");
+        String sColumn =  getPara("order[0][column]");
+        String sName =  getPara("columns["+sColumn+"][data]")==null?"order_export_date":getPara("columns["+sColumn+"][data]") ;
+        String ref_office = "";
+        Record relist = Db.findFirst("select DISTINCT CAST(group_concat(ref_office_id) AS char) office_id from party where type='CUSTOMER' and ref_office_id is not null and office_id=?",office_id);
+        if(relist!=null){
+        	ref_office = " or jo.office_id in ("+relist.getStr("office_id")+")";
+        }
+        String sql_money = "select ifnull(sum(price),0) sum_price,A.currency_name from( "
+        		+ " select joa.*,jo.id jobid,jo.order_no,jo.order_export_date,jo.create_stamp,jo.customer_id,p.abbr customer_name,p1.abbr sp_name,f.name charge_name,u.name unit_name,"
+        		+ " c.name currency_name, c1. NAME exchange_currency_name "
+				+ " from job_order_arap joa "
+				+ " left join job_order jo on jo.id=joa.order_id "
+				+ " left join party p on p.id=jo.customer_id "
+				+ " left join party p1 on p1.id=joa.sp_id "
+				+ " left join fin_item f on f.id=joa.charge_id "
+				+ " left join unit u on u.id=joa.unit_id "
+				+ " left join currency c on c.id=joa.currency_id "
+				+ " LEFT JOIN currency c1 ON c1.id = joa.exchange_currency_id"
+				+ " where joa.order_type='charge' and (jo.office_id = "+office_id + ref_office+")"
+				+ " and jo.delete_flag = 'N'"
+				+ " ) A where 1=1 ";
+        String condition = DbUtils.buildConditions(getParaMap());
+        List<Record>total_money = Db.find(sql_money +condition+" group by currency_name");
+        Map map = new HashMap();
+        map.put("total_money", total_money);
+        renderJson(map); 
+	}
+	
+	
+	
+//    public void list() {
+//        String sLimit = "";
+//        String pageIndex = getPara("draw");
+//        if (getPara("start") != null && getPara("length") != null) {
+//            sLimit = " LIMIT " + getPara("start") + ", " + getPara("length");
+//        }
+//        
+//        UserLogin user = LoginUserController.getLoginUser(this);
+//        long office_id=user.getLong("office_id");
+//     
+//        String sql = "select * from( "
+//        		+ " select joa.*,jo.order_no,jo.id jobid,jo.create_stamp,jo.customer_id,p.company_name customer,p1.company_name sp_name,f.name charge_name,u.name unit_name,c.name currency_name "
+//				+ " from job_order_arap joa "
+//				+ " left join job_order jo on jo.id=joa.order_id "
+//				+ " left join party p on p.id=jo.customer_id "
+//				+ " left join party p1 on p1.id=joa.sp_id "
+//				+ " left join fin_item f on f.id=joa.charge_id "
+//				+ " left join unit u on u.id=joa.unit_id "
+//				+ " left join currency c on c.id=joa.currency_id "
+//				+ " where joa.order_type='charge' and jo.office_id = "+office_id
+//				+ " and jo.delete_flag = 'N'"
+//				+ " ) A where 1=1 ";
+//        
+//        String condition = DbUtils.buildConditions(getParaMap());
+//
+//        String sqlTotal = "select count(1) total from ("+sql+ condition+") B";
+//        Record rec = Db.findFirst(sqlTotal);
+//        logger.debug("total records:" + rec.getLong("total"));
+//        
+//        List<Record> orderList = Db.find(sql+ condition  +sLimit);
+//        Map orderListMap = new HashMap();
+//        orderListMap.put("draw", pageIndex);
+//        orderListMap.put("recordsTotal", rec.getLong("total"));
+//        orderListMap.put("recordsFiltered", rec.getLong("total"));
+//
+//        orderListMap.put("data", orderList);
+//
+//        renderJson(orderListMap); 
+//    }   
+//	
+//	public void listTotal(){
+//		String sLimit = "";
+//        String pageIndex = getPara("draw");
+//        if (getPara("start") != null && getPara("length") != null) {
+//            sLimit = " LIMIT " + getPara("start") + ", " + getPara("length");
+//        }
+//        
+//        UserLogin user = LoginUserController.getLoginUser(this);
+//        long office_id=user.getLong("office_id");
+//        String sort = getPara("order[0][dir]")==null?"desc":getPara("order[0][dir]");
+//        String sColumn =  getPara("order[0][column]");
+//        String sName =  getPara("columns["+sColumn+"][data]")==null?"order_export_date":getPara("columns["+sColumn+"][data]") ;
+//        String ref_office = "";
+//        Record relist = Db.findFirst("select DISTINCT CAST(group_concat(ref_office_id) AS char) office_id from party where type='CUSTOMER' and ref_office_id is not null and office_id=?",office_id);
+//        if(relist!=null){
+//        	ref_office = " or jo.office_id in ("+relist.getStr("office_id")+")";
+//        }
+//        String sql_money = "select ifnull(sum(price),0) sum_price,A.currency_name from( "
+//        		+ " select joa.*,jo.id jobid,jo.order_no,jo.order_export_date,jo.create_stamp,jo.customer_id,p.abbr customer_name,p1.abbr sp_name,f.name charge_name,u.name unit_name,"
+//        		+ " c.name currency_name, c1. NAME exchange_currency_name "
+//				+ " from job_order_arap joa "
+//				+ " left join job_order jo on jo.id=joa.order_id "
+//				+ " left join party p on p.id=jo.customer_id "
+//				+ " left join party p1 on p1.id=joa.sp_id "
+//				+ " left join fin_item f on f.id=joa.charge_id "
+//				+ " left join unit u on u.id=joa.unit_id "
+//				+ " left join currency c on c.id=joa.currency_id "
+//				+ " LEFT JOIN currency c1 ON c1.id = joa.exchange_currency_id"
+//				+ " where joa.order_type='cost' and (jo.office_id = "+office_id + ref_office+")"
+//				+ " and jo.delete_flag = 'N'"
+//				+ " ) A where 1=1 ";
+//        String condition = DbUtils.buildConditions(getParaMap());
+//        List<Record>total_money = Db.find(sql_money +condition+" group by currency_name");
+//        Map map = new HashMap();
+//        map.put("total_money", total_money);
+//        renderJson(map); 
+//	}
     
 //    public void chargeConfirm(){
 //		String ids = getPara("itemIds");
