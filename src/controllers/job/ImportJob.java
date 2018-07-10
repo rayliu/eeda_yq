bc23385f31a59482f6791766f9a7b816f0d3dde7
kevin <kevin.dou@eeda123.com>
2018-07-10 22:24:32 +0800

    图片zip包解压

--- src/controllers/job/ImportJob.java
+++ src/controllers/job/ImportJob.java
@@ -3,15 +3,21 @@ package controllers.job;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream.GetField;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import models.UserLogin;
 
@@ -70,7 +76,9 @@ public class ImportJob implements Runnable{
             }
 		}
 		
-		
+		System.out.println((new Date())+"------------un_zip------------------");
+		unZip(PropKit.get("img_ftp_folder"), PropKit.get("old_zip_ftp_folder"));
+		//unZip("F:\\kevin_zip\\","F:\\kevin_zip\\old_zip\\");
 	}
 	
 	
@@ -130,6 +138,130 @@ public class ImportJob implements Runnable{
 			}
 		}
 		return resultMap;
+	}
+	
+	public static void unZip(String path,String new_path){
+		boolean unzip_result = false;
+		try {
+	    	File file = new File(path);
+	    	File[] filelist = file.listFiles();
+	    	for (int i = 0; i < filelist.length; i++) {
+	    		File f = filelist[i];
+	    		if(f.getName().endsWith(".zip")){
+	    			System.out.println("存在zip,开始解压");
+	    			String pathName = f.getAbsolutePath();
+	    			unzip_result = unZipFile(pathName , path);
+	    			
+	    			if(unzip_result){
+	    				if(moveTotherFolders(pathName , new_path)){
+	    					System.out.println("移动成功");
+	    				}else{
+	    					System.out.println("移动失败");
+	    				}
+	    			}else{
+	    				System.out.println("解压失败");
+	    			}
+	    		}
+	    	}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static boolean unZipFile(String pathName, String toPath) {
+	    File desFile = null;
+	    InputStream in = null;
+	    OutputStream out = null;
+	    ZipFile zf = null;
+	    try {
+	        //Log.v("unZipFile", "unzip start");
+	        System.out.println("unzip start");
+	        zf = new ZipFile(pathName, Charset.forName("GBK"));
+	        for (Enumeration <? > entries = zf.entries(); entries.hasMoreElements();) {
+	            ZipEntry entry = ((ZipEntry) entries.nextElement());
+	            if (entry.isDirectory()) {
+	                continue;
+	            }
+	            in = zf.getInputStream(entry);
+	            String strEntry = entry.getName();
+	            String strEntryFilename = strEntry.substring(strEntry.lastIndexOf("/") + 1);
+	            String filePath = toPath + File.separator + strEntryFilename;
+	            desFile = new File(filePath);
+	            out = new FileOutputStream(desFile);
+	            byte buffer2[] = new byte[1024 * 1024];
+	            int len = -1;
+	            while ((len = in .read(buffer2)) != -1) {
+	                out.write(buffer2, 0, len);
+	                //Log.v("unZipFile", "unZipFile len =" + len);
+	                //System.out.println("unZipFile len =" + len);
+	            }
+	            //FileUtils.renameFile(desFile.getAbsolutePath(), filePath);
+	        }
+	        //Log.v("unZipFile", "unzip finish");
+	        System.out.println("unzip finish");
+	        return true;
+	    } catch (IOException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	        return false;
+	    } finally {;
+	    	try {
+				out.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        try {
+				in.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        try {
+				zf.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }
+	}
+	
+	public static boolean moveTotherFolders(String startPath , String endPath){
+		File startFile = null;
+		String newFilePath = null;
+		boolean result = false;
+	    try {
+	        startFile = new File(startPath);
+	        File tmpFile = new File(endPath);//获取文件夹路径
+	        if(!tmpFile.exists()){//判断文件夹是否创建，没有创建则创建新文件夹
+	            tmpFile.mkdirs();
+	        }
+
+	        newFilePath = endPath + startFile.getName();
+	        File file = new File(endPath);
+	    	File[] filelist = file.listFiles();
+	    	for (int i = 0; i < filelist.length; i++) {
+	    		File f = filelist[i];
+	    		if(f.getName().endsWith(startFile.getName())){
+	    			//文件名重复处理
+	    			Date date = new Date();
+	    			long code = date.getTime();
+	    			newFilePath = newFilePath.substring(0, newFilePath.indexOf(".zip"))+"-"+code+".zip";
+	    			System.out.println(newFilePath);
+	    		}
+	    	}
+	    	result = startFile.renameTo(new File(newFilePath));
+	    	if(result){
+	    		System.out.println("success");
+	    	}else{
+	    		System.out.println("fail");
+	    	}
+	    } catch (Exception e) {
+	        //log.info("文件移动异常！文件名：《{}》 起始路径：{}",fileName,startPath);
+	    	System.out.println("文件移动异常！文件名：《"+startFile.getAbsolutePath()+"》 起始路径："+startPath);
+	    }
+	    return result;
 	}
 
 
