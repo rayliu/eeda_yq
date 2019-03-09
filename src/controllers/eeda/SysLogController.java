cd6b1e592e051f621c15186cd3da38523e5268e7
Ray.Liu <ray.liu@eeda123.com>
2019-03-10 01:30:13 +0800

    修复系统日志列表未区分公司的bug

--- src/controllers/eeda/SysLogController.java
+++ src/controllers/eeda/SysLogController.java
@@ -51,71 +51,75 @@ import controllers.util.EedaCommonHandler;
 import controllers.util.MD5Util;
 import controllers.util.ParentOffice;
 import controllers.util.getCurrentPermission;
-@Before({SetAttrLoginUserInterceptor.class,EedaMenuInterceptor.class})
+
+@Before({ SetAttrLoginUserInterceptor.class, EedaMenuInterceptor.class })
 public class SysLogController extends Controller {
     private static final String RANDOM_CODE_KEY = "eeda";
-	private Log logger = Log.getLog(SysLogController.class);
+    private Log logger = Log.getLog(SysLogController.class);
     Subject currentUser = SecurityUtils.getSubject();
 
-    public void index(){
-    	Record login_user = getAttr("login_user");
-    	List<Record> emailList = Db.find("select * from email_setting where office_id = ?",login_user.getLong("office_id"));
-    	for(Record email:emailList){
-    		setAttr(email.getStr("type"), email);
-    	}
-    	render("/eeda/profile/sysLog/sysLog.html");
+    public void index() {
+        Record login_user = getAttr("login_user");
+        List<Record> emailList = Db.find("select * from email_setting where office_id = ?",
+                login_user.getLong("office_id"));
+        for (Record email : emailList) {
+            setAttr(email.getStr("type"), email);
+        }
+        render("/eeda/profile/sysLog/sysLog.html");
     }
-    
-    public void save(){
-    	String params = getPara("params");
-    	Gson gson = new Gson();
-    	List<List<Map<String,Object>>> list = gson.fromJson(params,new TypeToken<List<List<Map<String,Object>>>>() { }.getType());
-    	if(list.size()>0){
-    		Record login_user = getAttr("login_user");
-    		Db.update("delete from email_setting where office_id = ?",login_user.getLong("office_id"));
-    		for(List<Map<String,Object>> i : list){
-    			Record re = new Record();
-        		for(Map<String,Object> map : i){
-            		re.set((String)map.get("name"), map.get("value"));
-            	}
-        		re.set("office_id", login_user.getLong("office_id"));
-        		Db.save("email_setting", re);
-        	}
-    	}
-    	renderJson("{\"result\":"+true+"}");
+
+    public void save() {
+        String params = getPara("params");
+        Gson gson = new Gson();
+        List<List<Map<String, Object>>> list = gson.fromJson(params, new TypeToken<List<List<Map<String, Object>>>>() {
+        }.getType());
+        if (list.size() > 0) {
+            Record login_user = getAttr("login_user");
+            Db.update("delete from email_setting where office_id = ?", login_user.getLong("office_id"));
+            for (List<Map<String, Object>> i : list) {
+                Record re = new Record();
+                for (Map<String, Object> map : i) {
+                    re.set((String) map.get("name"), map.get("value"));
+                }
+                re.set("office_id", login_user.getLong("office_id"));
+                Db.save("email_setting", re);
+            }
+        }
+        renderJson("{\"result\":" + true + "}");
     }
-    
-    public void getLoginLog(){
-    	//两种参数写法, 第一种为空就换第二种再试
-    	String condition = DbUtils.buildConditions(getParaMap());
-    	if(StrKit.isBlank(condition) && StrKit.notBlank(getPara("target_field")) && StrKit.notBlank(getPara("like_str"))){
-    	    String fields = getPara("target_field");
-    	    String like_str = getPara("like_str");
-    	    String[] fieldsArr = fields.split(",");
-    	    List<String> list = new ArrayList<String>();
-    	    for (int i = 0; i < fieldsArr.length; i++) {
-                String fieldName=fieldsArr[i];
-                String likeStr = fieldName+" like '%"+like_str+"%'";
+
+    public void getLoginLog() {
+        Record login_user = getAttr("login_user");
+        Long officeId = login_user.getLong("office_id");
+        // 两种参数写法, 第一种为空就换第二种再试
+        String condition = DbUtils.buildConditions(getParaMap());
+        if (StrKit.isBlank(condition) && StrKit.notBlank(getPara("target_field"))
+                && StrKit.notBlank(getPara("like_str"))) {
+            String fields = getPara("target_field");
+            String like_str = getPara("like_str");
+            String[] fieldsArr = fields.split(",");
+            List<String> list = new ArrayList<String>();
+            for (int i = 0; i < fieldsArr.length; i++) {
+                String fieldName = fieldsArr[i];
+                String likeStr = fieldName + " like '%" + like_str + "%'";
                 list.add(likeStr);
             }
-    	    condition=" and ("+StringUtils.join(list.toArray(), " or ") +")";
-    	}
-    	String sLimit = "";
+            condition = " and (" + StringUtils.join(list.toArray(), " or ") + ")";
+        }
+        String sLimit = "";
         String pageIndex = getPara("draw");
         if (getPara("start") != null && getPara("length") != null) {
             sLimit = " LIMIT " + getPara("start") + ", " + getPara("length");
         }
-        String sql = "select sl.id,sl.ip,sl.create_stamp,ul.user_name,o.office_name"
-        		+ " from sys_log sl"
-        		+ " left join user_login ul on ul.id = sl.user_id"
-        		+ " left join office o on o.id = sl.office_id"
-        		+ " where sl.log_type='登录'";
-        		
-        List<Record> orderList = Db.find(sql+ condition + " order by sl.id desc " +sLimit);
-        String sqlTotal = "select count(1) total from ("+sql+ condition+") B";
+        String sql = "select sl.id,sl.ip,sl.create_stamp,ul.user_name,o.office_name" + " from sys_log sl"
+                + " left join user_login ul on ul.id = sl.user_id" + " left join office o on o.id = sl.office_id"
+                + " where sl.log_type='登录' and sl.office_id="+officeId;
+
+        List<Record> orderList = Db.find(sql + condition + " order by sl.id desc " + sLimit);
+        String sqlTotal = "select count(1) total from (" + sql + condition + ") B";
         Record rec = Db.findFirst(sqlTotal);
         setAttr("queryTotal", sqlTotal);
-    	Map<String,Object> orderListMap = new HashMap<String,Object>();
+        Map<String, Object> orderListMap = new HashMap<String, Object>();
         orderListMap.put("draw", pageIndex);
         orderListMap.put("recordsTotal", rec.getLong("total"));
         orderListMap.put("recordsFiltered", rec.getLong("total"));
@@ -123,38 +127,40 @@ public class SysLogController extends Controller {
         orderListMap.put("data", orderList);
         renderJson(orderListMap);
     }
-    
-    public void getOperateLog(){
-    	//两种参数写法, 第一种为空就换第二种再试
-    	String condition = DbUtils.buildConditions(getParaMap());
-    	if(StrKit.isBlank(condition) && StrKit.notBlank(getPara("target_field")) && StrKit.notBlank(getPara("like_str"))){
-    	    String fields = getPara("target_field");
-    	    String like_str = getPara("like_str");
-    	    String[] fieldsArr = fields.split(",");
-    	    List<String> list = new ArrayList<String>();
-    	    for (int i = 0; i < fieldsArr.length; i++) {
-                String fieldName=fieldsArr[i];
-                String likeStr = fieldName+" like '%"+like_str+"%'";
+
+    public void getOperateLog() {
+        Record login_user = getAttr("login_user");
+        Long officeId = login_user.getLong("office_id");
+        // 两种参数写法, 第一种为空就换第二种再试
+        String condition = DbUtils.buildConditions(getParaMap());
+        if (StrKit.isBlank(condition) && StrKit.notBlank(getPara("target_field"))
+                && StrKit.notBlank(getPara("like_str"))) {
+            String fields = getPara("target_field");
+            String like_str = getPara("like_str");
+            String[] fieldsArr = fields.split(",");
+            List<String> list = new ArrayList<String>();
+            for (int i = 0; i < fieldsArr.length; i++) {
+                String fieldName = fieldsArr[i];
+                String likeStr = fieldName + " like '%" + like_str + "%'";
                 list.add(likeStr);
             }
-    	    condition=" and ("+StringUtils.join(list.toArray(), " or ") +")";
-    	}
-    	String sLimit = "";
+            condition = " and (" + StringUtils.join(list.toArray(), " or ") + ")";
+        }
+        String sLimit = "";
         String pageIndex = getPara("draw");
         if (getPara("start") != null && getPara("length") != null) {
             sLimit = " LIMIT " + getPara("start") + ", " + getPara("length");
         }
         String sql = "select sl.id,sl.order_id,action_type,sl.ip,sl.create_stamp,ul.user_name,o.office_name,efd.name form_name"
-        		+ " from sys_log sl"
-        		+ " left join user_login ul on ul.id = sl.user_id"
-        		+ " left join office o on o.id = sl.office_id"
-        		+ " left join eeda_form_define efd on efd.id = sl.form_id"
-        		+ " where sl.log_type = 'operate'";
-        List<Record> orderList = Db.find(sql+ condition + " order by sl.id desc " +sLimit);
-        String sqlTotal = "select count(1) total from ("+sql+ condition+") B";
+                + " from sys_log sl" + " left join user_login ul on ul.id = sl.user_id"
+                + " left join office o on o.id = sl.office_id"
+                + " left join eeda_form_define efd on efd.id = sl.form_id"
+                + " where sl.log_type = 'operate' and sl.office_id="+officeId;
+        List<Record> orderList = Db.find(sql + condition + " order by sl.id desc " + sLimit);
+        String sqlTotal = "select count(1) total from (" + sql + condition + ") B";
         Record rec = Db.findFirst(sqlTotal);
         setAttr("queryTotal", sqlTotal);
-    	Map<String,Object> orderListMap = new HashMap<String,Object>();
+        Map<String, Object> orderListMap = new HashMap<String, Object>();
         orderListMap.put("draw", pageIndex);
         orderListMap.put("recordsTotal", rec.getLong("total"));
         orderListMap.put("recordsFiltered", rec.getLong("total"));
