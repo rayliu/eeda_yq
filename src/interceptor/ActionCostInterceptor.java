5baa5129dc8800e759f5ec6e63fcb3293345627e
Allen <kevin.dou@eeda123.com>
2019-04-29 15:16:25 +0800

    非微信端打开app页面跳转到错误提示页面

--- src/interceptor/ActionCostInterceptor.java
+++ src/interceptor/ActionCostInterceptor.java
@@ -2,6 +2,7 @@ package interceptor;
 
 import com.jfinal.aop.Interceptor;
 import com.jfinal.aop.Invocation;
+import com.jfinal.core.Controller;
 import com.jfinal.log.Log;
 
 public class ActionCostInterceptor implements Interceptor {
@@ -14,6 +15,15 @@ public class ActionCostInterceptor implements Interceptor {
         long end = System.currentTimeMillis();
         long renderTime = end - start;
         logger.info(ai.getControllerKey()+"."+ai.getMethodName()+" action cost:"+renderTime+"ms");
+        String url = ai.getControllerKey();
+        if(url.indexOf("/app")>-1) {
+        	String userAgent = ai.getController().getRequest().getHeader("User-Agent")!=null?ai.getController().getRequest().getHeader("User-Agent").toLowerCase():"";
+        	boolean isWeiXin = false;
+    		if(!(userAgent.indexOf("micromessenger")>-1)){
+    			ai.getController().render("/lego_app/weiXinError.html");
+    			return;
+    		}
+        }
     }
 
 }
